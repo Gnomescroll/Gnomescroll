9a67978abef3f6758af4218f00d83377ccb2e402
HaltingState <haltingstate@gmail.com>
2011-02-09 08:55:08 -0500

    ok

--- html_client/js/game_state.js
+++ html_client/js/game_state.js
@@ -1,4 +1,5 @@
 
+//replace with more memory efficient version, later
 function Terrain_map()
 {
 	this.map = [];
@@ -6,19 +7,20 @@ function Terrain_map()
 	this.y_size = 0;
 	this.z_size = 0;
 	
-	this.prototype.init = function(x_size, y_size) {
+	this.prototype.init = function(x_size, y_size, z_size) {
+		this.x_size = x_size;
+		this.y_size = y_size;
+		this.z_size = z_size; //z is not used currently
 		
-		
+		this.map = new Array(x_size*y_size);
 	}
 	
-	
 	this.prototype.set = function(x,y,z,tile_value) {
-		
+		this.map[this.y_size * y + x] = tile_value;
 	}
 	
 	this.prototype.get = function(x,y,z) {
-		
-		
+		return this.map[this.y_size*y + x];
 	}
 }
 
@@ -28,13 +30,37 @@ function Game_world(world_id)
 	this.agent_list = [];
 	this.object_list = [];
 	
-	this.map = null;
+	this.map = new Terrain_map;
 	
 	
 	this.protype.load = function() {
 		//load map from server
 		
-		//
+		//make request for map data from server
+		info_get_map(this.world_id); //should be simple get method... but is call back function
+		info_get_agent_list(this.world_id);
+		info_get_object_list(this.world_id);
 	}
 
+}
+
+function info_get_map(world_id) {
+	client_id = 5;
+	
+	data = { 
+		command: "info_get_map",
+		client_id: client_id,
+		world_id: world_id 
+		};
+
+	$.post(window.ajax_server, data);
+}
+
+function info_get_agent_list(world_id) {
+	client_id = 5;
+	
+}
+
+function info_get_object_list(world_id) {
+	client_id = 5;
 }
