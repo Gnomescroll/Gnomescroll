84a4133e7b915982c8ee7526b6d6d9c37da5f92b
HaltingState <haltingstate@gmail.com>
2011-02-19 17:02:30 -0500

    new files

---
+++ libtcod_client/incoming/message_handlers.py
@@ -0,0 +1,40 @@
+from incoming.agent_messages import Agent_messages
+from incoming.object_messages import Object_messages
+from incoming.map_messages import Map_messages
+
+class Message_handlers:
+	
+	def __init__(self):
+		pass
+		
+	def define_handlers(self):
+		
+		self.info_handler = {
+		'get_agent' : self.info_commands.get_agent,
+		'get_object' : self.info_commands.get_object,
+		'get_map' : self.info_commands.get_map,
+		'get_agent_list' : self.info_commands.get_agent_list,
+		'get_object_list' : self.info_commands.get_object_list
+		}
+		
+		self.agent_handler = {
+		'move_0' : self.agent_commands.move_0,
+		'dig' : self.agent_commands.dig,
+		'construct_wall' : self.agent_commands.construct_wall,
+		
+		'pickup_item' : self.agent_commands.pickup_item,
+		'drop_item' : self.agent_commands.drop_item,
+		#'plant_tree' : self.agent_actions.plant_tree,
+		'till_soil' : self.agent_commands.till_soil,
+		'plant_crop' : self.agent_commands.plant_crop,
+		'harvest' : self.agent_commands.harvest,
+		#'store_item_in_container' : self.agent_actions.store_item_in_container,
+		#'remove_item_from_container' : self.agent_actions.remove_item_from_container
+		}
+		
+		self.admin_handler = {
+		'create_agent' : self.admin_commands.create_agent,
+		'create_item' : self.admin_commands.create_item,
+		'create_object' : self.admin_commands.create_object,
+		'set_map' : self.admin_commands.set_map
+		}
---
+++ libtcod_client/incoming/server_listener.py
@@ -0,0 +1,21 @@
+import Queue
+from threading import Thread
+import redis.client
+import json
+import sys
+
+def message_worker():
+	print "Message Worker Started"
+	r_client = redis.Redis("localhost") #will eventually need multiple output buffer servers
+	world_id = 0
+	key = "world_%s_out" % (world_id)
+	r_client.subscribe(key)
+	
+	for m in r_client.listen():
+		print str(m)
+			
+class Server_listener:
+	def __init__(self):
+		t = Thread(target=message_worker, args=())
+		t.daemon = True
+		t.start()
