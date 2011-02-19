3009e6bff6ec7d5c033ed401720946538d0ef2e8
HaltingState <haltingstate@gmail.com>
2011-02-19 11:54:38 -0500

    Basic template program.

---
+++ libtcod_client/client.py
@@ -0,0 +1,16 @@
+from server_listener import *
+from server_API import *
+import libtcodpy as libtcod
+
+##Constants##
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
+LIMIT_FPS = 20
+
+libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'dc_mmo', False)
+libtcod.sys_set_fps(LIMIT_FPS)
+
+while not libtcod.console_is_window_closed():
+	libtcod.console_set_foreground_color(0, libtcod.white)
+	libtcod.console_print_left(0, 1, 1, libtcod.BKGND_NONE, 'Welcome to dc_mmo!')
+	libtcod.console_flush()
