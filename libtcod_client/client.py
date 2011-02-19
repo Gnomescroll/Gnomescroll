91e29bf57d16dbd4d0dd0915b14b1002f62edb9e
HaltingState <haltingstate@gmail.com>
2011-02-19 12:11:12 -0500

    Oops, missed import and bad constant names.

--- libtcod_client/client.py
+++ libtcod_client/client.py
@@ -1,14 +1,15 @@
 from server_listener import *
 from server_API import *
 import libtcodpy as libtcod
+import textwrap
 
 def message(new_msg, color = libtcod.white):
     #split the message if necessary, among multiple lines
-    new_msg_lines = textwrap.wrap(new_msg, MSG_WIDTH)
+    new_msg_lines = textwrap.wrap(new_msg, MESSAGE_LOG_WIDTH)
  
     for line in new_msg_lines:
         #if the buffer is full, remove the first line to make room for the new one
-        if len(game_msgs) == MSG_HEIGHT:
+        if len(game_msgs) == MESSAGE_LOG_HEIGHT:
             del game_msgs[0]
  
         #add the new line as a tuple, with the text and the color
