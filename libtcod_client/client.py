4b8fcf49293fbb8d5ab58c951cc26e7dbde04b9e
HaltingState <haltingstate@gmail.com>
2011-02-19 11:59:01 -0500

    Added server listener, tweak screen setting size

--- libtcod_client/client.py
+++ libtcod_client/client.py
@@ -3,12 +3,18 @@ from server_API import *
 import libtcodpy as libtcod
 
 ##Constants##
-SCREEN_WIDTH = 80
-SCREEN_HEIGHT = 50
+SCREEN_WIDTH = 140
+SCREEN_HEIGHT = 80
 LIMIT_FPS = 20
 
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'dc_mmo', False)
 libtcod.sys_set_fps(LIMIT_FPS)
+
+#declare the server listener
+sl = Server_listener()
+
+#list of game messages
+game_msgs = []
 
 while not libtcod.console_is_window_closed():
 	libtcod.console_set_foreground_color(0, libtcod.white)
