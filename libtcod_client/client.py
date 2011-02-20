5dd016ac174b2032a365fab741477d88e79351c7
HaltingState <haltingstate@gmail.com>
2011-02-20 11:15:56 -0500

    Centered text in buttons, started work on the menu object.

--- libtcod_client/client.py
+++ libtcod_client/client.py
@@ -85,8 +85,6 @@ def render_all():
 		libtcod.console_print_left(fps_monitor, 0, 0, libtcod.BKGND_NONE, fps)	
 		libtcod.console_blit(fps_monitor, 0, 0, FPS_MONITOR_WIDTH, FPS_MONITOR_HEIGHT, 0, 0, 0, 1, 0.6)
 
-	libtcod.console_blit(con, 0, 0, 10, 1, 0, 0, 0, 1)
-
 
 ###MAIN PROGRAM###
 
--- libtcod_client/gui_elements.py
+++ libtcod_client/gui_elements.py
@@ -5,6 +5,7 @@ import libtcodpy as libtcod
 ##	button1 = Button(10, 1, "ab", 'A', "Tooltip text")
 ##	if button1.redraw:
 ##		button_console = button1.draw()
+##	libtcod.console_blit(button_console, 0, 0, 10, 1, 0, 0, 0, 1)
 
 class Button:
 	def __init__(self, width, height, text, hotkey, tooltip, fore_color = libtcod.white, back_color=libtcod.blue):
@@ -18,6 +19,9 @@ class Button:
 		self.mouse_is_hovering = False
 		self.button_con = libtcod.console_new(width, height)
 		self.redraw = True #True initially for first draw. After that, it should only be set to True if it is being hovered/clicked
+		self.msg = self.hotkey + ": " + self.text
+		self.center_x = (int)(width/2)
+		self.center_y = (int)(height/2)
 			
 
 	def draw(self):
@@ -32,9 +36,32 @@ class Button:
 			libtcod.console_rect(self.button_con, 0, 0, self.width, self.height, True, libtcod.BKGND_SET)
 
 		#Print the button to the button_con, and return that
-		libtcod.console_print_left(self.button_con, 0, 0, libtcod.BKGND_SET, self.hotkey)
-		libtcod.console_print_left(self.button_con, 1, 0, libtcod.BKGND_SET, ": ")
-		libtcod.console_print_left(self.button_con, 3, 0, libtcod.BKGND_SET, self.text)
+		print self.center_x, self.center_y
+		libtcod.console_print_center(self.button_con, self.center_x, self.center_y, libtcod.BKGND_SET, self.msg)
 
 		self.redraw = False
 		return self.button_con
+
+class Menu:
+	def __init__(self, title, fore_color, back_color, align="center", x=0, y=0):
+		self.title
+		self.fore_color = fore_color
+		self.back_color = back_color
+		self.align = align
+		self.x = x
+		self.y = y
+		self.lines = []
+		self.keys = []
+		
+	def add_button():
+		pass
+		
+
+	def initialize():
+		#todo add a cancel button at the end of all menus
+		self.menu_con = libtcod.console_new(self.width, self.height)
+		self.separator = '_' * self.width
+		
+		
+
+
