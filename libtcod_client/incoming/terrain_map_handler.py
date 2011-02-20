6a6cebbfe4a71a5b4257f52e51161a6bfcdcb562
HaltingState <haltingstate@gmail.com>
2011-02-20 14:43:50 -0500

    fixed

--- libtcod_client/incoming/terrain_map_handler.py
+++ libtcod_client/incoming/terrain_map_handler.py
@@ -6,7 +6,7 @@ class Terrain_map_handler:
 		pass
 
 	def init_terrain_map(self, map, z_level, x_size, y_size, world_id, client_id, **msg):
-		print "Terrain Map Assignment"
+		#print "Terrain Map Assignment"
 		self.terrain_map.x_size = x_size
 		self.terrain_map.y_size = y_size
 		self.terrain_map.map = map
