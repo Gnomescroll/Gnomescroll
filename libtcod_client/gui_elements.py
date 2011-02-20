c56e39797460b04b3facf9f85e1ca3680b23acaf
HaltingState <haltingstate@gmail.com>
2011-02-20 12:08:37 -0500

    Working on drawing menus efficiently.

--- libtcod_client/gui_elements.py
+++ libtcod_client/gui_elements.py
@@ -25,22 +25,24 @@ class Button:
 			
 
 	def draw(self):
-		#If the user is hovering over the button, swap foreground and background colors
-		if self.mouse_is_hovering:
-			libtcod.console_set_background_color(self.button_con, self.fore_color)
-			libtcod.console_set_foreground_color(self.button_con, self.back_color)
-			libtcod.console_rect(self.button_con, 0, 0, self.width, self.height, True, libtcod.BKGND_SET)
-		else:
-			libtcod.console_set_background_color(self.button_con, self.back_color)
-			libtcod.console_set_foreground_color(self.button_con, self.fore_color)
-			libtcod.console_rect(self.button_con, 0, 0, self.width, self.height, True, libtcod.BKGND_SET)
+		if self.redraw:
+			#If the user is hovering over the button, swap foreground and background colors
+			if self.mouse_is_hovering:
+				libtcod.console_set_background_color(self.button_con, self.fore_color)
+				libtcod.console_set_foreground_color(self.button_con, self.back_color)
+				libtcod.console_rect(self.button_con, 0, 0, self.width, self.height, True, libtcod.BKGND_SET)
+			else:
+				libtcod.console_set_background_color(self.button_con, self.back_color)
+				libtcod.console_set_foreground_color(self.button_con, self.fore_color)
+				libtcod.console_rect(self.button_con, 0, 0, self.width, self.height, True, libtcod.BKGND_SET)
 
-		#Print the button to the button_con, and return that
-		print self.center_x, self.center_y
-		libtcod.console_print_center(self.button_con, self.center_x, self.center_y, libtcod.BKGND_SET, self.msg)
+			#Print the button to the button_con, and return that
+			print self.center_x, self.center_y
+			libtcod.console_print_center(self.button_con, self.center_x, self.center_y, libtcod.BKGND_SET, self.msg)
 
-		self.redraw = False
-		return self.button_con
+			self.redraw = False
+
+			return self.button_con
 
 class Menu:
 	def __init__(self, title, fore_color, back_color, has_cancel_button = True, align="center", x=0, y=0):
@@ -55,18 +57,19 @@ class Menu:
 		self.width = 0
 		self.height = 0
 		self.has_cancel_button = has_cancel_button
+		self.redraw = True
 		
 	def add_button(button):
 		lines.append(["button", button])
 		if button.width > self.width:
 			self.width = button.width
-		height += 1
+		self.height += 1
 
 	def add_text(text):
 		lines.append(["text", text])
 		if len(text) > self.width:
 			self.width = len(text)
-		height += 1
+		self.height += 1
 		
 	def initialize():
 		if self.has_cancel_button:
@@ -76,7 +79,7 @@ class Menu:
 			if self.width < len(self.title):
 				self.width = len(self.title)
 			self.separator = '_' * self.width
-			height += 4 #Height increased by four, since we are adding two separators, a title, and a cancel button
+			self.height += 4 #Height increased by four, since we are adding two separators, a title, and a cancel button
 			self.lines.append(["text", self.separator])
 			self.lines.append(["button", Button(11, 1, "Cancel", "x", "Close this menu")])
 
@@ -85,11 +88,26 @@ class Menu:
 			if self.width < len(self.title):
 				self.width = len(self.title)
 			self.separator = '_' * self.width
-			height += 2 #Height increased by two, since we are adding a separator and a title
+			self.height += 2 #Height increased by two, since we are adding a separator and a title
 
 		self.lines.insert(0, ["text", self.title])
 		self.lines.insert(0, ["text", self.separator])
 		self.menu_con = libtcod.console_new(self.width, self.height)
+		self.center_x = (int)(width/2)
+
+	def draw():
+		current_line = 0
+		for line in lines:
+			if line[0] == "text":
+				libtcod.console_print_center(self.menu_con, self.center_x, current_line, libtcod.BKGND_SET, line[1])
+				current_line += 1
+
+			else:
+				if line[1].redraw:
+					button_con = line[1].draw()
+					libtcod.console_blit(menu_con, 0, 0, line[1].width, line[1].height, 0, MAP_VIEWER_WIDTH, 0)
+					
+		redraw = False		
 		
 
 
