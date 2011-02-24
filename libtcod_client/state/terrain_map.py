da83bd9f1fb29bd3280042fd626e4060506961e9
HaltingState <haltingstate@gmail.com>
2011-02-24 00:52:22 -0500

    Test code.

--- libtcod_client/state/terrain_map.py
+++ libtcod_client/state/terrain_map.py
@@ -1,3 +1,4 @@
+import time
 class Terrain_map:
 
 	def __init__(self):
@@ -23,3 +24,20 @@ class Terrain_map:
 	#needs to return an array, may be sparse
 	def get_z_level(self, z = 0):
 		return self.map
+
+	#returns a section of the map, starting at x, y, z, with width width, and height height
+	#this could probably be rewritten with list comprehensions if I understood them better
+	def get_map_section(self, x, y, z, width, height):
+		map_section = []
+		map_row = []
+		for h in range(y, height+1):
+			for w in range(x, width+1):
+				index = h * self.x_size + w
+				print "asking for index", index
+				map_row[w] = self.map[index]
+			map_section[h] = map_row
+		return map_section
+
+
+
+
