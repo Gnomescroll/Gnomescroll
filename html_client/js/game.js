24bf4f0400137e9bb88af5c3ab9c676f2e84264d
Steve Leonard <sleonard76@gmail.com>
2011-03-03 05:46:16 -0500

    fixed node server error

---
+++ html_client/js/game.js
@@ -0,0 +1,16 @@
+var game;
+
+game = {
+
+    init: // load z_level +/- 1. Load agents, objects. init render object.
+    function () {
+        
+    },
+    
+    start: // main game loop.  is it async/threaded?
+           // check user input, server updates, render game.
+    function () {
+        
+    },
+        
+};
