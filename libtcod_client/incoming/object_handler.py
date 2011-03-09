81ded06f69fc56ffcada48c103e7a5649acc13e4
A. Frederick Dudley <a.frederick.dudley@gmail.com>
2011-03-09 14:24:28 -0500

    Added update_agent and update_client to libtcod_client/client.py Finished object_handler.py Fixed typo in game_server/output/delta.py

--- libtcod_client/incoming/object_handler.py
+++ libtcod_client/incoming/object_handler.py
@@ -1,27 +1,36 @@
-
 class Object_handler:
 
-	def __init__(self):
-		self.objects = None
-		pass
+    def __init__(self):
+        self.objects = []
+        self.world_id = None
+        self.client_id = None
 
-	def object_info(self, id, client_id, value, **msg):
-		pass
-	
-	#state change
-	def object_position_change(self, id, position, version, **msg):
-		pass
-	
-	def object_state_change(self, id, position, version ,**msg):
-		pass
+    def object_info(self, id, client_id, value, **msg):
+        print 'object_info called.'
+    
+    #state change
+    def object_position_change(self, id, position, **msg):
+        for element in self.objects:
+            if element['id'] == id:
+                element['position'] = position
+    
+    def object_state_change(self, id, position, version ,**msg):
+        for element in self.objects:
+            if element['id'] == id:
+                element['version'] = version
 
-	#list
-	def object_list(self, list, client_id, world_id, **msg):
-		pass
-	
-	#creation and deletion
-	def object_create(self, id, position, **msg):
-		pass
-		
-	def object_delete(self, id, **msg):
-		pass
+    #list
+    def object_list(self, list, world_id, client_id, **msg):
+        self.objects = list
+        self.client_id = client_id
+        self.world_id  = world_id
+        
+    #creation and deletion
+    def object_create(self, id, position, **msg):
+        self.objects.append({"position": position, "version": 0,
+                             "type": msg['type'], "id": id})
+
+    def object_delete(self, id, **msg):
+        for element in self.objects:
+            if element['id'] == id:
+                del element
