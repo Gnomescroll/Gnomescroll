e583aa1d96ec68bbc4c772335e2fbab1491ed491
Steve Leonard <sleonard76@gmail.com>
2011-03-19 00:35:14 -0400

    fixed scope issue in tile_cache, added board canvas

--- html_client/tile_cache_example.js
+++ html_client/tile_cache_example.js
@@ -20,8 +20,30 @@ var tile_cache_canvas = {
             this.cache_canvas_dom = $('canvas#cache')[0];
             this.ctx = this.cache_canvas_dom.getContext("2d")
         },
-        
+}
+
+var board_canvas = {
+	    
+	//needs init code
+	//will re-init the cache when board is re-sized
 	
+	canvas_tile_width: 16,  //width of canvas in number of tiles
+	canvas_tile_height: 16, //width of canvas in number of tiles
+	
+	tile_pixel_width: 24,
+	tile_pixel_width: 24,
+    
+	cache_canvas_dom: $('<canvas></canvas>').attr('id', 'board'), // the thing used for reading/writing to canvas
+                       
+	ctx: null, // drawing surface
+    
+    init: function () {
+            $('body').append(this.cache_canvas_dom);
+            $('canvas#board').width(this.canvas_tile_width * this.tile_pixel_width)
+                             .height(this.canvas_tile_height * this.tile_pixel_width);
+            this.cache_canvas_dom = $('canvas#board')[0];
+            this.ctx = this.cache_canvas_dom.getContext("2d")
+        },
 }
 
 var tilemap = {
@@ -66,15 +88,30 @@ var drawTileToCache = function (tile_cache_canvas, tile_num, tilemap) {
 //				  cell_x, cell_y, cell_width, cell_height);
 }
 
+//this stores tile
+var tile_drawing_properties = (function () {
+	
+	var tile_drawing_property_array = [];
+	
+	function get(tile_id) {
+		return tile_drawing_property_array[tile_id];
+	}
+	
+	function set(tile_id, symbol, symbol_r, symbol_g, symbol_b, background_r, background_g, background_b) {
+		tile_drawing_property_array[tile_id] = [symbol, [symbol_r, symbol_g, symbol_b], [background_r, background_g, background_b]];
+	}
     
+    return { get: get,
+             set: set,
+             array: tile_drawing_property_array }
+}());
 var tile_cache = (function () {
-
 		//alert('entry 0');
-    var tile_cache_canvas = tile_cache_canvas,
+    var tcc = tile_cache_canvas,
         cache_dict = [], //maps tile_id to number, [tile_cache_position, x_offset, y_offset, width, height]
         cache_counter = 0, //gives the next free spot in cache
-        tile_drawing_properties = tile_drawing_properties, //stores the rendering metadata returned by the get_tiles info command
-        board_canvas = board_canvas; // canvas that we are drawing to
+        tdp = tile_drawing_properties, //stores the rendering metadata returned by the get_tiles info command
+        bc = board_canvas; // canvas that we are drawing to
 	
 	function free_index() {
         
@@ -89,7 +126,6 @@ var tile_cache = (function () {
 		 console.log('cache_dict');
 		 console.log(cache_dict);
 
-		
         var symbol,
             symbol_color,
             background_color,
@@ -115,7 +151,7 @@ var tile_cache = (function () {
 			//tile drawing properies are symbol, symbol_color, and background color
 			
 			/*
-			temp = tileset_metadata.get_tile_drawing_properties(tile_id) ;
+			temp = tileset_metadata.get_tdp(tile_id) ;
 			// returns [symbol_num, [symbol_color_r, symbol_color_g, symbol_color_b], [background_color_r, background_color_g, background_color_b]]
 			symbol = temp[0];
 			symbol_color = temp[1];
@@ -133,27 +169,27 @@ var tile_cache = (function () {
 			
 			tile_cache_position = free_index();
 			
-			x_row = tile_cache_position % tile_cache_canvas.canvas_tile_width;
+			x_row = tile_cache_position % tcc.canvas_tile_width;
 			y_row = tile_cache_position - x_row;
-			if (y_row != 0) { y_row = y_row / tile_cache_canvas.canvas_tile_height; }
+			if (y_row != 0) { y_row = y_row / tcc.canvas_tile_height; }
 			
-			x_offset = x_row * tile_cache_canvas.tile_width; //in pixels
-			y_offset = y_row * tile_cache_canvas.tile_height; // in pixels
+			x_offset = x_row * tcc.tile_width; //in pixels
+			y_offset = y_row * tcc.tile_height; // in pixels
 			
-			width = tile_cache_canvas.tile_width; //in pixels
-			height = tile_cache_canvas.tile_height; //in pixels
+			width = tcc.tile_width; //in pixels
+			height = tcc.tile_height; //in pixels
 			
 			//possibly clear canvas at (x_offset, y_offset, width, height)
 			
 			//draw solid color background onto canvas 
-			tile_cache_canvas.ctx.fillStyle = fillStyle =  'rgb(' + background_color .join(',') + ')';
-			tile_cache_canvas.ctx.fillRect(x_offset, y_offset, width,height);
+			tcc.ctx.fillStyle = fillStyle =  'rgb(' + background_color .join(',') + ')';
+			tcc.ctx.fillRect(x_offset, y_offset, width,height);
 			
 			//clear canvas cache at (0, 0, width, height)
-			tile_cache_canvas.ctx.clearRect(0, 0, width, height);
+			tcc.ctx.clearRect(0, 0, width, height);
 			
 			//draw symbol from tile map to (0, 0, width, height)
-			drawTileToCache(tile_cache_canvas, symbol, tilemap);
+			drawTileToCache(tcc, symbol, tilemap);
 	
 			//color symbol based upon symbol_color
 			/*INSERT CODE HERE*/
@@ -165,7 +201,7 @@ var tile_cache = (function () {
 		//copy tile from the tile_cache onto the board
 		
 
-		//copy (x_offset, y_offset, width, height) from canvas tile_cache_canvas.ctx
+		//copy (x_offset, y_offset, width, height) from canvas tcc.ctx
 		/*INSERT CODE HERE*/
 		
 		//to position x,y on board cavnas (need to calculate region)
@@ -174,27 +210,7 @@ var tile_cache = (function () {
 	
     return { free_index: free_index,
              draw_tile: draw_tile,
-             tile_cache_canvas: tile_cache_canvas,
              cache_dict: cache_dict,
              cache_counter: cache_counter,
-             tile_drawing_properties: tile_drawing_properties,
-             board_canvas: board_canvas,
            }
-}());
-
-//this stores tile
-var tile_drawing_properties = (function () {
-	
-	var tile_drawing_property_array = [];
-	
-	function get(tile_id) {
-		return tile_drawing_property_array[tile_id];
-	}
-	
-	function set(tile_id, symbol, symbol_r, symbol_g, symbol_b, background_r, background_g, background_b) {
-		tile_drawing_property_array[tile_id] = [symbol, [symbol_r, symbol_g, symbol_b], [background_r, background_g, background_b]];
-	}
-    
-    return { get: get,
-             set: set }
 }());
