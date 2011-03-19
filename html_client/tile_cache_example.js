e2f7ef1aa8e7ee25d76554876441db4d3a768f5b
HaltingState <haltingstate@gmail.com>
2011-03-18 20:13:55 -0400

    adding tileset drawing cache

--- html_client/tile_cache_example.js
+++ html_client/tile_cache_example.js
@@ -17,7 +17,8 @@ function tile_cache() {
 
 	tile_cache_canvas = ??? // assign an instance
 		
-	cache_dict = []; //maps tile_id to number
+	cache_dict = []; //maps tile_id to number, [tile_cache_position, x_offset, y_offset, x_width, y_width]
+	//contains the data needed for copying a tile from cache canvas to the board canvas
 	
 	tileset_metadata = ??? //stores the rendering metadata returned by the get_tiles info command
 	
@@ -31,12 +32,27 @@ function tile_cache() {
 			
 			//tile drawing properies are symbol, symbol_color, and background color
 			
-			//hardcode for now, but will get from tileset_metadata eventually
+			/*
+			temp = tileset_metadata.get_tile_drawing_properties(tile_id) ;
+			// returns [symbol_num, [symbol_color_r, symbol_color_g, symbol_color_b], [background_color_r, background_color_g, background_color_b]]
+			symbol = temp[0];
+			symbol_color = temp[1];
+			background_color = temp[2];
+			 */
+			
+			//hardcode for now, but get drawing properties from tileset_data eventually
 			symbol = 1; 
 			symbol_color = [256, 256, 256] //rgb
 			background_color = [100, 0, 0] //rgb
 			
+			//use square (0,0) as temporary drawing canvas
 			
+			//draw background color onto (x,y)
+			
+			x_row = 
+			y_row = 
+			
+			x_offset = 
 			
 		}
 		
@@ -46,4 +62,18 @@ function tile_cache() {
 		
 	}
 	
+}
+
+//this stores tile
+function tileset_metadata() {
+	
+	tile_drawing_property_array = [];
+	
+	function get_tile_drawing_properties(tile_id) {
+		return tile_drawing_property_array[tile_id];
+	}
+	
+	function set_tile_drawing_properties(tile_id, symbol, symbol_r, symbol_g, symbol_b, background_r, background_g, background_b) {
+		tile_drawing_property_array[tile_id] = [symbol, [symbol_r, symbol_g, symbol_b], [backgrond_r, background_g, background_b]];
+	}
 }
