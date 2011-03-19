72372f0fec9326914a0c553f93a8a1cda8b8fde9
HaltingState <haltingstate@gmail.com>
2011-03-19 00:27:29 -0400

    debug

--- html_client/tile_cache_example.js
+++ html_client/tile_cache_example.js
@@ -180,7 +180,7 @@ var tile_cache = (function () {
              cache_counter: cache_counter,
              tile_drawing_properties: tile_drawing_properties,
              board_canvas: board_canvas,
-             tile_cache_canvas = tile_cache_canvas
+             tile_cache_canvas: tile_cache_canvas
            }
 }());
 
