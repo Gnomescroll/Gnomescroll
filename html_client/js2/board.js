cf7a5b4fd01392c696c01d2b14d7e829c4f0ac86
HaltingState <haltingstate@gmail.com>
2011-03-24 16:26:11 -0400

    fixed

--- html_client/js2/board.js
+++ html_client/js2/board.js
@@ -363,8 +363,8 @@ var cursor_manager = {
 	reset_cursor_index: function() {
 		var i;
 		for(var x=0; x < this.board.board_tile_width; x++) {
-			for(var y=0; y < this.board_manager.board_tile_height; y++) {
-				i = x + y*this.board_manager.board_tile_width;
+			for(var y=0; y < this.board.board_tile_height; y++) {
+				i = x + y*this.board.board_tile_width;
 				this.index[i] = {
 					drawing_cursor: [0,-1,-1],
 					//last_blip : 0, //needed?
