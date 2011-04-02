e9ca3b141afe6f3250e7e3956893a8bf8706fe1e
Steve Leonard <sleonard76@gmail.com>
2011-04-02 17:02:10 -0400

    disabling python server Job_commands crap that prevents server from starting because it is entirely invalid and raises several exceptions

--- game_server/input/message_handlers.py
+++ game_server/input/message_handlers.py
@@ -1,55 +1,55 @@
 class Message_handlers:
-	
-	def __init__(self):
-		self.info_commands = None
-		self.agent_commands = None
-		self.admin_commands = None
-		self.agent_controller_commands = None
-		self.job_manager_commands = None
-		pass
-		
-	def define_handlers(self):
-		
-		self.info_handler = {
-		'get_agent' : self.info_commands.get_agent,
-		'get_object' : self.info_commands.get_object,
-		'get_map' : self.info_commands.get_map,
-		'get_agent_list' : self.info_commands.get_agent_list,
-		'get_object_list' : self.info_commands.get_object_list,
-		'get_tiles' : self.info_commands.get_tiles
-		}
-		
-		self.agent_handler = {
-		'move_0' : self.agent_commands.move_0,
-		'dig' : self.agent_commands.dig,
-		'construct_wall' : self.agent_commands.construct_wall,
-		
-		'pickup_item' : self.agent_commands.pickup_item,
-		'drop_item' : self.agent_commands.drop_item,
-		#'plant_tree' : self.agent_actions.plant_tree,
-		'till_soil' : self.agent_commands.till_soil,
-		'plant_crop' : self.agent_commands.plant_crop,
-		'harvest_crop' : self.agent_commands.harvest_crop,
-		'craft_item' : self.agent_commands.craft_item,
-		#'store_item_in_container' : self.agent_actions.store_item_in_container,
-		#'remove_item_from_container' : self.agent_actions.remove_item_from_container
-		'attack' : self.agent_commands.attack,
-		'construct_wall' : self.agent_commands.construct_wall,
-		}
-		
-		self.admin_handler = {
-		'create_agent' : self.admin_commands.create_agent,
-		'create_item' : self.admin_commands.create_item,
-		'create_object' : self.admin_commands.create_object,
-		'set_map' : self.admin_commands.set_map,
-		}
+    
+    def __init__(self):
+        self.info_commands = None
+        self.agent_commands = None
+        self.admin_commands = None
+        self.agent_controller_commands = None
+        self.job_manager_commands = None
+        pass
+        
+    def define_handlers(self):
+        
+        self.info_handler = {
+        'get_agent' : self.info_commands.get_agent,
+        'get_object' : self.info_commands.get_object,
+        'get_map' : self.info_commands.get_map,
+        'get_agent_list' : self.info_commands.get_agent_list,
+        'get_object_list' : self.info_commands.get_object_list,
+        'get_tiles' : self.info_commands.get_tiles
+        }
+        
+        self.agent_handler = {
+        'move_0' : self.agent_commands.move_0,
+        'dig' : self.agent_commands.dig,
+        'construct_wall' : self.agent_commands.construct_wall,
+        
+        'pickup_item' : self.agent_commands.pickup_item,
+        'drop_item' : self.agent_commands.drop_item,
+        #'plant_tree' : self.agent_actions.plant_tree,
+        'till_soil' : self.agent_commands.till_soil,
+        'plant_crop' : self.agent_commands.plant_crop,
+        'harvest_crop' : self.agent_commands.harvest_crop,
+        'craft_item' : self.agent_commands.craft_item,
+        #'store_item_in_container' : self.agent_actions.store_item_in_container,
+        #'remove_item_from_container' : self.agent_actions.remove_item_from_container
+        'attack' : self.agent_commands.attack,
+        'construct_wall' : self.agent_commands.construct_wall,
+        }
+        
+        self.admin_handler = {
+        'create_agent' : self.admin_commands.create_agent,
+        'create_item' : self.admin_commands.create_item,
+        'create_object' : self.admin_commands.create_object,
+        'set_map' : self.admin_commands.set_map,
+        }
 
-		self.agent_controller_handler = {
-		'move_agent_goal' : self.agent_controller_commands.move_agent_goal,
-		'move_item_goal' : self.agent_controller_commands.item_move_goal,
-		}
+        self.agent_controller_handler = {
+        'move_agent_goal' : self.agent_controller_commands.move_agent_goal,
+        'move_item_goal' : self.agent_controller_commands.item_move_goal,
+        }
 
-		self.job_manager_handler = {
-		'create_job' : self.job_manager_commands.create_job,
-		'delete_job' : self.job_manager_commands.delete_job,		
-		}
+        self.job_manager_handler = {
+        #'create_job' : self.job_manager_commands.create_job,
+        #'delete_job' : self.job_manager_commands.delete_job,       
+        }
--- game_server/server.py
+++ game_server/server.py
@@ -26,7 +26,7 @@ from input.info_commands import Info_commands
 from input.agent_commands import Agent_commands
 from input.admin_commands import Admin_commands
 from input.agent_controller_commands import Agent_controller_commands
-from input.job_manager_commands import Job_manager_commands
+#from input.job_manager_commands import Job_manager_commands
 ## output messages
 from output.info import Info
 from output.delta import Delta
@@ -38,78 +38,78 @@ from interface.crop import Crop
 
 class Server:
 
-	def __init__(self, world_id):
-		self.globals = Globals(world_id) #takes world_if as input
-		# listeners for input
-		self.message_listener = Message_listener()
-		self.message_handlers = Message_handlers()
-		self.info_commands = Info_commands()
-		self.agent_commands = Agent_commands()
-		self.admin_commands = Admin_commands()
-		self.agent_controller_commands = Agent_controller_commands()
-		self.job_manager_commands = Job_manager_commands()
-		# output to server
-		self.info = Info()
-		self.delta = Delta()
-		#game state
-		self.world_map = World_map()
-		self.world_time = World_time()
-		self.agents = Agents()
-		self.objects = Objects()
-		self.dat = Dat()
-		self.agent_command_scheduler = Agent_command_scheduler()
-		#agent controller
-		self.agent_controller = Agent_controller()
-		self.job_manager = Job_manager()
+    def __init__(self, world_id):
+        self.globals = Globals(world_id) #takes world_if as input
+        # listeners for input
+        self.message_listener = Message_listener()
+        self.message_handlers = Message_handlers()
+        self.info_commands = Info_commands()
+        self.agent_commands = Agent_commands()
+        self.admin_commands = Admin_commands()
+        self.agent_controller_commands = Agent_controller_commands()
+        #self.job_manager_commands = Job_manager_commands()
+        # output to server
+        self.info = Info()
+        self.delta = Delta()
+        #game state
+        self.world_map = World_map()
+        self.world_time = World_time()
+        self.agents = Agents()
+        self.objects = Objects()
+        self.dat = Dat()
+        self.agent_command_scheduler = Agent_command_scheduler()
+        #agent controller
+        self.agent_controller = Agent_controller()
+        #self.job_manager = Job_manager()
 
-	def share_state(self):
-		print "Share State Start"
-		not_singletons = []
-		to_share = [singleton for singleton in self.__dict__.items() if singleton[0] not in not_singletons]
-		def share(a,b):
-			name1, object1 = a[0], a[1]
-			name2, object2 = b[0], b[1]
-			if getattr(object1,name2,'xx') != 'xx':
-				#print 'Assigning',name2,'to',name1
-				setattr(object1,name2,object2)
-		[[share(singleton1,singleton2) for singleton2 in to_share] for singleton1 in to_share]
+    def share_state(self):
+        print "Share State Start"
+        not_singletons = []
+        to_share = [singleton for singleton in self.__dict__.items() if singleton[0] not in not_singletons]
+        def share(a,b):
+            name1, object1 = a[0], a[1]
+            name2, object2 = b[0], b[1]
+            if getattr(object1,name2,'xx') != 'xx':
+                #print 'Assigning',name2,'to',name1
+                setattr(object1,name2,object2)
+        [[share(singleton1,singleton2) for singleton2 in to_share] for singleton1 in to_share]
 
-		### INIT Templates ###
-		#Agent class init
-		Agent.world_map = self.world_map
-		Agent.delta = self.delta
-		Agent.agents = self.agents
-		Agent.objects = self.objects
-		Agent.world_time = self.world_time
-		Agent.dat = self.dat
-		#Object class init
-		Nobject.world_map = self.world_map
-		Nobject.delta = self.delta
-		Nobject.agents = self.agents
-		Nobject.objects = self.objects
-		Nobject.world_time = self.world_time
-		Nobject.dat = self.dat
-		#Crop class init
-		Crop.world_map = self.world_map
-		Crop.delta = self.delta
-		Crop.agents = self.agents
-		Crop.objects = self.objects
-		Crop.world_time = self.world_time
-		Crop.dat = self.dat
-		
-	def run(self):
-		self.share_state()
-		self.info.start(self.globals.world_id)
-		self.delta.start(self.globals.world_id)
-		self.world_map.init() #load map
-		self.agents.init() #load agents
-		#self.entities.init() #load objects
-		self.world_time.start()
-		self.message_handlers.define_handlers()
-		#self.message_listener.start()
-		self.message_listener.single_threaded_start()
-		
+        ### INIT Templates ###
+        #Agent class init
+        Agent.world_map = self.world_map
+        Agent.delta = self.delta
+        Agent.agents = self.agents
+        Agent.objects = self.objects
+        Agent.world_time = self.world_time
+        Agent.dat = self.dat
+        #Object class init
+        Nobject.world_map = self.world_map
+        Nobject.delta = self.delta
+        Nobject.agents = self.agents
+        Nobject.objects = self.objects
+        Nobject.world_time = self.world_time
+        Nobject.dat = self.dat
+        #Crop class init
+        Crop.world_map = self.world_map
+        Crop.delta = self.delta
+        Crop.agents = self.agents
+        Crop.objects = self.objects
+        Crop.world_time = self.world_time
+        Crop.dat = self.dat
+        
+    def run(self):
+        self.share_state()
+        self.info.start(self.globals.world_id)
+        self.delta.start(self.globals.world_id)
+        self.world_map.init() #load map
+        self.agents.init() #load agents
+        #self.entities.init() #load objects
+        self.world_time.start()
+        self.message_handlers.define_handlers()
+        #self.message_listener.start()
+        self.message_listener.single_threaded_start()
+        
 if __name__ == '__main__':
-	server = Server(0) #world _id = 0
-	server.run()
-	time.sleep(3600)
+    server = Server(0) #world _id = 0
+    server.run()
+    time.sleep(3600)
