0a0024a64aa0d329b467e1e38d1af1f8a0d926d0
HaltingState <haltingstate@gmail.com>
2011-04-17 15:56:57 -0400

    Map scrolling works I think

--- libtcod_client/worldgen.py
+++ libtcod_client/worldgen.py
@@ -3,55 +3,67 @@ import libtcodpy as libtcod
 #actual size of the window
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
+viewer_top_x = 0
+viewer_top_y = 0
 viewer_bot_x = SCREEN_WIDTH
 viewer_bot_y = SCREEN_HEIGHT
-MAP_WIDTH = 5
-MAP_HEIGHT = 10
+MAP_WIDTH = 15
+MAP_HEIGHT = 15
 map_viewer = libtcod.console_new(MAP_WIDTH, MAP_HEIGHT)
+mouse_on_drag_start = None
 
 LIMIT_FPS = 20  #20 frames-per-second maximum
 
-map = [[-1 for col in range(5)] for row in range(10)]
+map = [[-1 for col in range(MAP_WIDTH)] for row in range(MAP_HEIGHT )]
 
-print map
+def move_screen(dx, dy):
+	global viewer_top_x, viewer_top_y, viewer_bottom_x, viewer_bottom_y
+	
+	#moves the screen if that wouldn't cause the edge of the map to be exceeded.
+	#If it would, it moves as much as it can without passing the edge of the map.
+	viewer_bot_x = viewer_top_x + SCREEN_WIDTH
+	viewer_bot_y = viewer_top_y + SCREEN_HEIGHT
 
-"""def move_screen(dx, dy):
-		#moves the screen if that wouldn't cause the edge of the map to be exceeded.
-		#If it would, it moves as much as it can without passing the edge of the map.
-		viewer_bot_x = offset_x + MAP_VIEWER_WIDTH
-		viewer_bot_y = offset_y + MAP_VIEWER_HEIGHT
+	if viewer_top_x + dx < 0: 
+			dx = viewer_top_x * -1
+	
+	elif viewer_bot_x + dx >= MAP_WIDTH:
+			dx = MAP_WIDTH - viewer_bot_x
 
-		if offset_x + dx < 0: 
-		        dx = offset_x * -1
-	    
-		elif viewer_bot_x + dx >= MAP_WIDTH:
-		        dx = MAP_WIDTH - viewer_bot_x
+	if  viewer_top_y + dy < 0:
+			dy = viewer_top_y * -1
 
-		if  offset_y + dy < 0:
-		        dy = offset_y * -1
+	elif viewer_bot_y + dy >= MAP_HEIGHT:
+			dy = MAP_HEIGHT - viewer_bot_y
 
-		elif viewer_bot_y + dy >= MAP_HEIGHT:
-		        dy = MAP_HEIGHT - viewer_bot_y
-
-		
-		offset_x = offset_x + dx
-		offset_y = offset_y + dy"""
+	
+	viewer_top_x = viewer_top_x + dx
+	viewer_top_y = viewer_top_y + dy
 		
 def render():
+	global SCREEN_WIDTH, SCREEN_HEIGHT, map_viewer
 	x = 0;
 	y = 0;
 	libtcod.console_flush()
 	for x, row in enumerate(map):
 		for y, element in enumerate(row):
 			if element == -1:
-				color = libtcod.white
+				color = libtcod.green
 			libtcod.console_set_default_background(map_viewer, color)
 			libtcod.console_put_char(map_viewer, x, y, " ", libtcod.BKGND_SET)
-	libtcod.console_blit(map_viewer, 0, 0, 80, 50, 0, 0, 0)
+	libtcod.console_blit(map_viewer, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
 	
- 
+def handle_mouse(current_mouse):
+	global mouse_on_drag_start
+	if current_mouse.lbutton:
+		dx = mouse_on_drag_start.cx - current_mouse.cx;
+		dy = mouse_on_drag_start.cy - current_mouse.cy;
+		move_screen(dx, dy);
+		mouse_on_drag_start = current_mouse;
+	else:
+		mouse_on_drag_start = current_mouse;
+		
 def handle_keys():
- 
     key = libtcod.console_check_for_keypress()  #real-time
  
     if key.vk == libtcod.KEY_ENTER and libtcod.KEY_ALT:
@@ -75,8 +87,9 @@ while not libtcod.console_is_window_closed():
  
 	render();
 	
-	
+	handle_mouse(libtcod.mouse_get_status())
 	#handle keys and exit game if needed
 	exit = handle_keys()
+	
 	if exit:
 		break
