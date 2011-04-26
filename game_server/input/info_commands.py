5d329316b56e8931858ef62aae5636bc489d2066
Steve Leonard <sleonard76@gmail.com>
2011-04-26 17:06:56 -0400

    sanitizing input/info_commands.py

--- game_server/input/info_commands.py
+++ game_server/input/info_commands.py
@@ -8,12 +8,16 @@ class Info_commands:
 
     #required = ['agent_id', 'client_id', 'world_id']
     #optional = []      
-    def get_agent(self, agent_id, client_id, **msg):
+    def get_agent(self, agent_id=None, client_id=0, **msg):
+        if agent_id is None:
+            return
         self.info.agent_info(agent_id, client_id)
 
     #required = ['object_id', 'client_id', 'world_id']
     #optional = []          
-    def get_object(self, object_id, client_id, **msg):
+    def get_object(self, object_id=None, client_id=0, **msg):
+        if object_id is None:
+            return
         self.info.object_info(object_id, client_id)
 
     #required = ['client_id', 'world_id']
