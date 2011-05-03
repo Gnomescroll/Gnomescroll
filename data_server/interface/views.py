14d13b6ed5f36f77510083d5ec678400eddd0d1b
HaltingState <haltingstate@gmail.com>
2011-05-03 00:05:13 -0400

    moved stuff around

--- data_server/interface/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import Context, loader
-
-def index(request):
-	t = loader.get_template('index.html')
-	c = Context({})
-	return HttpResponse(t.render(c))
