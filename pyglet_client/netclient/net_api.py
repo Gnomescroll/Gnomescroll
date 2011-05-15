014a7be3232cab86eed5703092c4ed9c97ffe2b0
HaltingState <haltingstate@gmail.com>
2011-05-15 17:06:53 -0400

    refactoring breakthrough

--- pyglet_client/netclient/net_api.py
+++ pyglet_client/netclient/net_api.py
@@ -5,17 +5,17 @@ class NetApiGlobal:
     netOut = None
     SendMessage = None
     SendAdminMessage = None
+    SendChatMessage = None
 
     def __init__(self):
         #NetApiGlobal.netOut = NetOut()
         NetApiGlobal.sendMessage = SendMessage()
         NetApiGlobal.SendAdminMessage = SendAdminMessage()
+        NetApiGloba.SendChatMessage = SendChatMessage()
 
     @classmethod
     def init(self):
         NetApiGlobal.netOut = netOut.init()
-        NetApiGlobal.sendMessage.init()
-        NetApiGlobal.SendAdminMessage.init()
 
 class NetOut:
     sendMessage = None
@@ -32,11 +32,6 @@ class NetOut:
 
 
 class SendMessage:
-    client = None
-
-    @classmethod
-    def init(self):
-        self.send =
 
     def send_agent_control_state(self, id, d_x, d_y, d_xa, d_za, jetpack, brake):
         d = {
@@ -45,24 +40,20 @@ class SendMessage:
             'tick' : 0,
             'state': [d_x, d_y, d_xa, d_za, jetpack, brake]
            }
-        self.send_json(d)
-
-    def send_chat(self, d):
-        d['cmd'] = 'chat'
-        self.send_json(d)
+        NetOut.send_json(d)
 
     def send_client_id(self):
         d = {
             'cmd' : 'send_client_id',
             'id' : ClientGlobal.client_id,
            }
-        self.send_json(d)
+         NetOut.send_json(d)
 
     def request_chunk_list(self):
         d = {
             'cmd' : 'request_chunk_list',
             }
-        self.send_json(d)
+         NetOut.send_json(d)
 
     def request_chunk(x,y,z):
         d = {
@@ -70,26 +61,24 @@ class SendMessage:
             'value' : (x,y,z)
         }
 
-class SendAdminMessage:
+class ChatMessage:
 
-    @classmethod
-    def init(self):
-        pass
-    def __init__(self, client):
-        self.client = client
-        ClientGlobal.sendAdminMessage = self
-## Admin messages
+    def send_chat(self, d):
+        d['cmd'] = 'chat'
+        NetOut.send_json(d)
+
+class SendAdminMessage:
 
     def set_map(self,x,y,z,value):
         d = {
             'set_map' : 'set_map',
             'list' : [(x,y,z,value)],
             }
-        send_json(d)
+         NetOut.send_json(d)
 
     def set_map_bulk(self, list): #takes a list of 4 tuples of (x,y,z,value)
         d = {
             'set_map' : 'set_map',
             'list' : list,
             }
-        send_json(d)
+         NetOut.send_json(d)
