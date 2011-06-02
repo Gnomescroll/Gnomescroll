02167cdd43ca857bc5221df0c1999cd598acaee4
HaltingState <haltingstate@gmail.com>
2011-06-02 15:46:59 -0400

    speed fix

--- pyglet_client/netclient/agents.py
+++ pyglet_client/netclient/agents.py
@@ -459,10 +459,11 @@ def ray_cast_farest_empty_block(x,y,z, x_angle, y_angle, max_distance= 4., z_low
     n = 0.
     inc = 1. / sampling_density
     xy_inc = math.sqrt(dx**2 + dy**2)
+
     #md2 = max_distance**2
     while True:
         n += inc
-        if n*xy_inc > max_distance:
+        if n*xy_inc > max_distance or n*dz < z_low or n*dz > z_high:
             return None
 
         x_ = int(x+dx*n)
@@ -495,7 +496,7 @@ def ray_nearest_block(x,y,z, x_angle, y_angle, max_distance= 4., z_low=4, z_high
     #md2 = max_distance**2
     while True:
         n += inc
-        if n*xy_inc > max_distance:
+        if n*xy_inc > max_distance or n*dz < z_low or n*dz > z_high:
             return None
 
         x_ = int(x+dx*n)
