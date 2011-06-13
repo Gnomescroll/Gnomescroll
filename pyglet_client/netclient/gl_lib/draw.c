d81774d83299ad6576f0af602780e7a946687427
HaltingState <haltingstate@gmail.com>
2011-06-13 01:37:08 -0400

    drawing error fixed

--- pyglet_client/netclient/gl_lib/draw.c
+++ pyglet_client/netclient/gl_lib/draw.c
@@ -5,7 +5,7 @@ int _draw_line(int r, int g,int b, float x0, float y0, float z0, float x1, float
 
 printf("(%i, %i, %i)\n", r,g,b);
 
-glColor3i(r,g,b);
+glColor3b((unsigned char)r,(unsigned char)g,(unsigned char)b);
 glBegin(GL_LINES);
 glVertex3f(x0,y0,z0); // origin of the line
 glVertex3f(x1,y1,z1); // ending point of the line
@@ -14,7 +14,7 @@ return 0;
 }
 
 int _draw_point(int r, int g,int b, float x0, float y0, float z0) {
-glColor3i(r,g,b);
+glColor3b((unsigned char)r,(unsigned char)g,(unsigned char)b);
 glBegin(GL_POINTS);
 glVertex3f(x0,y0,z0); // point
 glEnd();
