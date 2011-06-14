12811ad86845aeb64ba5dca37e1af86b5ac385fb
HaltingState <haltingstate@gmail.com>
2011-06-14 03:49:46 -0400

    cube_lib.pyx

--- pyglet_client/netclient/cube_lib/cube_lib.pyx
+++ pyglet_client/netclient/cube_lib/cube_lib.pyx
@@ -6,10 +6,11 @@
 cdef enum:
     max_cubes = 4096
 
- cdef enum:
+cdef enum:
     x_chunk_size = 8
     y_chunk_size = 8
     z_chunk_size = 8
+    chunk_size = 512
 
 #structs
 cdef struct Vertex:
@@ -24,12 +25,12 @@ cdef struct Cube:
     Quad quad[6]
 
 cdef struct Chunk_scratch:
-    Quad quad[x_chunk_size*y_chunk_size*z_chunk_size]
+    Quad quad[chunk_size*6]
     int v_num
 
 #globals
 
-cdef quad_cache quad[4096]
+cdef quad_cache quad[max_cubes]
 
 cdef float v_index[72] = [
          0,1,1 , 0,0,1 , 1,0,1 , 1,1,1 , #top
@@ -46,21 +47,62 @@ cdef inline set_side(float x, float y, float z, int tile_id, int side_num, Quad*
     quad->y +=y
     quad->z +=z
 
+cdef inline set_tex(int vert_num, Quad* quad, float x, float y):
+    quad.tx = x * (1/8)
+    quad.ty = x * (1/8)
+    if vert_num == 0:
+        quad.tx += 0
+        quad.ty += 0
+    if vert_num == 1:
+        quad.tx += 1/8
+        quad.ty += 0
+    if vert_num == 2:
+        quad.tx += 1/8
+        quad.ty += 1/8
+    if vert_num == 3:
+        quad.tx += 0
+        quad.ty += 1/8
+
+def convert_index(index, height, width):
+    index = int(index)
+    height = int(height)
+    width = int(width)
+    x_ = index % width
+    y_ = int((index - x_) / width)
+    y = height - y_ -1
+    rvalue =  x_ + y*width
 #init
 def init_quad_cache():
     cdef Quad* quad
-    cdef int i,j,index
-    for i in range(0,5):
-        for j in range(0,4)
-            index = 12*i + 4*j
-            quad->x = v_index[index + 0]
-            quad->y = v_index[index + 1]
-            quad->z = v_index[index + 2]
-(tv_list, tc_list, ttex_list) = self.cubeRenderCache.get_side(rx, ry, rz, tile_id, side_num)
+    cdef int i,j,k,index
+    for k in range(0, chunk_size):
+        for i in range(0,5):
+            quad = &quad_cache[6*k+i]
+            for j in range(0,4)
+                index = 12*i + 4*j
+                #vertices
+                quad.x = v_index[index + 0]
+                quad.y = v_index[index + 1]
+                quad.z = v_index[index + 2]
+                #colors
+                quad.r = 255
+                quad.g = 255
+                quad.b = 255
+                quad.a = 255
+                set_tex(j, quad, 0, 3)
+
+#(tv_list, tc_list, ttex_list) = self.cubeRenderCache.get_side(rx, ry, rz, tile_id, side_num)
 
 #    int x_off,y_off,z_off
 
-from libc.stdlib cimport malloc, free
+#from libc.stdlib cimport malloc, free
 
-cdef Chunk_scratch * chunk_scratch = <Chunk_scratch *>malloc(sizeof(Chunk_scratch))
+#cdef Chunk_scratch * chunk_scratch = <Chunk_scratch *>malloc(sizeof(Chunk_scratch))
 
+## control state
+
+def clear_buffer():
+    quad_cache.v_num = 0
+
+def add_quad(x,y,z,side,tile):
+    quad_cache.v_num += 1
