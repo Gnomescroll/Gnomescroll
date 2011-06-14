1f63931b37657f320806a1d7ffbf438e30b66889
HaltingState <haltingstate@gmail.com>
2011-06-14 02:39:33 -0400

    added cube_lib to build sysystem

--- pyglet_client/setup.sh
+++ pyglet_client/setup.sh
@@ -2,7 +2,7 @@ cd server/
 echo 'building server/*.py'
 python setup.py build_ext --inplace
 
-cd ../netclient/
+cd ../netclient
 echo 'building netclient/*.py'
 python setup.py build_ext --inplace
 
