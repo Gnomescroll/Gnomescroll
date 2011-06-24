2cbafddf219b29c2d28f172f215440a1de3784d8
HaltingState <haltingstate@gmail.com>
2011-06-24 03:17:00 -0400

    text rendering progress

--- pyglet_client/netclient/SDL/SDL_text.c
+++ pyglet_client/netclient/SDL/SDL_text.c
@@ -1,7 +1,28 @@
 #include "SDL_text.h"
 
+
+TTF_Font *font;
+
+char* fontpath = "media/fonts/freesansbold.ttf";
+
 int _init_text() {
     TTF_Init();
+    //atexit(TTF_Quit);
+
+
+    if(!(font = TTF_OpenFont(fontpath, 20))) {
+        printf("Error loading font: %s", TTF_GetError());
+        return 1;
+    }
 
     return 0;
+/*
+    SDL_Color clrFg = {0,0,255,0};  // Blue ("Fg" is foreground)
+    SDL_Surface *sText = TTF_RenderText_Solid( font, "Courier 12", clrFg );
+    SDL_Rect rcDest = {0,0,0,0};
+    SDL_BlitSurface( sText,NULL, screen,&rcDest );
+    SDL_FreeSurface( sText );
+
+    return 0;
+*/
 }
--- pyglet_client/netclient/SDL/hud.pyx
+++ pyglet_client/netclient/SDL/hud.pyx
@@ -1,3 +1,6 @@
 
 cdef extern from 'SDL_text.h':
     int _init_text()
+
+def init_text():
+    _init_text()
