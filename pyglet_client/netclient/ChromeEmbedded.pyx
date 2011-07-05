838725a77e0fbfcdf79b2eec1202bbf4dff3cfe6
HaltingState <haltingstate@gmail.com>
2011-07-05 03:50:00 -0400

    working version

--- pyglet_client/netclient/ChromeEmbedded.pyx
+++ pyglet_client/netclient/ChromeEmbedded.pyx
@@ -1,8 +1,20 @@
 
 cdef extern from 'libChrome.h':
     int start()
+    int _init()
+    int _update()
+    int _draw()
 
 def test():
     print "test started"
     start()
     print "test finished"
+
+def init():
+    _init()
+
+def update():
+    _update()
+
+def draw():
+    _draw()
