cb8d5726ee5b1ea19a465657aeb6048ebe7c2ab6
HaltingState <haltingstate@gmail.com>
2011-07-15 03:52:29 -0400

    works but minor bugs

--- pyglet_client/netclient/gameloop.py
+++ pyglet_client/netclient/gameloop.py
@@ -191,8 +191,8 @@ class App(object):
                 x = a.x
                 y = a.y
                 z = a.z
-                x_angle = a.d_xa
-                y_angle = a.d_ya
+                x_angle = a.x_angle
+                y_angle = a.y_angle
                 v.ray_cast2(x,y,z, x_angle, y_angle)
             #SDL.gl.draw_point(255*random.random(),255*random.random(),255*random.random(), 0.1,0.0,2.0)
             #v.collision_test(0.1,0.0,2.0)
