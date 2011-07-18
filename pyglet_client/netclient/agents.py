257d33d425eeb6570218be6097c24d7f96f3b5bb
HaltingState <haltingstate@gmail.com>
2011-07-18 08:12:13 -0400

    fixed hit messages

--- pyglet_client/netclient/agents.py
+++ pyglet_client/netclient/agents.py
@@ -632,7 +632,7 @@ class PlayerAgent(AgentModel, AgentPhysics, PlayerAgentRender):
             if weapon.hitscan:
                 (ob, distance) = vox_lib.hitscan2(self.x,self.y,self.z,self.x_angle, self.y_angle)
                 if ob == None:
-                    print "Hit nothing, distance %f" % (distance/256.0)"
+                    print "Hit nothing, distance %f" % (distance/256.0)
                 else:
                     print "Hit Something, distance %f" % (distance/256.0)
                 # determine target w/ ray cast
