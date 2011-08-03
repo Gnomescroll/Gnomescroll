a2b33224a32f2580b2b249250f44ca87f046f4e7
HaltingState <haltingstate@gmail.com>
2011-08-02 20:31:42 -0400

    removed unneeded files

---
+++ pyglet_client/netclient/cube_lib/t_net.h
@@ -0,0 +1,4 @@
+
+
+int _get_net_out(char* buffer, int* length);
+int _net_in(char* buffer, int length);
---
+++ pyglet_client/netclient/profiler.py
@@ -0,0 +1,35 @@
+
+import SDL.gl
+
+def get_ticks():
+    return SDL.gl.get_ticks()
+
+class Profiler:
+
+    def __init__(self,):
+        self.frame_id = 0
+
+    def start_frame(self):
+        self.frame_id += 1
+        self.id = 0
+        self.times = []
+        self.label = []
+
+    def event(self, description):
+        self.times.insert(self.id, get_ticks())
+        self.label.insert(self.id, description)
+        self.id +=1
+
+    def finish_frame(self):
+        self.times.insert(self.id, get_ticks())
+        #print "Frame number: %i, total time= %i" % (self.frame_id, self.times[self.id] - self.times[0])
+        for i in range(0, self.id):
+            self.times[i] = self.times[i+1] -self.times[i]
+        for i in range(0, self.id):
+            pass
+            #print "%s: %i ms" % (self.label[i], self.times[i])
+
+    def flush_to_disc(self):
+        pass
+
+P = Profiler()
