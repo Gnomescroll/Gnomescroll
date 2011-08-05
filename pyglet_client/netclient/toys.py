339d0c61f59d49363e95150de4e87d0a2ecb773b
Steve Leonard <sleonard76@gmail.com>
2011-08-05 01:29:16 -0400

    client not overriding flag/base spawn

--- pyglet_client/netclient/toys.py
+++ pyglet_client/netclient/toys.py
@@ -3,10 +3,10 @@ Toys, miscellaneous objects
 '''
 
 from game_objects import DetachableObject, GameObjectRender, StaticObject
-from random import randint as rand
+#from random import randint as rand
 
-def rand_spot():
-    return (rand(0, 100), rand(0, 100), 10)
+#def rand_spot():
+    #return (rand(0, 100), rand(0, 100), 10)
 
 class Flag(DetachableObject):
 
@@ -18,15 +18,15 @@ class Flag(DetachableObject):
             self.team = GameStateGlobal.teamList[team]
         else:
             self.team = team
-        self.spawn()
+        #self.spawn()
         self.type = 1
         self.radius = 1
 
-    def spawn(self):
-        xyz = rand_spot()
-        self.pos(xyz)
-        self.on_ground = True
-        self.owner = None
+    #def spawn(self):
+        #xyz = rand_spot()
+        #self.pos(xyz)
+        #self.on_ground = True
+        #self.owner = None
 
 
 class Base(StaticObject, GameObjectRender):
@@ -34,7 +34,7 @@ class Base(StaticObject, GameObjectRender):
     def __init__(self, id, radius=None, team=None, state=None, **kwargs):
         StaticObject.__init__(self, id, state)
         GameObjectRender.__init__(self, 'auto.vox')
-        self.spawned = False
+        #self.spawned = False
         if team is not None:
             self.team = GameStateGlobal.teamList[team]
         else:
@@ -42,9 +42,9 @@ class Base(StaticObject, GameObjectRender):
         self.type = 2
         self.radius = 1
         
-    def spawn(self):
-        if not self.spawned:
-            self.state[0:3] = rand_spot()
-            self.spawned = True
+    #def spawn(self):
+        #if not self.spawned:
+            #self.state[0:3] = rand_spot()
+            #self.spawned = True
 
 from game_state import GameStateGlobal
