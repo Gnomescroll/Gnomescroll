97a0f14612e9ee2475ad4fed649d4de95ec32c62
HaltingState <haltingstate@gmail.com>
2011-08-19 01:34:53 -0400

    over optimized

--- pyglet_client/netclient/c_lib/ray_trace/ray_trace.c
+++ pyglet_client/netclient/c_lib/ray_trace/ray_trace.c
@@ -8,10 +8,16 @@
 
 float dummy;
 
+inline int collision_check(int x, int y, int z) {
+    //do something
+    return 0;
+}
+
 int ray_cast(float x0,float y0,float z0, float x1,float y1,float z1) {
     float len = sqrt( (x0-x1)*(x0-x1) + (y0-y1)*(y0-y1) + (z0-z1)*(z0-z1) );
-    int x,y,z;
+
     //int lx,ly,lz; //may or may not be used
+    int x,y,z;
     x = x0; //truncating conversion
     y = y0;
     z = z0;
@@ -37,25 +43,31 @@ int ray_cast(float x0,float y0,float z0, float x1,float y1,float z1) {
     dz = _dz*cdz;
 
     int i;
-    int max_i = (bsize / ssize)*len + 1
+    int max_i = (bsize / ssize)*len + 1; //over project so we dont end up in wall
     for(i =0; i <= max_i; i++) {
             cx += dx;
             cy += dy;
             cz += dz;
-
-            if(cx >= bsize) {
-                cx % bsize;
-                //check
+            if(cx >= bsize || cx >= bsize || cx >= bsize) {
+                if(cx >= bsize) {
+                    cx % bsize;
+                    x += cdx;
+                    //check
+                    collision_check(x,y,z);
+                }
+                if(cx >= bsize) {
+                    cx % bsize;
+                    y += cdy;
+                    //check
+                    collision_check(x,y,z);
+                }
+                if(cx >= bsize) {
+                    cx % bsize;
+                    z += cdz;
+                    //check
+                    collision_check(x,y,z);
+                }
             }
-            if(cx >= bsize) {
-                cx % bsize;
-                //check
-            }
-            if(cx >= bsize) {
-                cx % bsize;
-                //check
-            }
-
         }
     }
 
