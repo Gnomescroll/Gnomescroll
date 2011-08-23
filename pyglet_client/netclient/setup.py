b1cfb7b953701d38fab522074037ed1f77c984d7
HaltingState <haltingstate@gmail.com>
2011-08-23 11:34:54 -0400

    try now

--- pyglet_client/netclient/setup.py
+++ pyglet_client/netclient/setup.py
@@ -42,7 +42,7 @@ if SYSTEM == 'Windows':
 elif OS == "Darwin":
     libraries =["GLEW"]#, 'SDL_image', 'SDL', 'SDLmain'] #, "SDLmain"] # 'GL','GLU',
     #extra_link_args += ["-framework OpenGL", "-framework SDL"]
-    extra_link_args = [] #["-framework Cocoa","-framework SDL", "-framework SDL_image"]#  "-framework OpenGL" #"-framework Carbon", "-framework SDL", "-framework SDL_image",
+    extra_link_args = ["-framework OpenGL"] #["-framework Cocoa","-framework SDL", "-framework SDL_image"]#  "-framework OpenGL" #"-framework Carbon", "-framework SDL", "-framework SDL_image",
     include_dirs =  []#["/usr/local/include/SDL"] #['/usr/include/SDL'], #"/usr/local/Cellar/sdl/include",
     runtime_library_dirs = ["./"]
     library_dirs = [] #, "/usr/local/lib"]
