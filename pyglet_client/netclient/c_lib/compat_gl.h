e9c6c2b38fc1099e679601188c2807715e0c7fcf
HaltingState <haltingstate@gmail.com>
2011-08-28 11:12:10 -0400

    new config for apple

--- pyglet_client/netclient/c_lib/compat_gl.h
+++ pyglet_client/netclient/c_lib/compat_gl.h
@@ -8,13 +8,15 @@
         #include <SDL.h>
         #include <SDL_opengl.h>
     #elif _WIN32
-        #include <SDL.h>
         #include <windows.h>
+        #include <SDL.h>
         #include <GL/glew.h>
     #elof __APPLE__
         //#define NO_SDL_GLEXT
         #include <GL/glew.h>
         #include <SDL.h>
+        #include <OpenGL/gl.h>
+        #include <OpenGL/glu.h>
         //#include <SDL_opengl.h>
     #endif
 
