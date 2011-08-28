d68e3b30175682e595d4d396df3d5b1bc8a88b19
HaltingState <haltingstate@gmail.com>
2011-08-28 10:33:44 -0400

    ok

--- pyglet_client/netclient/c_lib/texture_loader.h
+++ pyglet_client/netclient/c_lib/texture_loader.h
@@ -1,15 +1,8 @@
 #ifndef texture_loader_h
 #define texture_loader_h
 
-#define NO_SDL_GLEXT
-#include <GL/glew.h>
-
-#include <SDL.h>
-#include <SDL_opengl.h>
-
-#include <SDL_image.h> //temp?
-
 #include "compat.h"
+#include <compat_gl.h>
 
 int init_texture_loader();
 
