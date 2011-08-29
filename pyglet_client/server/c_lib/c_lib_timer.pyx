ae1c6a19b682367d03da2738a72da0f3aa4857e8
HaltingState <haltingstate@gmail.com>
2011-08-29 19:29:48 -0400

    patched up configuration file for physics timer

---
+++ pyglet_client/server/c_lib/c_lib_timer.pyx
@@ -0,0 +1,17 @@
+cdef extern from "../c_lib/physics_timer.h":
+    int _start(int frequency)
+    int _tick_check()
+    long _get_time()
+    long _get_tick()
+
+def start_physics_timer(int frequency):
+    _start(frequency)
+
+def tick_check():
+    return _tick_check()
+
+def get_time():
+    return _get_time()
+
+def get_tick():
+    return _get_tick()
