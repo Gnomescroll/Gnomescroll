dee904a0892042f5588e46a6c8eab94ad3642b21
HaltingState <haltingstate@gmail.com>
2011-08-31 04:46:54 -0400

    testing UDP

--- pyglet_client/netclient/net_lib/server.c
+++ pyglet_client/netclient/net_lib/server.c
@@ -5,29 +5,16 @@
 //uint16_t
 
 //ip in a.b.c.d format
-struct NetPeer* create_net_peer(int a, int b, int c, int d, unsigned short port) {
-    struct NetPeer s* = malloc(sizeof(struct NetPeer));
 
-    unsigned int destination_address = ( a << 24 ) | ( b << 16 ) | ( c << 8 ) | d;
-
-    s->address.sin_family = AF_INET;
-    s->address.sin_addr.s_addr = htonl( destination_address );
-    s->address.sin_port = htons( destination_port );
-
-    s->ip = htonl( destination_address )
-    s->port = htons( destination_port )
-    return s;
-}
 
 //pass in zero to get any port
 struct Socket* create_socket(uint32_t IP, uint16_t  port) {
     //create socket
-    struct Socket s* = malloc(sizeof(struct Socket));
+    struct Socket* s = malloc(sizeof(struct Socket));
     if(s==NULL) { printf("Malloc of socket failed.  Out of memory? \n"); return NULL;}
 
-    s->handle = socket( AF_INET, SOCK_DGRAM, IPPROTO_UDP );
-
-    if ( s->handle <= 0 )
+    s->socket = socket( AF_INET, SOCK_DGRAM, IPPROTO_UDP );
+    if ( s->socket <= 0 )
     {
         printf( "failed to create socket\n" );
         free(s);
@@ -43,20 +30,20 @@ struct Socket* create_socket(uint32_t IP, uint16_t  port) {
     s->port = port;
     //s->ip =  ?? do we know public IP?
 
-    if ( bind( handle, (const sockaddr*) &s->address, sizeof(sockaddr_in) ) < 0 )
+    if ( bind( s->socket, (const struct sockaddr*) &s->address, sizeof(struct sockaddr_in) ) < 0 )
     {
         printf( "failed to bind socket\n" );
         free(s);
         return NULL;
     }
-    printf("Socket bound to port %i\n", ntohl(s->port));
+    printf("Socket bound to port %i\n", port);
 
     //set socket to non-blocking
 
     #if PLATFORM == PLATFORM_MAC || PLATFORM == PLATFORM_UNIX
 
         int nonBlocking = 1;
-        if ( fcntl( s->handle, F_SETFL, O_NONBLOCK, nonBlocking ) == -1 )
+        if ( fcntl( s->socket, F_SETFL, O_NONBLOCK, nonBlocking ) == -1 )
         {
             printf( "failed to set non-blocking socket\n" );
             free(s);
@@ -66,7 +53,7 @@ struct Socket* create_socket(uint32_t IP, uint16_t  port) {
     #elif PLATFORM == PLATFORM_WINDOWS
 
         DWORD nonBlocking = 1;
-        if ( ioctlsocket( s->handle, FIONBIO, &nonBlocking ) != 0 )
+        if ( ioctlsocket( s->socket, FIONBIO, &nonBlocking ) != 0 )
         {
             printf( "failed to set non-blocking socket\n" );
             free(s);
@@ -77,20 +64,35 @@ struct Socket* create_socket(uint32_t IP, uint16_t  port) {
     return s;
 }
 
-int send_packet(struct* Socket socket, char* packet_data, int packet_size) {
+struct NetPeer* create_net_peer(int a, int b, int c, int d, unsigned short port) {
+    struct NetPeer* s = malloc(sizeof(struct NetPeer));
 
-    int sent_bytes = sendto( s->handle, (const char*)packet_data, packet_size,
-                             0, (sockaddr*)&socket_address, sizeof(sockaddr_in) );
+    unsigned int destination_address = ( a << 24 ) | ( b << 16 ) | ( c << 8 ) | d;
+
+    s->address.sin_family = AF_INET;
+    s->address.sin_addr.s_addr = htonl( destination_address );
+    s->address.sin_port = htons( port );
+
+    s->ip = htonl( destination_address );
+    s->port = htons( port );
+    return s;
+}
+
+int send_packet(struct Socket* socket, struct NetPeer* p, char* packet_data, int packet_size) {
+
+    int sent_bytes = sendto( socket->socket, (const char*)packet_data, packet_size,
+                             0, (const struct sockaddr*)&p->address, sizeof(struct sockaddr_in) );
 
     if ( sent_bytes != packet_size )
     {
         printf( "failed to send packet: return value = %d\n", sent_bytes );
-        return false;
+        return 0;
     }
+    return sent_bytes;
 }
 
 #define maximum_packet_size 1024
-void receive_packets(struct* Socket socket) {
+void receive_packets(struct Socket* s) {
     printf("Listening on port: %i\n", ntohl(s->port));
 
     unsigned char packet_data[maximum_packet_size];
@@ -98,7 +100,7 @@ void receive_packets(struct* Socket socket) {
     unsigned int from_port;
     int received_bytes;
 
-    while ( true )
+    while ( 1 )
     {
 
 
@@ -106,14 +108,35 @@ void receive_packets(struct* Socket socket) {
         typedef int socklen_t;
         #endif
 
-        sockaddr_in from;
+        struct sockaddr_in from;
         socklen_t fromLength = sizeof( from );
 
-        received_bytes = recvfrom( socket, (char*)packet_data, maximum_packet_size,
-                                   0, (sockaddr*)&from, &fromLength );
+        received_bytes = recvfrom( s->socket, (char*)packet_data, maximum_packet_size,
+                                   0, (struct sockaddr*)&from, &fromLength );
 
-        if ( received_bytes <= 0 )
+        if ( received_bytes < 0 ) {
+
+            if ( errno == EAGAIN ||  errno == EWOULDBLOCK) {
+                continue;
+            }
+            if(received_bytes==-1) {
+                perror("Socket Error:");
+               printf("Network: socket error!\n");
+               break;
+            }
+            perror("Socket Error:");
+            printf("Network: negative received bytes, %i\n", received_bytes);
             break;
+        }
+        if ( received_bytes == 0) {
+            printf("No new packets: continue\n");
+            continue;
+        }
+
+        if(received_bytes > 0) {
+            packet_data[received_bytes] = NULL;
+            printf("packet: %s \n", packet_data);
+        }
 
         from_address = ntohl( from.sin_addr.s_addr );
         from_port = ntohs( from.sin_port );
--- pyglet_client/netclient/net_lib/server.h
+++ pyglet_client/netclient/net_lib/server.h
@@ -1,25 +1,56 @@
 #ifndef net_lib_server_h
 #define net_lib_server_h
 
+    #define PLATFORM_WINDOWS  1
+    #define PLATFORM_MAC      2
+    #define PLATFORM_UNIX     3
+
+    #if defined(_WIN32)
+    #define PLATFORM PLATFORM_WINDOWS
+    #elif defined(__APPLE__)
+    #define PLATFORM PLATFORM_MAC
+    #else
+    #define PLATFORM PLATFORM_UNIX
+    #endif
+
+    #if PLATFORM == PLATFORM_WINDOWS
+
+        #include <winsock2.h>
+
+    #elif PLATFORM == PLATFORM_MAC || PLATFORM == PLATFORM_UNIX
+
+        #include <sys/socket.h>
+        #include <netinet/in.h>
+        #include <fcntl.h>
+
+    #endif
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <errno.h>
+
 struct Socket {
     uint32_t ip;
     uint16_t port;
-    int handle;
-    sockaddr_in address;
-}
+    int socket;
+    struct sockaddr_in address;
+};
 
 struct NetPeer {
     uint32_t ip;
     uint16_t port;
-    sockaddr_in address;
-}
+    struct sockaddr_in address;
+};
 
 
 struct NetPeer* create_net_peer(int a, int b, int c, int d, unsigned short port);
-struct Socket* create_socket(uint32_t IP, uint16_t  port); //port=0 to get any port
-int send_packet(struct* Socket socket, char* packet_data, int packet_size);
+//port=0 to get any port
+struct Socket* create_socket(uint32_t IP, uint16_t  port);
 
-void receive_packets(struct* Socket socket);
+int send_packet(struct Socket* socket, struct NetPeer* p, char* packet_data, int packet_size);
+void receive_packets(struct Socket* socket);
 
 
 #endif
