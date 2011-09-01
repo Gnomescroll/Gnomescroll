a4005a57892396ebcec0d548a1393f7f8d2b7835
HaltingState <haltingstate@gmail.com>
2011-08-31 21:06:48 -0400

    write out gen code

--- pyglet_client/netclient/net_lib/code_generator.py
+++ pyglet_client/netclient/net_lib/code_generator.py
@@ -24,19 +24,26 @@ def run():
                 continue
             s.insert_member(n2)
         #print n1
-    for s in struct_list:
-        s.print_def()
-    print ""
-    for s in struct_list:
-        s.struct_size()
-    print ""
-    for s in struct_list:
-        s.packing_def()
-    print ""
-    for s in struct_list:
-        s.unpacking_def()
 
-type_list = ["n","un","s","us","b"]
+    x,y,z,z2 = "\n","\n","\n","\n"
+    for s in struct_list:
+        z = s.print_def()
+    for s in struct_list:
+        x+=s.struct_size()
+    for s in struct_list:
+        y+=s.packing_def()
+        y+=s.unpacking_def()
+    print x
+    print y
+
+    size_out = open("./mgen/size.h","w")
+    size_out.write(x)
+    struct_def = open("./mgen/struct_def.h", "w")
+    struct_def.write(z)
+    struck_packing = open("./mgen/struct_packing.h", "w")
+    struct_def.write(y)
+
+type_list = ["n","un","s","us","b","f"]
 
 def struct_proc(list):
     n = list[1];
@@ -51,8 +58,9 @@ def struct_proc(list):
         return "uint16_t %s" % (nn)
     if n == "b":
         return "uint8_t %s" % (nn)
-
-    print "Type Missing: %s" % (n)
+    if n == "f":
+        return "float %s" % (nn)
+    print "ERROR: Type Missing: %s" % (n)
     assert False
 
 def pack_proc(list):
@@ -68,10 +76,33 @@ def pack_proc(list):
     if n == "us":
         m = "uint16_t"
     if n == "b":
-        m ="uint8_t"
+        m = "uint8_t"
+    if n == "f":
+        m = "float"
     if m != "":
         return "\tPACK_%s(s->%s, buffer, &n);\n" % (m,nn)
-    print "Type Missing: %s" % (n)
+    print "ERROR: Type Missing: %s" % (n)
+    assert False
+
+def pack_proc(list):
+    n = list[1];
+    nn = list[2];
+    m = ""
+    if n == "n":
+        m = "int32_t"
+    if n == "un":
+        m = "uint32_t"
+    if n == "s":
+        m ="int16_t"
+    if n == "us":
+        m = "uint16_t"
+    if n == "b":
+        m = "uint8_t"
+    if n == "f":
+        m = "float"
+    if m != "":
+        return "\tUNPACK_%s(s->%s, buffer, &n);\n" % (m,nn)
+    print "ERROR: Type Missing: %s" % (n)
     assert False
 
 class Struct:
@@ -107,32 +138,40 @@ class Struct:
 
     def struct_def(self):
         if self.name == None:
-            return
+            return ''
         p1 = "struct %s { \n" % (self.name)
         p2 = ""
         for i in self.members:
             p2 += "\t%s;\n" % (struct_proc(i))
         p3 = "};\n"
-        print p1+p2+p3
+        return p1+p2+p3
 
     def struct_size(self):
         if self.name == None:
-            return
-        print "printf(" +'"'+ "sizeof(%s)="%(self.name)+'%i' +'", ' +"sizeof(struct %s);" % (self.name)
+            return ''
+        return "printf(" +'"'+ "sizeof(%s)="%(self.name)+'%i' +'", ' +"sizeof(struct %s));" % (self.name)
 
     def packing_def(self):
         if self.name == None:
-            return
+            return ''
         s1 = "int pack_struct_%s" %(self.name) + "(void* buffer, struct %s* s) { \n" % (self.name)
         s1 += "\tint n=0;\n"
         s2 = ''
         for i in self.members:
             s2 += pack_proc(i)
         s3 = "\treturn n;\n}\n"
-        print s1+s2+s3
+        return s1+s2+s3
 
     def unpacking_def(self):
-        pass
+        if self.name == None:
+            return ''
+        s1 = "void unpack_struct_%s" %(self.name) + "(void* buffer, struct %s* s) { \n" % (self.name)
+        s1 += "\tint n=0;\n"
+        s2 = ''
+        for i in self.members:
+            s2 += unpack_proc(i)
+        s3 = "\treturn n;\n}\n"
+        return s1+s2+s3
 
     def send_function(self):
         pass
