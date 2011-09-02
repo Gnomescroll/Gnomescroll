17a4bada5f5c9f2237e8cdd3a742ac25c8f1d89c
Steve Leonard <sleonard76@gmail.com>
2011-09-02 06:16:00 -0400

    sound module. bad wscript. needs compilation

---
+++ pyglet_client/netclient/sound/player_sounds.c
@@ -0,0 +1,16 @@
+#include "player_sounds.h"
+
+void load_player_gunshot() {
+    player_gunshot = load_2d_sound("./wav/semishoot.wav");
+}
+
+void play_player_gunshot() {
+    player_gunshot_channel = play_2d_sound(player_gunshot);
+}
+
+void end_player_gunshot() {
+    release_sound(player_gunshot);
+}
+
+int main(int argc, char *argv[]){return 0;}
+
---
+++ pyglet_client/netclient/sound/player_sounds.h
@@ -0,0 +1,8 @@
+#include "sound.h"
+
+FMOD_SOUND* player_gunshot;
+FMOD_CHANNEL* player_gunshot_channel;
+
+void load_player_gunshot();
+void play_player_gunshot();
+void end_player_gunshot();
