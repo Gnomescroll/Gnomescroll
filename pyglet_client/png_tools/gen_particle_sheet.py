6b0d08df2f89cddf02d7c8d60150785cbf4e9c54
Steve Leonard <sleonard76@gmail.com>
2011-09-01 23:31:51 -0400

    clean up of gen_particle_sheet.py

--- pyglet_client/png_tools/gen_particle_sheet.py
+++ pyglet_client/png_tools/gen_particle_sheet.py
@@ -1,72 +1,35 @@
-
-sprites = [
-('star1.png', 0),
-('star2.png', 1),
-('star3.png', 2),
-('star4.png', 3),
-('star5.png', 4),
-('star6.png', 5),
-
-#laser effects
-('laser1.png', 16),
-('laser2.png', 17),
-('laser3.png', 18),
-('laser4.png', 19),
-('laser5.png', 20),
-('fire1.png', 21),
-#neutron
-
-('neutron/blue/1.png', 48),
-('neutron/blue/2.png', 49),
-('neutron/blue/3.png', 50),
-('neutron/yellow/1.png', 51),
-('neutron/yellow/2.png', 52),
-('neutron/yellow/3.png', 53),
-('neutron/red/1.png', 54),
-('neutron/red/2.png', 55),
-('neutron/red/3.png', 56),
-('neutron/purple/1.png', 57),
-('neutron/purple/2.png', 58),
-('neutron/purple/3.png', 59),
-('neutron/pink/1.png', 60),
-('neutron/pink/2.png', 61),
-('neutron/pink/3.png', 62),
-('neutron/orange/1.png', 63),
-('neutron/orange/2.png', 64),
-('neutron/orange/3.png', 65),
-('neutron/green/1.png', 66),
-('neutron/green/2.png', 67),
-('neutron/green/3.png', 68),
-('neutron/dark_blue/1.png', 69),
-('neutron/dark_blue/2.png', 70),
-('neutron/dark_blue/3.png', 71),
-('neutron/cyan/1.png', 72),
-('neutron/cyan/2.png', 73),
-('neutron/cyan/3.png', 74),
-
-#calibration
-('test1.png', 255),
-]
-
-'''
-neutron_colors = ['blue','yellow','red','purple','pink','orange','green','dark_blue','cyan']
-i = 16+32
-for color in neutron_colors:
-    for n in range(1,4):
-        filename = 'neutron/' + color + "/" + str(n) +".png"
-        print str((filename,i)) +","
-        i+=1
-'''
-
-def _offset(id):
-    x = id % 16
-    y = (id - x) / 16
-    return (x,y)
-
+#!/usr/bin/python
 import png
-png_out = png.Writer(width=512, height=512, alpha=True, bitdepth=8, transparent=None)
+import sys
+import os.path
+from sprites import sprites
 
-if True:
+def verify_sprite_conf(sprites):
+
+    def warn(f, i, msg=''):
+        print 'WARNING: %s [ %s :: %d ]' % (msg, f, i)
+        return True
+
+    fail = False
+    ids = []
+    for fn, i in sprites:
+        i = int(i)
+        if i in ids:
+            fail = warn(fn, i, 'id already in use')
+        ids.append(i)
+
+        if not os.path.exists(fn):
+            fail = warn(fn, i, 'file does not exist')
+    return fail
+    
+def generate_spritesheet(sprites):
+
+    def _offset(id):
+        x = id % 16
+        y = (id - x) / 16
+        return (x,y)
+
+
     print "start row generation"
     rows = []
     i2=0
@@ -74,54 +37,56 @@ if True:
         rows.insert(i,[])
         rows[i] = [0,0,0,0] * 512
 
-    print "works"
-
+    for pngfile, id in sprites:
+        print "Processing Particle: %s, %i" % (pngfile, id)
+        x = png.Reader(filename = "./particle/"+pngfile)
+        #width, height, pixels, meta = x.asDirect()
+        width, height, pixels, meta = x.asRGBA8()
+        #print "width,height= %i, %i" % (width, height)
+        if width != 32 or height != 32:
+            print "%s, width,height= %i, %i" % (pngfile, width, height)
+            print "Height, Width must be 32,32!"
+            print 'Aborting'
+            sys.exit()
+        #print "meta= " + str(meta)
+        xi,yi = _offset(id)
+        #print "xi, yi= %i, %i" % (xi, yi)
+        i = 0
+        j= 0
+        jr = range(32)
+        for row in pixels:
+            for j in jr:
+                r = row[4*j+0]
+                g = row[4*j+1]
+                b = row[4*j+2]
+                a = row[4*j+3]
+                #print "r,g,b,a= %i,%i,%i,%i" %(r,g,b,a)
+                rows[i+32*yi][4*(j+32*xi)+0] = r
+                rows[i+32*yi][4*(j+32*xi)+1] = g
+                rows[i+32*yi][4*(j+32*xi)+2] = b
+                rows[i+32*yi][4*(j+32*xi)+3] = a
+            i += 1
+    return rows
+    
 ## open file for writing
-for pngfile, id in sprites:
-    print "Processing Particle: %s, %i" % (pngfile, id)
-    x = png.Reader(filename = "./particle/"+pngfile)
-    #width, height, pixels, meta = x.asDirect()
-    width, height, pixels, meta = x.asRGBA8()
-    #print "width,height= %i, %i" % (width, height)
-    if width != 32 or height != 32:
-        print "%s, width,height= %i, %i" % (pngfile, width, height)
-        print "Height, Width must be 32,32!"
-        assert False
-    #print "meta= " + str(meta)
-    xi,yi = _offset(id)
-    #print "xi, yi= %i, %i" % (xi, yi)
-    #for i in range(0,32):
-    #    for j in range(0,32):
-    i = 0
-    j= 0
-    for row in pixels:
-        for j in range(32):
-            r = row[4*j+0]
-            g = row[4*j+1]
-            b = row[4*j+2]
-            a = row[4*j+3]
-            #print "r,g,b,a= %i,%i,%i,%i" %(r,g,b,a)
-            rows[i+32*yi][4*(j+32*xi)+0] = r
-            rows[i+32*yi][4*(j+32*xi)+1] = g
-            rows[i+32*yi][4*(j+32*xi)+2] = b
-            rows[i+32*yi][4*(j+32*xi)+3] = a
-        i += 1
-            #rows[i+xi][j+yi] = value
-       #     j+=1
-        #i+=1
+def write_png(rows):
+    png_out = png.Writer(width=512, height=512, alpha=True, bitdepth=8, transparent=None)
 
- #           rows[i+xi][j+yi] = pixels[i,j]
-#write out
-print "writing out"
-f = open('./particles_01.png', 'wb')      # binary mode is important
-png_out.write(f, rows)
+    print "writing out"
+    with open('./particles_01.png', 'wb') as f:      # binary mode is important
+        png_out.write(f, rows)
 
-f = open('../netclient/texture/particles_01.png', 'wb')      # binary mode is important
-png_out.write(f, rows)
+    with open('../netclient/media/texture/particles_01.png', 'wb') as f:      # binary mode is important
+        png_out.write(f, rows)
 
-if False:
-    f = open('particle_sheet.png', 'wb')      # binary mode is important
-    w = png.Writer(512, 512, greyscale=True)
-    w.write(f, [range(256)])
-    f.close()
 
+def main(sprites):
+    if not verify_sprite_conf(sprites):
+        print 'Spritesheet generation failed. Please fix errors in sprites configuration'
+        sys.exit()
+    pixels = generate_spritesheet(sprites)
+    write_png(pixels)
+    
+if __name__ == '__main__':
+    main(sprites)
+    print 'Done'
---
+++ pyglet_client/png_tools/sprites.py
@@ -0,0 +1,52 @@
+'''
+(relative file path, position in spritesheet (0-indexed))
+'''
+
+sprites = [
+('star1.png', 0),
+('star2.png', 1),
+('star3.png', 2),
+('star4.png', 3),
+('star5.png', 4),
+('star6.png', 5),
+
+#laser effects
+('laser1.png', 16),
+('laser2.png', 17),
+('laser3.png', 18),
+('laser4.png', 19),
+('laser5.png', 20),
+('fire1.png', 21),
+#neutron
+
+('neutron/blue/1.png', 48),
+('neutron/blue/2.png', 49),
+('neutron/blue/3.png', 50),
+('neutron/yellow/1.png', 51),
+('neutron/yellow/2.png', 52),
+('neutron/yellow/3.png', 53),
+('neutron/red/1.png', 54),
+('neutron/red/2.png', 55),
+('neutron/red/3.png', 56),
+('neutron/purple/1.png', 57),
+('neutron/purple/2.png', 58),
+('neutron/purple/3.png', 59),
+('neutron/pink/1.png', 60),
+('neutron/pink/2.png', 61),
+('neutron/pink/3.png', 62),
+('neutron/orange/1.png', 63),
+('neutron/orange/2.png', 64),
+('neutron/orange/3.png', 65),
+('neutron/green/1.png', 66),
+('neutron/green/2.png', 67),
+('neutron/green/3.png', 68),
+('neutron/dark_blue/1.png', 69),
+('neutron/dark_blue/2.png', 70),
+('neutron/dark_blue/3.png', 71),
+('neutron/cyan/1.png', 72),
+('neutron/cyan/2.png', 73),
+('neutron/cyan/3.png', 74),
+
+#calibration
+('test1.png', 255),
+]
