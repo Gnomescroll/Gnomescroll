a95d7c424d520fd339c5350070bb11643620efbd
Steve Leonard <sleonard76@gmail.com>
2011-09-05 12:14:31 -0400

    remove old sound files

--- pyglet_client/netclient/sound/player_sounds.h
+++
@@ -1,8 +0,0 @@
-#include "sound.h"
-
-FMOD_SOUND* player_gunshot;
-FMOD_CHANNEL* player_gunshot_channel;
-
-void load_player_gunshot();
-void play_player_gunshot();
-void end_player_gunshot();
--- pyglet_client/netclient/sound/projectile_sounds.h
+++
@@ -1,7 +0,0 @@
-#include "sound.h"
-
-void load_projectile();
-void end_projectile();
-
-int play_projectile_sound(float x, float y, float z, float vx, float vy, float vz);
-int update_projectile_sound(int pid, float x, float y, float z, float vx, float vy, float vz);
