96269fec52fe3da73c635ac1d3ef3cb86fa151ee
Steve Leonard <sleonard76@gmail.com>
2011-09-06 14:36:05 -0400

    .

--- pyglet_client/server/c_lib/c_lib.h
+++ pyglet_client/server/c_lib/c_lib.h
@@ -1,15 +1,13 @@
 #ifndef c_lib_h
 #define c_lib_h
 
-#include "compat.h"
-#ifdef DC_CLIENT
-#include "compat_gl.h"
-#endif
-
-int init_c_lib();
-#ifdef DC_CLIENT
-GLint get_particle_sheet_id();
-#endif
-//extern GLint particle_sheet_id;
+    #include "compat.h"
+    #ifdef DC_CLIENT
+        #include "compat_gl.h"
+        GLint get_particle_sheet_id();
+        //extern GLint particle_sheet_id;
+    #endif
+    
+    int init_c_lib();
 
 #endif
