02a008848d29ca66a6372a4f9998fccca707651c
HaltingState <haltingstate@gmail.com>
2011-09-10 08:13:22 -0400

    ok

---
+++ png_tools/block_sprite_conf.py
@@ -0,0 +1,18 @@
+'''
+(relative file path, position in spritesheet (0-indexed))
+'''
+
+sprites = [
+#('star1.png', 0),
+
+#grass block
+('grass-side.png', 1),
+('grass-top.png', 2),
+('dirt.png', 3),
+#stone
+('stone.png', 4),
+
+('purplemoon.png', 5),
+('bluemoon.png', 6),
+
+]
