e0dd936284c090b458f72aa0c233ede59ff628c1
HaltingState <haltingstate@gmail.com>
2011-09-10 10:28:14 -0400

    try now

--- netclient/cube_lib/t_vbo.c
+++ netclient/cube_lib/t_vbo.c
@@ -75,9 +75,10 @@ int _init_draw_terrain() {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE);
+    //glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE);
 
-    glTexImage2D(GL_TEXTURE_2D, 0, 4, surface->w, surface->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, surface->pixels );
+    //Uint32 GL_PIXEL_TYPE = GL_BGR;
+    glTexImage2D(GL_TEXTURE_2D, 0, 4, surface->w, surface->h, 0, GL_BGRA, GL_UNSIGNED_BYTE, surface->pixels );
     glDisable(GL_TEXTURE_2D);
     }
 }
