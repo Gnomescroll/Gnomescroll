b1d182513c3eb960e1247e803ecbf1b755cff5a1
HaltingState <haltingstate@gmail.com>
2011-09-10 08:49:04 -0400

    added files

---
+++ png_tools/block_gen/block_dat.py
@@ -0,0 +1,68 @@
+dat = {
+    0 : {
+        'name': 'tile0',
+        'occludes' : False,
+        'active' : False,
+        'solid' : False,
+        'max_damage': 32,
+        'neutron_tolerance' : 2,
+        'nuclear': 0,
+        'texture_id': [-1]*6,
+        'texture_order': [[0,1,2,3]] * 6,
+        'gravity'   :   0,
+        'transparent': 0,
+    },
+
+    2 : {
+            'name'  :   'tile2',
+            'occludes': True,
+            'active':   True,
+            'solid':    True,
+            'max_damage' : 32,
+            'neutron_tolerance' : 2,
+            'nuclear' : 0,
+            'texture_id': [1]*6,
+            'gravity'   :   0,
+            'transparent': 0,
+    },
+
+    3 : {
+            'name'  :   'tile2',
+            'occludes': True,
+            'active':   True,
+            'solid':    True,
+            'max_damage' : 32,
+            'neutron_tolerance' : 2,
+            'nuclear' : 0,
+            'texture_id': [2]*6,
+            'gravity'   :   0,
+            'transparent': 0,
+    },
+
+    4 : {
+            'name'  :   'tile2',
+            'occludes': True,
+            'active':   True,
+            'solid':    True,
+            'max_damage' : 32,
+            'neutron_tolerance' : 2,
+            'nuclear' : 0,
+            'texture_id': [3]*6,
+            'gravity'   :   0,
+            'transparent': 0,
+    },
+
+    5 : {
+            'name'  :   'tile2',
+            'occludes': True,
+            'active':   True,
+            'solid':    True,
+            'max_damage' : 32,
+            'neutron_tolerance' : 2,
+            'nuclear' : 0,
+            'texture_id': [4]*6,
+            'gravity'   :   0,
+            'transparent': 0,
+    },
+
+}
---
+++ png_tools/block_gen/gen_block_dat.py
@@ -0,0 +1,10 @@
+from block_dat import dat
+
+
+import pprint
+
+
+f = open("../../server/dats/cube_dat.py", "w")
+pp = pprint.PrettyPrinter(stream=f, indent=4)
+
+pp.pprint(dat)
