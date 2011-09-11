de1df29469649fd6dcfb9e11fefa1d6a5477ed65
Steve Leonard <sleonard76@gmail.com>
2011-09-11 18:25:40 -0400

    more _ray_Tracepyx deprecation

--- server/c_lib/_ray_trace.pyx
+++ server/c_lib/_ray_trace.pyx
@@ -1,7 +1,7 @@
 
-cdef extern from "ray_trace/ray_trace.h":
-    int ray_cast(float x0,float y0,float z0, float x1,float y1,float z1)
-    int ray_cast_lax(float x0,float y0,float z0, float x1,float y1,float z1)
+#cdef extern from "ray_trace/ray_trace.h":
+#    int ray_cast(float x0,float y0,float z0, float x1,float y1,float z1)
+#    int ray_cast_lax(float x0,float y0,float z0, float x1,float y1,float z1)
 #    int* _ray_cast3(float x0,float y0,float z0, float x1,float y1,float z1, float* distance)
 
 def ray_cast3(float x0,float y0,float z0, float x1,float y1,float z1):
