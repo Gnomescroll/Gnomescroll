e5a997d1f0c9aabdf43a6cfd34d0b7c74ae7171a
HaltingState <haltingstate@gmail.com>
2011-09-13 11:36:42 -0400

    creating agent netork stuff

--- netclient/c_lib/agent/agent2.c
+++ netclient/c_lib/agent/agent2.c
@@ -3,7 +3,6 @@
 struct Agent_cs { //control state
     int tick;
     int seq;
-    float x,y,z;
     float theta,phi;
     int cs[6]; //movement, jetpack
 };
@@ -11,9 +10,9 @@ struct Agent_cs { //control state
 struct Agent_snapshot {
     //int id; //ignore on client
     int tick;
-    float theta,phi;
-
-    struct Agent_ick
+    float x,y,z;
+    float vx,vy,vz;
+    //float theta,phi;
 }
 
 struct Agent {
@@ -28,12 +27,12 @@ struct Agent {
     int tbn;
     struct Agent_cs t_buffer[128];
 
-    int sbn;
+    int sbn; //index to last snap shot
     struct Agent_snapshot s_buffer[8];
 
 };
 
-void create_agent(int id, struct Agent_snapshot* as) {
+struct Agent* create_agent(int id, struct Agent_snapshot* as) {
 
     struct Agent* a = malloc(sizeof(struct Agent));
     a.id = id;
@@ -42,7 +41,43 @@ void create_agent(int id, struct Agent_snapshot* as) {
 
     int i;
     for(i=0;i<8;i++) {
-        a->s_buffer[i].
+        a->s_buffer[i].tick = -1;
+    }
+    for(i=0;i<128;i++) {
+        a->t_buffer[i].tick = -1;
+        a->t_buffer[i].seq = -1;
     }
 
+    a->x = as->x;
+    a->y = as->y;
+    a->z = as->z;
+    a->vx = as->vx;
+    a->vy = as->vy;
+    a->vz = as->vz;
+
+    return a;
+}
+
+void apply_control_state(int id, struct Agent_cs* cs ) {
+    //get agent
+    a->t_buffer[cs->tick % 128] = *cs;
+}
+
+void apply_agent_snapshot(int id, struct Agent_snapshot* as) {
+    //get agent
+    if(a->s_buffer[a->sbn].tick < as->tick]) {
+        a->sbn = (a->sbn+1)%8;
+        s_buffer[a->sbn] = *as;
+
+        int i;
+        for(i=0; i<128; i++) {
+
+            if(a->t_buffer[i].tick < s_buffer[a->sbn]) {
+                a->t_buffer[i] = -1; //wipe control state before current snapshot
+            }
+        }
+        printf("Processed Agent Snapshot: agent_id=%i, tick=%i\n", a-id, as->tick);
+    } else {
+        printf("Old snapshot received: tick=%i, newest snapshot is tick=%i\n",as->tick ,a->s_buffer[a->sbn].tick);
+    }
 }
