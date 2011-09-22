016b2cea2d896c4918f7d1faaf9b807a275a3cdd
HaltingState <haltingstate@gmail.com>
2011-09-22 02:40:10 -0400

    agent addremove

--- netclient/c_lib/agent/agent.hpp
+++ netclient/c_lib/agent/agent.hpp
@@ -2,7 +2,8 @@
 
 	
 class Agent_list {
-
+	private:
+		int id_c;
     public:
         int num
         Agent_state* a[1024];
@@ -19,6 +20,7 @@ class Agent_list {
 
 Agent_list::Agent_list(){
 	num = 0;
+	id_c = 0;
 	int i;
 	for(i=0;i<1024;i++) a[i] = NULL;
 }
@@ -32,17 +34,35 @@ Agent_state* Agent_list::get(int id) {
 		printf("agent get error: agent is null, id=%i\n", id);
 		return NULL;
 	}
+	return a[i];
 }
 
-Agent_state* Agent_list::new_agent(int i) {
-	
-
-
+Agent_state* Agent_list::new_agent() {
+	int i;
+	int id = id_c;
+	id_c++;
+	for(i=0; i<1024;i++) {
+		id = (i+id)%1024
+		if(a[id] == NULL) break;
+	}
+	if(i==1024) {
+		printf("Error: cannot create agent, agent limit exceeded\n");
+		return NULL;
+	}
+	num++;
+	a[id] = new Agent_state(id);
+	printf("Created agent %i\n", id);
 }
 
-void Agent_list::delete_agent() {
-	
-	printf("Agent_list::delete_agent not implemented\n");
+void Agent_list::delete_agent(int id) {
+	if(a[i]==NULL) {
+		printf("Cannot delete agent: agent is null\n");
+	}
+	delete a[i];
+	a[i] == NULL;
+	num--;
+	printf("Deleted agent %i\n", id);
+	//printf("Agent_list::delete_agent not implemented\n");
 }
 
 void Agent_list::draw() {
