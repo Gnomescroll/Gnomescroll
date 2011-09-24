436397b76c1cbe6e5c5fec09207f95fa5f54c47d
HaltingState <haltingstate@gmail.com>
2011-09-24 01:39:04 -0400

    compiles

--- netclient/c_lib/agent/net_agent.cpp
+++ netclient/c_lib/agent/net_agent.cpp
@@ -8,52 +8,65 @@
 #include <net_lib/server/server.h>
 
 //0 for unpack, 1 for pack
-static inline float pack_float(float* x, unsigned char* buff, int*buff_n, bool pack)
+
+static inline void pack_message_id(int message_id, unsigned char* buff, int*buff_n, bool pack) {
+    if(pack == true) *((uint8_t*)(buff+*buff_n)) = (uint8_t)message_id;
+    //if(pack == false); //do nothing
+    *buff_n += sizeof(uint8_t);
+}
+
+static inline void pack_float(float* x, unsigned char* buff, int*buff_n, bool pack)
 {
-    if(pack == true) *((float*)(buffer+*buff_n)) = *x;
+    if(pack == true) *((float*)(buff+*buff_n)) = *x;
     if(pack == false) *x = *((float*)(buff+*buff_n));
     *buff_n += sizeof(float);
 }
 
-static inline void pack_32(int32_t* x, unsigned char* buff, int* buff_n, bool pack)
+static inline void pack_32(int* x, unsigned char* buff, int* buff_n, bool pack)
 {
-    if(pack == true)    *((int32_t*)(buffer+*buff_n)) = *x;
+    if(pack == true)    *((int32_t*)(buff+*buff_n)) = *x;
     if(pack == false)   *x = *((int32_t*)(buff+*buff_n));
     *buff_n += sizeof(int32_t);
 }
 
-static inline void pack_u32(uint32_t* x, unsigned char* buff, int* n, bool pack)
+static inline void pack_u32(int* x, unsigned char* buff, int* buff_n, bool pack)
 {
-    if(pack == true)    *((uint8_t*)(buffer+*buff_n)) = *x;
+    if(pack == true)    *((uint8_t*)(buff+*buff_n)) = *x;
     if(pack == false)   *x = *((uint32_t*)(buff+*buff_n));
     *buff_n += sizeof(uint32_t);
 }
 
-static inline void pack_16(int16_t* x, unsigned char* buff, int* buff_n, bool pack)
+static inline void pack_u32_t(uint32_t* x, unsigned char* buff, int* buff_n, bool pack)
 {
-    if(pack == true)    *((int16_t*)(buffer+*buff_n)) = *x; 
+    if(pack == true)    *((uint8_t*)(buff+*buff_n)) = *x;
+    if(pack == false)   *x = *((uint32_t*)(buff+*buff_n));
+    *buff_n += sizeof(uint32_t);
+}
+
+static inline void pack_16(int* x, unsigned char* buff, int* buff_n, bool pack)
+{
+    if(pack == true)    *((int16_t*)(buff+*buff_n)) = *x; 
     if(pack == false)   *x = *((int16_t*)(buff+*buff_n));
     *buff_n += sizeof(int16_t);
 }
 
-
-static inline void pack_u16(uint16_t* x, unsigned char* buff, int* buff_n, bool pack)
+static inline void pack_u16(int* x, unsigned char* buff, int* buff_n, bool pack)
 {
-    if(pack == true)    *((uint16_t*)(buffer+*buff_n)) = *x; 
+    if(pack == true)    *((uint16_t*)(buff+*buff_n)) = *x; 
     if(pack == false)   *x = *((uint16_t*)(buff+*buff_n));
     *buff_n += sizeof(uint16_t);
 }
 
-static inline void pack_8(int8_t* x, unsigned char* buff, int* buff_n, bool pack)
+static inline void pack_8(int* x, unsigned char* buff, int* buff_n, bool pack)
 {
-    if(pack == true)    *((int8_t*)(buffer+*buff_n)) = *x; 
+    if(pack == true)    *((int8_t*)(buff+*buff_n)) = *x; 
     if(pack == false)   *x = *((int8_t*)(buff+*buff_n));
     *buff_n += sizeof(int8_t);
 }
 
-static inline void pack_u8(uint8_t* x, unsigned char* buff, int* buff_n, bool pack)
+static inline void pack_u8(int* x, unsigned char* buff, int* buff_n, bool pack)
 {
-    if(pack == true)    *((uint8_t*)(buffer+*buff_n)) = *x; 
+    if(pack == true)    *((uint8_t*)(buff+*buff_n)) = *x; 
     if(pack == false)   *x = *((uint8_t*)(buff+*buff_n));
     *buff_n += sizeof(uint8_t);
 }
@@ -61,13 +74,81 @@ static inline void pack_u8(uint8_t* x, unsigned char* buff, int* buff_n, bool pa
 
 typedef void (*pt2handler)(unsigned char*, int, int*);
 
+class FixedSizeNetPacketToServer {
+    public:
+
+        void serialize(unsigned char* buff, int* buff_n, int* size) {
+            int _buff_n = *buff_n;
+            packet(buff, buff_n, true);
+            *size = *buff_n - _buff_n;
+        };
+        void unserialize(unsigned char* buff, int* buff_n, int* size){
+            int _buff_n = *buff_n;
+            packet(buff, buff_n, false);
+            *size = *buff_n - _buff_n;
+        }
+        
+        virtual inline void packet(unsigned char* buff, int* buff_n, bool pack) = 0;
+
+        void send() {
+            unsigned char* buff= NetClient::get_client_out_buffer();
+            int* buff_n = NetClient::get_client_out_buffer_n();
+            if(*buff_n > 800) { printf("Cannot send message: output buffer is full! %i bytes\n", *buff_n); return; }
+
+            int bytes_written;
+            serialize(buff, buff_n, &bytes_written);
+        }
+
+        void register_handler(pt2handler* handler) {
+            return; //implement!
+        }
+
+};
+
+class FixedSizeNetPacketToClient {
+    public:
+        void serialize(unsigned char* buff, int* buff_n, int* size) {
+            int _buff_n = *buff_n;
+            packet(buff, buff_n, true);
+            *size = *buff_n - _buff_n;
+        };
+        void unserialize(unsigned char* buff, int* buff_n, int* size){
+            int _buff_n = *buff_n;
+            packet(buff, buff_n, false);
+            *size = *buff_n - _buff_n;
+        }
+        virtual inline void packet(unsigned char* buff, int* buff_n, bool pack) = 0;
+
+        void sendToClient(int client_id) {
+            unsigned char buff[64]; //max message size
+            int buff_n = 0;
+            int size;
+            serialize(buff, &buff_n, &size);
+            push_message(client_id, buff, size);
+        }
+
+        void broadcast() {
+            unsigned char buff[64]; //max message size
+            int buff_n = 0;
+            int size;
+            serialize(buff, &buff_n, &size);
+            push_broadcast_message(buff, size);
+        }
+};
+/*
+EXAMPLE
+*/
+
+
+
 //helper class
+
 class ClientToServer {
     public:
         void send();
         void register_handler(pt2handler* handler);
-        //virtual void serialize(unsigned char* buff, int* buff_n, int* size){};
-        virtual void serialize(unsigned char* buff, int* buff_n, int* size) = 0;
+        virtual void serialize(unsigned char* buff, int* buff_n, int* size){};
+        //virtual void serialize(unsigned char* buff, int* buff_n, int* size) = 0;
 };
 
 /*
@@ -76,6 +157,8 @@ void ClientToServer::serialize(unsigned char* buff, int* buff_n, int* size) {
     return;
 }
 */
+
+
 void ClientToServer::send() {
     unsigned char* buff= NetClient::get_client_out_buffer();
     int* buff_n = NetClient::get_client_out_buffer_n();
@@ -136,6 +219,34 @@ void handle_agent_control_state_message(unsigned char* buff, int buff_n, int* re
 }
 */
 
+
+class Agent_control_state_message: public FixedSizeNetPacketToServer
+{
+    public:
+        static const int message_id = 3;
+        static const int size = 2*sizeof(uint8_t) + 2*sizeof(uint16_t) + 3*sizeof(uint32_t);
+
+        int id;
+        int seq;
+        uint32_t cs;
+        int tick;
+        int ctick;
+        float theta;
+        float phi;
+
+        inline void packet(unsigned char* buff, int* buff_n, bool pack) 
+        {
+            pack_message_id(message_id, buff, buff_n, pack);
+            pack_u16(&id, buff, buff_n, pack);
+            pack_u8(&seq, buff, buff_n, pack);
+            pack_16(&tick, buff, buff_n, pack);
+            pack_u32_t(&cs, buff, buff_n, pack);
+            pack_float(&theta, buff, buff_n, pack);
+            pack_float(&phi, buff, buff_n, pack);
+        }
+};
+
+/*
 class Agent_control_state_message: public ClientToServer 
 {
     public:
@@ -153,9 +264,11 @@ class Agent_control_state_message: public ClientToServer
         void serialize(unsigned char* buff, int* buff_n, int* bytes_written);
         void deserialize(unsigned char* buff, int buff_n, int* read_bytes);
 };
+*/
 
-void Agent_control_state_message::serialize(unsigned char* buff, int* buff_n, int* bytes_written) {
 /*
+void Agent_control_state_message::serialize(unsigned char* buff, int* buff_n, int* bytes_written) {
+
     unsigned char* buff= NetClient::get_client_out_buffer();
     int* buff_n = NetClient::get_client_out_buffer_n();
 
@@ -163,7 +276,7 @@ void Agent_control_state_message::serialize(unsigned char* buff, int* buff_n, in
         printf("Cannot send message: output buffer is full! %i bytes\n", *buff_n);
         return;
     }
-*/
+
     int _buff_n = *buff_n;
     PACK_uint8_t(this->message_id, buff, buff_n);  //push message id on stack
     PACK_uint16_t(id, buff, buff_n); //agent id
@@ -187,6 +300,7 @@ void Agent_control_state_message::deserialize(unsigned char* buff, int buff_n, i
     *read_bytes = buff_n - _buff_n;
 }
 
+*/
 /*
     agent state message
 */
