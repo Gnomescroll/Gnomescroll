c6d755ed9802853a26085319c226bbfe477874a7
Steve Leonard <sleonard76@gmail.com>
2011-10-06 18:45:05 -0400

    new noise

--- server/run.py
+++ server/run.py
@@ -45,22 +45,27 @@ import time
 _n = time.time()
 
 
-#c_lib.map_gen.conf.seed(666)\
-                  #.size(128,128,128)\
-                  #.interpolate(4,4,2)\
-                  #.p3()\
-                  #.start()
+c_lib.map_gen.conf.seed(571)\
+                  .size(128,128,128)\
+                  .interpolate(4,4,2)\
+                  .p3()\
+                  .start()
                   #.gradient(z0=-0.5, z1=0.5)\
 
+
+c_lib.map_gen.reset()
                   #.interpolate(4,2,1)\
-c_lib.map_gen.conf.seed(666)\
+#c_lib.map_gen.conf.seed(666)\
+c_lib.map_gen.conf.reset()\
                   .size(128,128,128)\
                   .gradient(z0=0, z1=1)\
-                  .heightmap(baseline=1, maxheight=127)\
+                  .heightmap(baseline=3, maxheight=10)\
                   .start()
                   #.rmf()\
                   #.p2()\
-                  
+
+#c_lib.map_gen.conf.floor().start()
+
 print 'map gen took %0.2f seconds' % (time.time() - _n) # 71 seconds!
 
 def pallet_pillar(x,y,z):
