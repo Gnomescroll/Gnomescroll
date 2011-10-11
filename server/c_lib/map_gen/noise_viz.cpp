1f374e458606a0d4046089441bf9bed6bf1178a4
HaltingState <haltingstate@gmail.com>
2011-10-11 06:25:35 -0400

    stuff

--- server/c_lib/map_gen/noise_viz.cpp
+++ server/c_lib/map_gen/noise_viz.cpp
@@ -1,41 +1,55 @@
 
+//srand((unsigned)time(0));
 
-int bins[1000];
+static const int num_bins = 1000;
+static int bin[num_bins];
 
-float bin_min = -0.2;
-float bin_inc = 0.001;
+const float bin_min = 0;
+const float bin_inc = 0.001;
+
+float bin_scale;
 
 static int less_than_min = 0;
 static int greater_than_max = 0;
 
+void test_setup() {
+    
+    int i;
+    float test[2000];
+    for(i=0; i< 2000; i++) {
+    test[i] = (float)rand()/(float)RAND_MAX;
+    }
+}
 inline void bin_float(float x) {
     if(x < bin_min) {
         less_than_min++;
         return;
     }
-    int bin = x / bin_inc;
-    if(bin > 1000){
+    int index = (x-bin_min) / bin_inc;
+    if(index > num_bins){
         greater_than_max++;
         return;    
     }
-    bins[bin]++;
+    bin[index]++;
 }
 
 void setup_bin(float* arr, int n) {
     printf("Starting Binning\n");
     int i;
-    for(i=0; i<1000;i++) bins[i] = 0;
+    for(i=0; i<1000;i++) bin[i] = 0;
     double average = 0; 
     for(i=0; i<n; i++) {
         average += arr[n];
         bin_float(arr[n]);
     }
     average = average / (double)(n);
+    printf("number of samples= %i\n", n);
     printf("average= %d \n", average);
     printf("less_than_min= %i \n",less_than_min);
     printf("greater_than_max= %i \n", greater_than_max);
     printf("finished binning\n");
 
+    bin_scale = ((float)(n))/1000;
 }
 
 #ifdef DC_CLIENT
@@ -43,22 +57,7 @@ void bin_histrogram_draw(float x, float y, float z) {
     //printf("po=%i\n", PO_L);
     x -= 10;
     y += 10;
-    int i,j;
-    for(i=0;i<num_bins;i++) bin[i]=0;
-    int ft;
-    for(i=0; i<PO_L; i++) {
-        if(packet_out_array[i].ack_frame == -1) continue;
-        ft = packet_out_array[i].ack_time - packet_out_array[i].send_time;
-        //printf("ft=%i\n", ft);
-        ft = ft / bin_size;
-
-        if(ft <= 0 || ft >= num_bins) {
-            //printf("ft error: ft= %i\n", ft);
-            continue;
-        }
-        bin[ft]++;
-
-    }
+    int i;
 
     glBegin(GL_POINTS);
     glColor3ub((unsigned char) 0,(unsigned char)0,(unsigned char)255);
@@ -69,7 +68,7 @@ void bin_histrogram_draw(float x, float y, float z) {
         }
     }
 
-    glColor3ub((unsigned char) 00,(unsigned char)0,(unsigned char)200);
+    glColor3ub((unsigned char) 200,(unsigned char)0,(unsigned char)0);
     for(i=0; i<8; i++) {
         glVertex3f(_C+x-2,_C+y+2*10*i,z);
         glVertex3f(_C+x-3,_C+y+2*10*i,z);
