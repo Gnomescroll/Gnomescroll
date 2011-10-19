75c4ad6f9d2bfcc8eab464af73767e9a1a398f7f
HaltingState <haltingstate@gmail.com>
2011-10-18 22:29:07 -0400

    fixed movement

--- netclient/c_lib/agent/agent.cpp
+++ netclient/c_lib/agent/agent.cpp
@@ -10,32 +10,6 @@
 
 #include <c_lib/agent/agent_draw.hpp>
 
-void Agent_state::handle_state_snapshot(int seq, float theta, float phi, float x,float y,float z, float vx,float vy,float vz) {
-    state_snapshot.seq = seq;
-    state_snapshot.theta = theta;
-    state_snapshot.phi = phi;
-    state_snapshot.x=x;state_snapshot.y=y;state_snapshot.z=z;
-    state_snapshot.vx=vx;state_snapshot.vy=vy;state_snapshot.vz=vz;
-
-    //do this fast way
-    
-    int i;
-    for(i=0;i<128;i++){
-        if(cs[i].seq < seq || cs[i].seq > seq+60) cs[i].seq = -1; //clear any control state not from next 60 ticks
-    }
-    
-    state_rollback = state_snapshot; //when new snapshot comes, in, set rollbacks
-    cs_window_min = seq;
-    //printf("handle_state_snapshot: seq= %i, cs_seq= %i \n", seq, cs_seq);
-    cs_seq = seq;
-
-    s = state_snapshot;
-
-    #ifdef DC_CLIENT
-    AgentDraw::add_snapshot_to_history(this);
-    #endif
-    _tick();
-}
 
 
 void Agent_list::draw() 
@@ -61,6 +35,81 @@ void Agent_list::draw()
 	#endif
 }
 
+void Agent_state::handle_control_state(int _seq, int _cs, float _theta, float _phi) {
+    //printf("control state received: seq=%i, cs=%i \n", _seq, _cs);
+    int index = _seq%128;
+
+    cs[index].seq = _seq;
+    cs[index].cs = _cs;
+    cs[index].theta = _theta;
+    cs[index].phi = _phi;
+
+    printf("cs_seq= %i, _seq= %i \n", cs_seq, _seq);
+
+    #ifdef DC_SERVER
+        //if(tick_n % 30 == 0) {
+        //if(cs_seq % 32 == 0) {
+        if( _seq % 32 == 0 ) {
+            Agent_state_message A;
+
+            A.id = id;
+            A.seq = cs_seq;
+
+            A.x = s.x;
+            A.y = s.y;
+            A.z = s.z;
+            A.vx = s.vz;
+            A.vy = s.vy;
+            A.vz = s.vz;
+            A.broadcast();
+
+            //clean out old control state
+            int i;
+            int index;
+            for(i=16;i<96;i++){
+                index = (_seq + i)%128;
+                cs[index].seq = -1;
+            }
+        }
+    #endif
+    //printf("control state= %i\n", new_control_state);
+
+    _tick();
+}
+
+void Agent_state::handle_state_snapshot(int seq, float theta, float phi, float x,float y,float z, float vx,float vy,float vz) {
+    state_snapshot.seq = seq;
+    state_snapshot.theta = theta;
+    state_snapshot.phi = phi;
+    state_snapshot.x=x;state_snapshot.y=y;state_snapshot.z=z;
+    state_snapshot.vx=vx;state_snapshot.vy=vy;state_snapshot.vz=vz;
+
+    //do this fast way
+    
+//    int i;
+//    for(i=0;i<128;i++){
+//        if(cs[i].seq < seq || cs[i].seq > seq+60) cs[i].seq = -1; //clear any control state not from next 60 ticks
+//    }
+
+    int i;
+    int index;
+    for(i=16;i<96;i++){
+        index = (seq + i)%128;
+        cs[index].seq = -1;
+    }
+                
+    state_rollback = state_snapshot; //when new snapshot comes, in, set rollbacks
+    cs_window_min = seq;
+    //printf("handle_state_snapshot: seq= %i, cs_seq= %i \n", seq, cs_seq);
+    cs_seq = seq;
+
+    s = state_snapshot;
+
+    #ifdef DC_CLIENT
+    AgentDraw::add_snapshot_to_history(this);
+    #endif
+    _tick();
+}
 
 Agent_state::Agent_state(int _id) {
     id = _id;
@@ -97,50 +146,6 @@ void Agent_state::client_tick() {
 	}
 
 void Agent_state::server_tick() {
-
-    if(_new_control_state == 1) {
-		//_tick();	//advance agent
-		//tick on input received
-    }
-
-	tick_n++;
-	//if(tick_n % 30 == 0) {
-	if(cs_seq % 32 == 0) {
-        Agent_state_message A;
-    /*
-        int id;
-        int seq;
-        int tick;
-
-        float x;
-        float y;
-        float z;
-        float vx,vy,vz;
-    */
-        A.id = id;
-        A.seq = cs_seq;
-
-        A.x = s.x;
-        A.y = s.y;
-        A.z = s.z;
-        A.vx = s.vz;
-        A.vy = s.vy;
-        A.vz = s.vz;
-        A.broadcast();
-
-
-        //clean out old control state
-        int i;
-        for(i=0;i<128;i++){
-            if(cs[i].seq < cs_seq || cs[i].seq > cs_seq+60) {
-	        	if(cs[i].seq == cs_seq) printf("!!! Error 1 \n");
-	        	if(cs[i].seq == (cs_seq+1)%128) printf("!!! Error 2 \n");
-	        	if(cs[i].seq == (cs_seq+25)%128) printf("!!! Error 3 \n");
-	        	if(cs[i].seq == (cs_seq+55)%128) printf("!!! Error 4 \n");
-	            cs[i].seq = -1; //clear any control state not from next 60 ticks
-    		}    
-		}
-	}
 
     return;
 }
