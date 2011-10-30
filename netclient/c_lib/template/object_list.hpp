569581a6c5eba64ad1119384e666814339f6035f
Steve Leonard <sleonard76@gmail.com>
2011-10-30 10:37:50 -0400

    print object list address instantiation

--- netclient/c_lib/template/object_list.hpp
+++ netclient/c_lib/template/object_list.hpp
@@ -43,7 +43,7 @@ Object_list<Object_state, max_n>::Object_list()
     id_c = 0;
     int i;
     for(i=0;i<max_n;i++) a[i] = NULL;
-    printf("%s list instantiated\n", name());
+    printf("%s list instantiated at %p\n", name(), this);
     //where();
 }
 
