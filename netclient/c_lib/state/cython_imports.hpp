eb7e375ced0261a9d70392cacc63ed3045028e64
Steve Leonard <sleonard76@gmail.com>
2011-10-30 09:36:00 -0400

    singleton state lists work

--- netclient/c_lib/state/cython_imports.hpp
+++ netclient/c_lib/state/cython_imports.hpp
@@ -5,12 +5,18 @@
 
 #ifdef DC_SERVER
     #define STATE ServerState
-    #include <c_lib/state/server_state.hpp>
+    //extern namespace ServerState;
 #else
     #define STATE ClientState
-    #include <c_lib/state/client_state.hpp>
+    //extern namespace ClientState;
 #endif
 
-extern Agent_list STATE::agent_list;
-extern Cspray_list STATE::cspray_list;
+namespace STATE {
+    extern Agent_list agent_list;
+    extern Cspray_list cspray_list;
+
+    extern void set_control_state(int f, int b, int l, int r, int jet, int jump, float theta, float phi);
+    extern void set_PlayerAgent_id(int id);
+
+}
 
