a8f26860149cb9c26c724dbc57a101e623e52fb1
Steve Leonard <sleonard76@gmail.com>
2011-11-06 02:59:16 -0800

    text init

--- netclient/c_lib/c_lib.cpp
+++ netclient/c_lib/c_lib.cpp
@@ -2,23 +2,29 @@
     #define DC_CLIENT
 #endif
 
-#include <c_lib.hpp>
-
 /* kill the compiler */
 #ifdef DC_SERVER 
     sasfsafgdg4423+3
 #endif
 
-/* kill the compiler */
-#ifndef DC_CLIENT
- asdfsfs;fgf;
-#endif
+#include <c_lib.hpp>
+
+#include <defines.h>
+
+/* Vectors, Ray Tracers, Physics */
+#include <c_lib/physics/vector.c>
+#include <c_lib/ray_trace/ray_trace.c>
+#include <c_lib/physics/common.c>
 
 /* objects */
 #include <c_lib/objects/object_lib.cpp>
 
-/* animations */
-#include <c_lib/animations/animations.cpp>
+/* Network */
+#include <net_lib/net_lib.h>
+
+/* Agents */
+#include <c_lib/agent/agent_include.h>
+
 
 /* map */
 #include <c_lib/t_map/t_map.c>
@@ -26,38 +32,17 @@
 #ifdef DC_CLIENT
     #include <c_lib/t_map/t_viz.c>
     #include <c_lib/t_map/t_vbo.c>
-#endif
 
-/* input */
-#ifdef DC_CLIENT
-    #include <c_lib/input/input.cpp>
-#endif
+    /* animations */
+    #include <c_lib/animations/animations.cpp>
 
-/* camera */
-/*
-#ifdef DC_CLIENT
-#include <c_lib/camera/camera.c>
-#endif
-*/
-
-/* Vectors, Ray Tracers, Physics */
-#include <c_lib/physics/vector.c>
-#include <c_lib/ray_trace/ray_trace.c>
-#include <c_lib/physics/common.c>
-
-/* Agents */
-#include <c_lib/agent/agent_include.h>
-
-/* HUD */
-#ifdef DC_CLIENT
+    /* HUD */
     #include <c_lib/hud/cube_select.cpp>
     #include <c_lib/hud/inventory.cpp>
     #include <c_lib/texture_loader.c>
     #include <c_lib/hud/hud_texture_loader.cpp>
-#endif
 
-/* SDL */
-#ifdef DC_CLIENT
+    /* SDL */
     #include <c_lib/SDL/camera.c>
     #include <c_lib/SDL/draw_functions.c>
     #include <c_lib/SDL/particle_functions.c>
@@ -65,10 +50,16 @@
     #include <c_lib/SDL/SDL_text.c>
     #include <c_lib/SDL/texture_loader.c>
     #include <c_lib/SDL/block_selector.c>
-#endif
 
-/* Network */
-#include <net_lib/net_lib.h>
+    /* input */
+    #include <c_lib/input/input.cpp>
+
+    /* camera */
+    /*
+    #include <c_lib/camera/camera.c>
+    */
+
+#endif
 
 int init_c_lib() {
     printf("init c_lib\n");
@@ -78,6 +69,7 @@ int init_c_lib() {
         init_texture_loader();
         init_hud_texture_loader();
         _init_image_loader();
+        _init_text();
     #endif
 return 0;
 }
--- netclient/c_lib/c_lib_sdl.pyx
+++ netclient/c_lib/c_lib_sdl.pyx
@@ -215,8 +215,7 @@ cdef extern from './SDL/SDL_text.h':
     SDL_Surface* _create_text_surface(char* text,r,g,b,a)
     int _draw_text_surface(SDL_Surface* surface, int x, int y)
     int _free_text_surface(SDL_Surface* surface)
-    int _draw_text(char* text, float x, float y, float height, float width, float depth)
-    int _draw_text2(char* text, float x, float y, float height, float width, float depth, int r, int g, int b, int a)
+    int _draw_text(char* text, float x, float y, float height, float width, float depth, int r, int g, int b, int a)
 
 cdef extern from './SDL/draw_functions.h':
     int _blit_sprite(int tex, float x0, float y0, float x1, float y1, float z)
@@ -224,12 +223,6 @@ cdef extern from './SDL/draw_functions.h':
 cdef extern from './SDL/block_selector.h':
     void _draw_block_selector(int x, int y)
     void _load_block_selector_texture(char *file, int scale)
-
-def init():
-    _init_text()
-
-def draw_text(text, x, y, height = 10., width = 10., depth = -.5):
-    _draw_text(text, x, y, height, width, depth)
 
 def create_text_surface(string):
     pass
@@ -247,40 +240,24 @@ def load_block_selector_texture(file, scale=1):
     _load_block_selector_texture(file, scale)
 
 class text:
-#    cdef float x, y
-#    cdef float height, width, depth
-#    cdef int r,g,b,a
-#    text = {}
 
     def __init__(self, text='', x=0, y=0, color=(255,255,255,255)):
         self.height = 10
         self.width = 10
         self.depth = -0.1
         if len(color) == 4:
-            self.r, self.g, self.b, self.a = color
+            self.color = color
         else:
-            self.r, self.g, self.b = color
-            self.a = 255
+            self.color = list(color)
+            self.color.append(255) # default alpha
+
         self.text = text
         self.x = x
         self.y = y
 
     def draw(self):
-        assert self.text != None
-        #_draw_text(self.text, self.x, self.y, self.height, self.width, self.depth)
-        _draw_text2(self.text, self.x, self.y, self.height, self.width, self.depth, self.r, self.g, self.b, self.a)
-
-    def _get_color(self):
-        return (self.r, self.g, self.b, self.a)
-    def _set_color(self, color):
-        if len(color) == 4:
-            self.r, self.g, self.b, self.a = color
-        else:
-            self.r, self.g, self.b = color
-    def _del_color(self):
-        del self.r, self.g, self.b, self.a
-
-    color = property(_get_color, _set_color, _del_color, "text color")
+        r,g,b,a  = self.color
+        _draw_text(self.text, self.x, self.y, self.height, self.width, self.depth, r,g,b,a)
 
 class reticle:
 
@@ -289,10 +266,8 @@ class reticle:
         self.y = y
         load_hud_texture(file)
 
-
     def draw(self):
         draw_loaded_hud_texture(self.x, self.y)
-
 
 class block_selector:
 
