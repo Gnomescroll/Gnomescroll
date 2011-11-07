75d8e93aba08d7f902f7371cd6e08faed47f6151
HaltingState <haltingstate@gmail.com>
2011-11-06 21:07:06 -0500

    infinite texture loading in terrain map

--- netclient/c_lib/t_map/t_properties.c
+++ netclient/c_lib/t_map/t_properties.c
@@ -2,6 +2,21 @@
 
 struct cubeProperties cube_list[max_cubes];
 
+short infinite_texture_array[1024];
+
+void set_infinite_texture(int id, int texture) {
+    if(id > 1024) {
+        printf("ERROR: more than 1024 infinite texture tiles, increase array size\n");
+    }
+    infinite_texture_array[id] = texture;
+    printf("id=%i texture= %i \n", id, texture);
+}
+
+void init_t_properties() {
+    int i;
+    for(i=0;i<max_cubes; i++) cube_list[i].infinite_texture = 0;
+}
+
 struct cubeProperties* _get_cube_list() {
     return cube_list;
 }
