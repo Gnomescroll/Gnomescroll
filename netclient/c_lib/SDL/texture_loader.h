1566a5dc3f2facb1edeb9a1dd48d23170e572b78
Steve Leonard <sleonard76@gmail.com>
2011-11-10 00:41:31 -0800

    eliminate SDL header dependency from c_lib_hud

--- netclient/c_lib/SDL/texture_loader.h
+++ netclient/c_lib/SDL/texture_loader.h
@@ -2,15 +2,16 @@
 
 #include <compat_gl.h>
 
-typedef struct {
+struct Texture {
     int tex;
     int w;
     int h;
-} Texture;
+};
 
 int init_image_loader();
 
 SDL_Surface* _load_image(char *file);
+struct Texture _load_image_create_texture(char *file);
 
 int create_texture_from_file(char* filename, int* tex);
 int create_texture_from_surface(SDL_Surface* surface, int* tex);
