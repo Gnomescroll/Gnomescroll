408bfe0c8186ebe9ce05597622486d33545c3026
Steve Leonard <sleonard76@gmail.com>
2011-11-11 02:40:14 +0000

    setup file

---
+++ windows_setup.sh
@@ -0,0 +1,25 @@
+
+
+#copies over stuff
+
+rm -rf ./server/c_lib/agent
+cp -rf ./netclient/c_lib/agent ./server/c_lib/agent
+
+
+rm -rf ./server/c_lib/particles
+cp -rf ./netclient/c_lib/particles ./server/c_lib/particles
+
+rm -rf ./server/c_lib/physics
+cp -rf ./netclient/c_lib/physics ./server/c_lib/physics
+
+rm -rf ./server/c_lib/ray_trace
+cp -rf ./netclient/c_lib/ray_trace ./server/c_lib/ray_trace
+
+rm -rf ./server/c_lib/state
+cp -rf ./netclient/c_lib/state ./server/c_lib/state
+
+rm -rf ./server/c_lib/template
+cp -rf ./netclient/c_lib/template ./server/c_lib/template
+
+rm -rf ./server/c_lib/time
+cp -rf ./netclient/c_lib/template ./server/c_lib/time
