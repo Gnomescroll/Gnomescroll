01cee010f88401b0dd3b9996f830cca24bbe45d2
HaltingState <haltingstate@gmail.com>
2011-11-16 05:00:30 -0500

    massive refactor

--- netclient/c_lib/agent/net_agent.cpp
+++ netclient/c_lib/agent/net_agent.cpp
@@ -18,155 +18,23 @@
 
 #include <c_lib/defines.h>
 
-//send at fixed interval, absolute position
-class Agent_state_message: public FixedSizeNetPacketToClient<Agent_state_message>
-{
-    public:
 
-        int id;
-        int seq;
-        int tick;
-
-        float x;
-        float y;
-        float z;
-        float vx,vy,vz;
-
-        float theta, phi;
-
-        inline void packet(unsigned char* buff, int* buff_n, bool pack) 
-        {
-            pack_u8(&id, buff, buff_n, pack);   //assume id is 1 byte
-            pack_u8(&seq, buff, buff_n, pack);
-            //pack_16(&tick, buff, buff_n, pack);
-
-            pack_float(&x, buff, buff_n, pack);
-            pack_float(&y, buff, buff_n, pack);
-            pack_float(&z, buff, buff_n, pack);
-            pack_float(&vx, buff, buff_n, pack);
-            pack_float(&vy, buff, buff_n, pack);
-            pack_float(&vz, buff, buff_n, pack);
-
-            pack_float(&theta, buff, buff_n, pack);
-            pack_float(&phi, buff, buff_n, pack);
-        }
-
-        inline void handle() {
-            Agent_state* A = STATE::agent_list.get(id);
-            if(A == NULL) {
-                printf("Agent does not exist: create agent, id=%i \n", id);
-                A = STATE::agent_list.create(id);
-            }            
-            //do something
-        /*
-            A->x = x;
-            A->y = y;
-            A->z = z;
-            A->vx = vx;
-            A->vy = vy;
-            A->vz = vz;
-        */
-            //float theta = 0;
-            //float phi = 0;
-            A->handle_state_snapshot(seq, theta, phi, x, y, z, vx, vy, vz);
-            //printf("Received Agent_state_message packet: agent_id= %i \n", id);
-
-            //printf("seq= %i \n", seq);
-
-            return;
-        }
-};
-
-PlayerAgent_state::handle_net_control_state(int _seq, int _cs, float _theta, float _phi);
 
 //Agent control state, server to client
-class Agent_cs_StoC: public FixedSizeNetPacketToClient<Agent_cs_StoC>
-{
-    public:
 
-        int id;
-        int seq;
-        uint16_t cs;
-        float theta;
-        float phi;
+inline void Agent_cs_StoC::handle() {
+    Agent_state* A = STATE::agent_list.get(id);
+    if(A == NULL) {
+        printf("Agent_control_to_client_message: agent does not exist, id= %i\n", id);
+        return;
+    }
 
-        inline void packet(unsigned char* buff, int* buff_n, bool pack) 
-        {
-            pack_u8(&id, buff, buff_n, pack);
-            pack_u8(&seq, buff, buff_n, pack);
-            pack_u16(&cs, buff, buff_n, pack);
-            pack_float(&theta, buff, buff_n, pack);
-            pack_float(&phi, buff, buff_n, pack);
-        }
-
-        inline void handle() {
-            Agent_state* A = STATE::agent_list.get(id);
-            if(A == NULL) {
-                printf("Agent_control_to_client_message: agent does not exist, id= %i\n", id);
-                return;
-            }
-
-            //printf("!!! control state= %i \n", cs);
-            A->handle_control_state(seq, cs, theta, phi);
-            #ifdef DC_CLIENT
-            ClientState::playerAgent_state.handle_net_control_state(seq, cs, theta, phi);
-            #endif
-        }
-};
-
-//agent control state, client to server
-class Agent_cs_CtoS: public FixedSizeNetPacketToServer<Agent_cs_CtoS>
-{
-    public:
-
-        int id;
-        int seq;
-        uint16_t cs;
-        float theta;
-        float phi;
-
-        inline void packet(unsigned char* buff, int* buff_n, bool pack) 
-        {
-            pack_u8(&id, buff, buff_n, pack);
-            pack_u8(&seq, buff, buff_n, pack);
-            pack_u16(&cs, buff, buff_n, pack);
-            pack_float(&theta, buff, buff_n, pack);
-            pack_float(&phi, buff, buff_n, pack);
-        }
-
-        inline void handle() {
-            //printf("cs_CtoS: seq= %i \n", seq);
-
-            Agent_state* A = STATE::agent_list.get(id);
-            if(A == NULL) {
-                STATE::agent_list.create(id);
-                printf("Agent_control_to_client_message: agent does not exist, id= %i\n", id);
-                return;
-            }
-
-
-            //determine if message is new, if so send out
-            /*
-                Client should send last 2 control states each packet, must handle redundant control state properly
-            */
-            Agent_cs_StoC M;
-            M.id = id;
-            M.seq = seq;
-            M.cs = cs;
-            M.theta = theta;
-            M.phi = phi;
-            M.broadcast();
-
-            A->handle_control_state(seq, cs, theta, phi);
-
-            /*
-                Warning: setting agent client id by the client the last control state was received for that agent
-                This needs to be done properly, at agent creation
-            */
-            A->client_id = client_id;
-
-        }
-};
+    //printf("!!! control state= %i \n", cs);
+    A->handle_control_state(seq, cs, theta, phi);
+    #ifdef DC_CLIENT
+    ClientState::playerAgent_state.handle_net_control_state(seq, cs, theta, phi);
+    #endif
+}
 
 
 //#include <c_lib/agent/player_agent.cpp>
--- netclient/c_lib/agent/net_agent.hpp
+++ netclient/c_lib/agent/net_agent.hpp
@@ -1,63 +1,142 @@
 #pragma once
 
 #include <net_lib/client/client.hpp>
+#include <net_lib/common/message_handler.h>
 
-/*
-Agent control state message
-*/
+#include <c_lib/agent/agent.hpp>
 
-/*
-class Agent_control_state_message {
+#include <net_lib/server/server.h>
 
+#include <c_lib/template/net.hpp>
+
+#include <c_lib/state/server_state.hpp>
+#include <c_lib/state/client_state.hpp>
+
+#include <c_lib/defines.h>
+
+
+//send at fixed interval, absolute position
+class Agent_state_message: public FixedSizeNetPacketToClient<Agent_state_message>
+{
     public:
-        int id;
-        int seq;
-        uint32_t cs;
-        unsigned int tick;
-        unsigned int ctick;
-        float theta;
-        float phi;
 
-        Agent_control_state_message(); //default constructor
-        void register_message();
-        void send_message();
-        void deserialize(unsigned char* buff, int buff_n, int* read_bytes);
-
-};
-
-*/
-
-/*
-void handle_agent_control_state_message(unsigned char* buff, int buff_n, int* read_bytes);
-void Agent_control_state_message_register_message();
-*/
-
-/*
-Agent state message
-*/
-
-/*
-
-class Agent_state_message {
-
-    public:
         int id;
         int seq;
         int tick;
 
         float x;
         float y;
-        float z; //,y,x;
+        float z;
         float vx,vy,vz;
 
+        float theta, phi;
 
-        Agent_state_message(); //default constructor
-        void register_message();
-        void send_message();
-        void deserialize(unsigned char* buff, int buff_n, int* read_bytes);
+        inline void packet(unsigned char* buff, int* buff_n, bool pack) 
+        {
+            pack_u8(&id, buff, buff_n, pack);   //assume id is 1 byte
+            pack_u8(&seq, buff, buff_n, pack);
+            //pack_16(&tick, buff, buff_n, pack);
 
+            pack_float(&x, buff, buff_n, pack);
+            pack_float(&y, buff, buff_n, pack);
+            pack_float(&z, buff, buff_n, pack);
+            pack_float(&vx, buff, buff_n, pack);
+            pack_float(&vy, buff, buff_n, pack);
+            pack_float(&vz, buff, buff_n, pack);
+
+            pack_float(&theta, buff, buff_n, pack);
+            pack_float(&phi, buff, buff_n, pack);
+        }
+
+        inline void handle() {
+            Agent_state* A = STATE::agent_list.get(id);
+            if(A == NULL) {
+                printf("Agent does not exist: create agent, id=%i \n", id);
+                A = STATE::agent_list.create(id);
+            }            
+
+
+            A->handle_state_snapshot(seq, theta, phi, x, y, z, vx, vy, vz);
+
+            return;
+        }
 };
 
-void handle_agent_state_message(unsigned char* buff, int buff_n, int* read_bytes);
-void Agent_state_message_register_message();
-*/
+PlayerAgent_state::handle_net_control_state(int _seq, int _cs, float _theta, float _phi);
+
+//Agent control state, server to client
+class Agent_cs_StoC: public FixedSizeNetPacketToClient<Agent_cs_StoC>
+{
+    public:
+
+        int id;
+        int seq;
+        uint16_t cs;
+        float theta;
+        float phi;
+
+        inline void packet(unsigned char* buff, int* buff_n, bool pack) 
+        {
+            pack_u8(&id, buff, buff_n, pack);
+            pack_u8(&seq, buff, buff_n, pack);
+            pack_u16(&cs, buff, buff_n, pack);
+            pack_float(&theta, buff, buff_n, pack);
+            pack_float(&phi, buff, buff_n, pack);
+        }
+
+        inline void handle();
+};
+
+//agent control state, client to server
+class Agent_cs_CtoS: public FixedSizeNetPacketToServer<Agent_cs_CtoS>
+{
+    public:
+
+        int id;
+        int seq;
+        uint16_t cs;
+        float theta;
+        float phi;
+
+        inline void packet(unsigned char* buff, int* buff_n, bool pack) 
+        {
+            pack_u8(&id, buff, buff_n, pack);
+            pack_u8(&seq, buff, buff_n, pack);
+            pack_u16(&cs, buff, buff_n, pack);
+            pack_float(&theta, buff, buff_n, pack);
+            pack_float(&phi, buff, buff_n, pack);
+        }
+
+        inline void handle() {
+            //printf("cs_CtoS: seq= %i \n", seq);
+
+            Agent_state* A = STATE::agent_list.get(id);
+            if(A == NULL) {
+                STATE::agent_list.create(id);
+                printf("Agent_control_to_client_message: agent does not exist, id= %i\n", id);
+                return;
+            }
+
+
+            //determine if message is new, if so send out
+            /*
+                Client should send last 2 control states each packet, must handle redundant control state properly
+            */
+            Agent_cs_StoC M;
+            M.id = id;
+            M.seq = seq;
+            M.cs = cs;
+            M.theta = theta;
+            M.phi = phi;
+            M.broadcast();
+
+            A->handle_control_state(seq, cs, theta, phi);
+
+            /*
+                Warning: setting agent client id by the client the last control state was received for that agent
+                This needs to be done properly, at agent creation
+            */
+            A->client_id = client_id;
+
+        }
+};
