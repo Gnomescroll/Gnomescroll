8f0fe0765de365f71f48b855dfda4f305fb88bec
HaltingState <haltingstate@gmail.com>
2011-11-22 14:41:22 -0500

    more find and replace

--- netclient/c_lib/net_lib/common/message_handler.h
+++ netclient/c_lib/net_lib/common/message_handler.h
@@ -2,16 +2,16 @@
 
 #include "type_pack.h"
 
-//typedef int (*pt2handler)(int, unsigned char*);
-typedef void (*pt2handler)(unsigned char*, int, int*, int);
+//typedef int (*pt2handler)(int, char*);
+typedef void (*pt2handler)(char*, int, int*, int);
 
-//void default_handler_function(int message_id, unsigned char* buff);
+//void default_handler_function(int message_id, char* buff);
 
 void init_message_handler();
 //void register_message_handler(int message_id, int size, pt2handler fptr);
-void process_packet_messages(unsigned char* buff, int *n, int max_n, int client_id);
-int pop_message(unsigned char* buff, int *n, int max_n, int client_id);
+void process_packet_messages(char* buff, int *n, int max_n, int client_id);
+int pop_message(char* buff, int *n, int max_n, int client_id);
 
-//typedef const void (*_pt2handler)(unsigned char*, int, int*);
+//typedef const void (*_pt2handler)(char*, int, int*);
 void register_server_message_handler(int message_id, int size, pt2handler fptr);
 void register_client_message_handler(int message_id, int size, pt2handler fptr);
