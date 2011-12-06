cf56177ae4197cc1f6783f3c014c26ee64733c70
HaltingState <haltingstate@gmail.com>
2011-12-06 04:55:19 -0500

    python message debug print statements disabled

--- netclient/c_lib/skybox/skybox.cpp
+++ netclient/c_lib/skybox/skybox.cpp
@@ -45,8 +45,8 @@ void load() {
 
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
         glTexImage2D(
             GL_TEXTURE_2D,  //target
