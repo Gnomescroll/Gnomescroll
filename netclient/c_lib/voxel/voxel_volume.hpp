75d866cc733f59c6cc8379704907e1415422d012
HaltingState <haltingstate@gmail.com>
2011-12-23 07:23:58 -0500

    vector code

--- netclient/c_lib/voxel/voxel_volume.hpp
+++ netclient/c_lib/voxel/voxel_volume.hpp
@@ -57,6 +57,123 @@ int pow2_2(int n)
 #include <voxel/common.h>
 #include <physics/vector.hpp>
 
+//z vector rotated in x,y plane by theta degrees
+//theta from 0 to 1
+static inline struct Vector unit_z_rot(float theta) __attribute((always_inline));
+struct Vector unit_z_rot(float theta)
+{   
+    Vector u;
+    u.x = 0.0;
+    u.y = sin(theta * 2*PI);
+    u.z = cos(theta * 2*PI);
+    return u;
+}
+
+/*
+void _3D::RotateFill(double *A, double xAngle, double yAngle, double zAngle)
+{
+   // Fill the rotation matrix, using Euler angles 
+
+   double x[12];
+   double y[12];
+   double z[12];
+   double tempArray[12];
+   double cx,cy,cz;
+   double sx,sy,sz;
+
+   cx = cos(xAngle);
+   cy = cos(yAngle);
+   cz = cos(zAngle);
+
+   sx = sin(xAngle);
+   sy = sin(yAngle);
+   sz = sin(zAngle);
+
+   x[0]=1;     x[1]=0;     x[2] =0;     x[3] =0;
+   x[4]=0;     x[5]=cx;    x[6] =-sx;   x[7] =0;
+   x[8]=0;     x[9]=sx;    x[10]=cx;    x[11]=0;
+
+   y[0]=cy;    y[1]=0;     y[2] =sy;    y[3] =0;
+   y[4]=0;     y[5]=1;     y[6] =0;     y[7] =0;
+   y[8]=-sy;   y[9]=0;     y[10]=cy;    y[11]=0;
+
+   z[0]=cz;    z[1]=-sz;   z[2] =0;     z[3] =0;
+   z[4]=sz;    z[5]=cz;    z[6] =0;     z[7] =0;
+   z[8]=0;     z[9]=0;     z[10]=1;     z[11]=0;
+
+   //Note we are multiplying x*y*z. You can change the order,
+   //  but you will get different results. 
+
+   MatrixMultiply(z,y,tempArray);   // multiply 2 matrices
+   MatrixMultiply(tempArray,x,A);   // multiply result by 3rd matrix
+}
+*/
+
+/*
+    inline static void rotate_euler(matrix3 &m, float x, float y, float z)
+    {
+        double cx = cos(x);
+        double sx = sin(x);
+        double cy = cos(y);
+        double sy = sin(y);
+        double cz = cos(z);
+        double sz = sin(z);
+        
+        m.col[0].x = (float)(cy*cz); 
+        m.col[0].y = (float)(cy*sz);
+        m.col[0].z = (float)(-sy);
+
+        double sxsy = sx*sy;
+        double cxsy = cx*sy;
+        
+        m.col[1].x = (float)(sxsy*cz-cx*sz);
+        m.col[1].y = (float)(sxsy*sz+cx*cz);
+        m.col[1].z = (float)(sx*cy);
+    
+        m.col[2].x = (float)(cxsy*cz+sx*sz);
+        m.col[2].y = (float)(cxsy*sz-sx*cz);
+        m.col[2].z = (float)(cx*cy);
+    }
+
+*/
+
+struct Vector euler_rotation(Vector v, float theta) __attribute((always_inline));
+struct Vector euler_rotation(Vector v, float x, float y, float z)
+{   
+    float cx = cos(x);
+    float sx = sin(x);
+    float cy = cos(y);
+    float sy = sin(y);
+    float cz = cos(z);
+    float sz = sin(z);
+    
+    Vector m[3];
+    Vector u;
+
+    m[0].x = (cy*cz); 
+    m[0].y = (cy*sz);
+    m[0].z = (-sy);
+
+    float sxsy = sx*sy;
+    float cxsy = cx*sy;
+    
+    m[1].x = (sxsy*cz-cx*sz);
+    m[1].y = (sxsy*sz+cx*cz);
+    m[1].z = (sx*cy);
+
+    m[2].x = (cxsy*cz+sx*sz);
+    m[2].y = (cxsy*sz-sx*cz);
+    m[2].z = (cx*cy);
+
+
+    u.x = v.x*m[0].x + v.y*m[1].x + v.z*m[2].x, 
+    u.y = v.x*m[0].y + v.y*m[1].y + v.z*m[2].y, 
+    u.z = v.x*m[0].z + v.y*m[1].z + v.z*m[2].z;
+
+    return u;
+}
+
+
 class Voxel_volume
 {
     public:
@@ -75,12 +192,13 @@ class Voxel_volume
 
     float hdx,hdy,hdz;  //half of width, height, depth as floats
 
-    void update()
+
+    void set_unit_axis()
     {
-        
-
+        v[0] = Vector_init(1.0f,0.0f,0.0f);
+        v[1] = Vector_init(0.0f,1.0f,0.0f);
+        v[2] = Vector_init(0.0f,0.0f,1.0f);
     }
-
     //vector_cross(struct Vector* v1, struct Vector* v2, struct Vector* dest)
     //forward and up vector
     void set_axis(Vector* f, Vector* u)
@@ -91,22 +209,45 @@ class Voxel_volume
     }
 
     //applies rotation
-    void rotate_axis(float theta, float phi)
+    //x_angle from 0 to 2
+    //y_angle from 0 to 1
+    void set_rotated_unit_axis(float x_angle, float y_angle, float z_angle)
     {
-        
+        Vector vx = Vector_init(1.0f,0.0f,0.0f);
+        vx = euler_rotation(vx, x_angle, y_angle, z_angle);
 
+        Vector vz = Vector_init(0.0f,0.0f,1.0f);
+        vz = euler_rotation(vz, x_angle, y_angle, z_angle);
+
+
+        vector_cross(&vx, &vz, &v[1]); //set v1
+        v[0] = vx;
+        v[2] = vz;
+
+        update_center();
     }
 
     // testing/tweaking
+/*
     void translate_center(float dx, float dy, float dz)
     {
         center.x += dx;
         center.y += dy;
         center.z += dz;
 
-        Vector vx = vector_scalar2(&v[0],hdx*scale);
-        Vector vy = vector_scalar2(&v[1],hdy*scale);
-        Vector vz = vector_scalar2(&v[2],hdz*scale);
+        Vector vx = vector_scalar2(&v[0],-1.0*hdx*scale);
+        Vector vy = vector_scalar2(&v[1],-1.0*hdy*scale);
+        Vector vz = vector_scalar2(&v[2],-1.0*hdz*scale);
+
+        v[4] = vector_add4(&vx,&vy,&vz,&center);
+    }
+*/
+    void update_center()
+    {
+        Vector vx = vector_scalar2(&v[0],-1.0*hdx*scale);
+        Vector vy = vector_scalar2(&v[1],-1.0*hdy*scale);
+        Vector vz = vector_scalar2(&v[2],-1.0*hdz*scale);
+
         v[4] = vector_add4(&vx,&vy,&vz,&center);
     }
 
@@ -116,9 +257,10 @@ class Voxel_volume
         center.y = y;
         center.z = z;
 
-        Vector vx = vector_scalar2(&v[0],hdx*scale);
-        Vector vy = vector_scalar2(&v[1],hdy*scale);
-        Vector vz = vector_scalar2(&v[2],hdz*scale);
+        Vector vx = vector_scalar2(&v[0],-1.0*hdx*scale);
+        Vector vy = vector_scalar2(&v[1],-1.0*hdy*scale);
+        Vector vz = vector_scalar2(&v[2],-1.0*hdz*scale);
+
         v[4] = vector_add4(&vx,&vy,&vz,&center);
     }
 
