6497655134a3b87865df52fef92c135f40a820c2
HaltingState <haltingstate@gmail.com>
2011-12-25 06:47:15 -0500

    more voxel stuff

--- netclient/c_lib/voxel/voxel_volume.hpp
+++ netclient/c_lib/voxel/voxel_volume.hpp
@@ -1,24 +1,6 @@
 #pragma once
 
 
-class Orientation_matrix
-{
-    
-    float v[4][4];
-    //x,y,z,offset
-
-    //multiply current by other
-    void multiply(Orientation_matrix* m)
-    {
-        return;
-    }
-
-    Orientation_matrix()
-    {
-        return;
-    }
-
-};
 
 unsigned NextPow2( unsigned x ) {
     --x;
@@ -68,74 +50,6 @@ struct Vector unit_z_rot(float theta)
     u.z = cos(theta * 2*PI);
     return u;
 }
-
-/*
-void _3D::RotateFill(double *A, double xAngle, double yAngle, double zAngle)
-{
-   // Fill the rotation matrix, using Euler angles 
-
-   double x[12];
-   double y[12];
-   double z[12];
-   double tempArray[12];
-   double cx,cy,cz;
-   double sx,sy,sz;
-
-   cx = cos(xAngle);
-   cy = cos(yAngle);
-   cz = cos(zAngle);
-
-   sx = sin(xAngle);
-   sy = sin(yAngle);
-   sz = sin(zAngle);
-
-   x[0]=1;     x[1]=0;     x[2] =0;     x[3] =0;
-   x[4]=0;     x[5]=cx;    x[6] =-sx;   x[7] =0;
-   x[8]=0;     x[9]=sx;    x[10]=cx;    x[11]=0;
-
-   y[0]=cy;    y[1]=0;     y[2] =sy;    y[3] =0;
-   y[4]=0;     y[5]=1;     y[6] =0;     y[7] =0;
-   y[8]=-sy;   y[9]=0;     y[10]=cy;    y[11]=0;
-
-   z[0]=cz;    z[1]=-sz;   z[2] =0;     z[3] =0;
-   z[4]=sz;    z[5]=cz;    z[6] =0;     z[7] =0;
-   z[8]=0;     z[9]=0;     z[10]=1;     z[11]=0;
-
-   //Note we are multiplying x*y*z. You can change the order,
-   //  but you will get different results. 
-
-   MatrixMultiply(z,y,tempArray);   // multiply 2 matrices
-   MatrixMultiply(tempArray,x,A);   // multiply result by 3rd matrix
-}
-*/
-
-/*
-    inline static void rotate_euler(matrix3 &m, float x, float y, float z)
-    {
-        double cx = cos(x);
-        double sx = sin(x);
-        double cy = cos(y);
-        double sy = sin(y);
-        double cz = cos(z);
-        double sz = sin(z);
-        
-        m.col[0].x = (float)(cy*cz); 
-        m.col[0].y = (float)(cy*sz);
-        m.col[0].z = (float)(-sy);
-
-        double sxsy = sx*sy;
-        double cxsy = cx*sy;
-        
-        m.col[1].x = (float)(sxsy*cz-cx*sz);
-        m.col[1].y = (float)(sxsy*sz+cx*cz);
-        m.col[1].z = (float)(sx*cy);
-    
-        m.col[2].x = (float)(cxsy*cz+sx*sz);
-        m.col[2].y = (float)(cxsy*sz-sx*cz);
-        m.col[2].z = (float)(cx*cy);
-    }
-
-*/
 
 struct Vector euler_rotation(Vector v, float theta) __attribute((always_inline));
 struct Vector euler_rotation(Vector v, float x, float y, float z)
@@ -317,12 +231,26 @@ class Voxel_volume
         return &voxel[x+(y << index1)+(z << index1)];
     }
 
-    inline void set(int x, int y, int z, Voxel* v) __attribute((always_inline))  __attribute((always_inline)) 
+    //external methods
+    void set(int x, int y, int z, Voxel* v)
+    {
+        set(x,y,z,v);
+        needs_vbo_update = true;
+    }
+
+    inline void set(int x, int y, int z, unsigned char r, unsigned char g, unsigned char b, unsigned char a)
+    {
+        set(x,y,z,r,g,b,a);
+        needs_vbo_update = true;
+    }
+
+    //internal methods
+    inline void _set(int x, int y, int z, Voxel* v) __attribute((always_inline))
     {
         voxel[x+(y << index1)+(z << index1)] = *v;
     }
 
-    inline void set(int x, int y, int z, unsigned char r, unsigned char g, unsigned char b, unsigned char a) __attribute((always_inline))  __attribute((always_inline)) 
+    inline void _set(int x, int y, int z, unsigned char r, unsigned char g, unsigned char b, unsigned char a) __attribute((always_inline))
     {
         Voxel* v = &voxel[x+(y << index1)+(z << index1)];
         v->r = r;
@@ -371,8 +299,7 @@ class Voxel_volume
         delete voxel;
     }
 
-    void draw_bounding_box_1();
-    void draw_bounding_box_2();
+    void draw_bounding_box();
 
 };
 
@@ -415,12 +342,12 @@ static int vertex_index2[2*12] = {
         3,7,
 };
 
-void Voxel_volume::draw_bounding_box_1()
+
+void Voxel_volume::draw_bounding_box()
 {
 #ifdef DC_CLIENT
 
-    //printf("dfd\n");
-    //glDisable()
+    //disable to draw over
     glEnable (GL_DEPTH_TEST);
 
     glDisable(GL_TEXTURE_2D);
@@ -444,98 +371,6 @@ void Voxel_volume::draw_bounding_box_1()
     Vector vz;
 
     Vector u;
-
-        v[3] = vector_add4(&vx,&vy,&vz,&center);
-
-    for(i=0; i<12; i++) {
-            j = 3*vertex_index2[2*i+0];
-            
-            vx = vector_scalar2(&v[0], v_set2[j+0]*hdx*scale);
-            vy = vector_scalar2(&v[1], v_set2[j+1]*hdy*scale);
-            vz = vector_scalar2(&v[2], v_set2[j+2]*hdz*scale);
-            u = vector_add3(&vx,&vy,&vz);
-                
-        /*
-            _x = v[3].x + u.x;
-            _y = v[3].y + u.y;
-            _z = v[3].z + u.z;
-        */
-
-            _x = center.x + u.x;
-            _y = center.y + u.y;
-            _z = center.z + u.z;
-
-            glVertex3f(_x,_y,_z);
-            j = 3*vertex_index2[2*i+1];
-
-            vx = vector_scalar2(&v[0], v_set2[j+0]*hdx*scale);
-            vy = vector_scalar2(&v[1], v_set2[j+1]*hdy*scale);
-            vz = vector_scalar2(&v[2], v_set2[j+2]*hdz*scale);
-            u = vector_add3(&vx,&vy,&vz);
-
-        /*
-            _x = v[3].x + u.x;
-            _y = v[3].y + u.y;
-            _z = v[3].z + u.z;
-        */
-
-            _x = center.x + u.x;
-            _y = center.y + u.y;
-            _z = center.z + u.z;
-
-            glVertex3f(_x,_y,_z);
-    }
-
-    glEnd();
-
-    glDisable (GL_DEPTH_TEST);
-    glColor3ub((unsigned char)0,(unsigned char)0,(unsigned char)255);
-    glPointSize(10.0);
-    glBegin(GL_POINTS);
-    glVertex3f(v[3].x, v[3].y, v[3].z);
-    glEnd();
-    glPointSize(1.0);
-
-    glColor3ub((unsigned char) 255,(unsigned char)255,(unsigned char)255);
-
-
-    glEnable(GL_TEXTURE_2D);
-    glLineWidth(1.0f);
-
-#endif
-}
-
-void Voxel_volume::draw_bounding_box_2()
-{
-#ifdef DC_CLIENT
-
-    //printf("dfd\n");
-    //glDisable()
-    glEnable (GL_DEPTH_TEST);
-
-    glDisable(GL_TEXTURE_2D);
-    glLineWidth(5.0f);
-
-    int i,j;
-    float _x,_y,_z;
-
-    glColor3ub((unsigned char)0,(unsigned char)255,(unsigned char)0);
-    glPointSize(10.0);
-    glBegin(GL_POINTS);
-    glVertex3f(center.x,center.y, center.z);
-    glEnd();
-    glPointSize(1.0);
-
-    glBegin(GL_LINES);
-    glColor3ub((unsigned char)255,(unsigned char)0,(unsigned char)0);
-
-    Vector vx;
-    Vector vy;
-    Vector vz;
-
-    Vector u;
-
-    int DEBUG = 0;
 
     for(i=0; i<12; i++) {
             j = 3*vertex_index2[2*i+0];
@@ -545,28 +380,6 @@ void Voxel_volume::draw_bounding_box_2()
             vz = vector_scalar2(&v[2], 2.0*v_set[j+2]*hdz*scale);
 
             u = vector_add4(&vx,&vy,&vz,&v[3]);
-            //u = vector_add3(&vx,&vy,&vz);
-
-            if(DEBUG)
-            {
-            printf("0v x,y,z= %f, %f, %f \n", vx.x, vx.y, vx.z);
-            printf("1v x,y,z= %f, %f, %f \n", vy.x, vy.y, vy.z);
-            printf("2v x,y,z= %f, %f, %f \n", vz.x, vz.y, vz.z);
-            printf("v[3] x,y,z= %f, %f, %f \n", v[3].x, v[3].y, v[3].z);
-            
-            printf("sum x,y,z= %f, %f, %f \n", u.x, u.y, u.z);
-            }
-
-            //printf("v[3] x,y,z= %f, %f, %f \n", v[3].x, v[3].y, v[3].z);
-            //printf("sum x,y,z= %f, %f, %f \n", u.x, u.y, u.z);
-
-            //u = vector_add3(&vx,&vy,&vz);
-
-        /*
-            _x = v[3].x + u.x;
-            _y = v[3].y + u.y;
-            _z = v[3].z + u.z;
-        */
 
             _x = u.x;
             _y = u.y;
@@ -580,13 +393,6 @@ void Voxel_volume::draw_bounding_box_2()
             vz = vector_scalar2(&v[2], 2.0*v_set[j+2]*hdz*scale);
             
             u = vector_add4(&vx,&vy,&vz,&v[3]);
-            //u = vector_add3(&vx,&vy,&vz);
-
-        /*
-            _x = v[3].x + u.x;
-            _y = v[3].y + u.y;
-            _z = v[3].z + u.z;
-        */
 
             _x = u.x;
             _y = u.y;
@@ -636,19 +442,95 @@ void voxel_test()
 
     static float c0 = 0.0;
     static float c1 = 0.0;
-    c0 += 0.005;
+    static float c2 = 0.0;
+    c0 += 0.0050;
     c1 += 0.0025;
+    c2 += 0.0100;
 
-    printf("start\n");
-    printf("1 v[3] x,y,z= %f, %f, %f \n", vv.v[3].x, vv.v[3].y, vv.v[3].z);
+
+    c1 = 0.5;
+    c2 = 0.25;
+    //printf("start\n");
+    //printf("1 v[3] x,y,z= %f, %f, %f \n", vv.v[3].x, vv.v[3].y, vv.v[3].z);
     
-    vv.set_rotated_unit_axis(0.0f, c1, c0);
+    vv.set_rotated_unit_axis(c0, c1, c2);
     //vv.set_rotated_unit_axis(0.0f, 0.0f, 0.0f);
     vv.set_center( 2.0, 2.0, 2.0);
-    printf("2 v[3] x,y,z= %f, %f, %f \n", vv.v[3].x, vv.v[3].y, vv.v[3].z);
+    //printf("2 v[3] x,y,z= %f, %f, %f \n", vv.v[3].x, vv.v[3].y, vv.v[3].z);
 
     //vv.set_unit_axis();
-    printf("3 v[3] x,y,z= %f, %f, %f \n", vv.v[3].x, vv.v[3].y, vv.v[3].z);
-    vv.draw_bounding_box_2();
-    printf("4 v[3] x,y,z= %f, %f, %f \n", vv.v[3].x, vv.v[3].y, vv.v[3].z);
+    //printf("3 v[3] x,y,z= %f, %f, %f \n", vv.v[3].x, vv.v[3].y, vv.v[3].z);
+    vv.draw_bounding_box();
+    //printf("4 v[3] x,y,z= %f, %f, %f \n", vv.v[3].x, vv.v[3].y, vv.v[3].z);
 }
+
+
+//Matix reference implementation
+
+/*
+void _3D::RotateFill(double *A, double xAngle, double yAngle, double zAngle)
+{
+   // Fill the rotation matrix, using Euler angles 
+
+   double x[12];
+   double y[12];
+   double z[12];
+   double tempArray[12];
+   double cx,cy,cz;
+   double sx,sy,sz;
+
+   cx = cos(xAngle);
+   cy = cos(yAngle);
+   cz = cos(zAngle);
+
+   sx = sin(xAngle);
+   sy = sin(yAngle);
+   sz = sin(zAngle);
+
+   x[0]=1;     x[1]=0;     x[2] =0;     x[3] =0;
+   x[4]=0;     x[5]=cx;    x[6] =-sx;   x[7] =0;
+   x[8]=0;     x[9]=sx;    x[10]=cx;    x[11]=0;
+
+   y[0]=cy;    y[1]=0;     y[2] =sy;    y[3] =0;
+   y[4]=0;     y[5]=1;     y[6] =0;     y[7] =0;
+   y[8]=-sy;   y[9]=0;     y[10]=cy;    y[11]=0;
+
+   z[0]=cz;    z[1]=-sz;   z[2] =0;     z[3] =0;
+   z[4]=sz;    z[5]=cz;    z[6] =0;     z[7] =0;
+   z[8]=0;     z[9]=0;     z[10]=1;     z[11]=0;
+
+   //Note we are multiplying x*y*z. You can change the order,
+   //  but you will get different results. 
+
+   MatrixMultiply(z,y,tempArray);   // multiply 2 matrices
+   MatrixMultiply(tempArray,x,A);   // multiply result by 3rd matrix
+}
+*/
+
+/*
+    inline static void rotate_euler(matrix3 &m, float x, float y, float z)
+    {
+        double cx = cos(x);
+        double sx = sin(x);
+        double cy = cos(y);
+        double sy = sin(y);
+        double cz = cos(z);
+        double sz = sin(z);
+        
+        m.col[0].x = (float)(cy*cz); 
+        m.col[0].y = (float)(cy*sz);
+        m.col[0].z = (float)(-sy);
+
+        double sxsy = sx*sy;
+        double cxsy = cx*sy;
+        
+        m.col[1].x = (float)(sxsy*cz-cx*sz);
+        m.col[1].y = (float)(sxsy*sz+cx*cz);
+        m.col[1].z = (float)(sx*cy);
+    
+        m.col[2].x = (float)(cxsy*cz+sx*sz);
+        m.col[2].y = (float)(cxsy*sz-sx*cz);
+        m.col[2].z = (float)(cx*cy);
+    }
+
+*/
