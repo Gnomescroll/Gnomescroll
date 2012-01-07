cac5a84d4d6a8a4dce3a38a3d0d59a7e7074c6fa
HaltingState <haltingstate@gmail.com>
2012-01-06 20:53:10 -0500

    stuff

--- netclient/c_lib/net_lib/host.cpp
+++ netclient/c_lib/net_lib/host.cpp
@@ -62,7 +62,7 @@ void init_client()
 }
 
 
-void connect_to(int a, int b, int c, int d, unsigned short port) 
+void client_connect_to(int a, int b, int c, int d, unsigned short port) 
 {
     printf("connect_to: attempting connection with server \n");
 
@@ -103,53 +103,10 @@ void connect_to(int a, int b, int c, int d, unsigned short port)
         puts ("connection to some.server.net:1234 failed.");
     }
 
-
-
     printf("Client is connected to server \n");
     client.connected = 1;
 }
 
-void client_connect()
-{
-    ENetAddress address;
-    ENetEvent event;
-    ENetPeer *peer;
-
-    /* Connect to some.server.net:1234. */
-    enet_address_set_host (& address, "some.server.net");
-    address.port = 1234;
-
-    /* Initiate the connection, allocating the two channels 0 and 1. */
-    peer = enet_host_connect (client, & address, 2, 0);    
-    
-    if (peer == NULL)
-    {
-       fprintf (stderr, 
-                "No available peers for initiating an ENet connection.\n");
-       exit (EXIT_FAILURE);
-    }
-    
-    /* Wait up to 5 seconds for the connection attempt to succeed. */
-    if (enet_host_service (client, & event, 5000) > 0 &&
-        event.type == ENET_EVENT_TYPE_CONNECT)
-    {
-        puts ("Connection to some.server.net:1234 succeeded.");
-        ...
-        ...
-        ...
-    }
-    else
-    {
-        /* Either the 5 seconds are up or a disconnect event was */
-        /* received. Reset the peer in the event the 5 seconds   */
-        /* had run out without any significant event.            */
-        enet_peer_reset (peer);
-
-        puts ("Connection to some.server.net:1234 failed.");
-    }
-
-
-}
 
 //iterate through client list
 void dispatch_network_events()
@@ -157,18 +114,34 @@ void dispatch_network_events()
     ENetEvent event;
     
     /* Wait up to 5 milliseconds for an event. */
-    while (enet_host_service (client, & event, 2) > 0)
+    while (enet_host_service (enet_host, & event, 5) > 0)
     {
         switch (event.type)
         {
+
+        //ENET_EVENT_TYPE_NONE       = 0,  
+        //ENET_EVENT_TYPE_CONNECT    = 1,  
+        //ENET_EVENT_TYPE_DISCONNECT = 2,  
+        //ENET_EVENT_TYPE_RECEIVE    = 3
+
+        case ENET_EVENT_TYPE_NONE:
+            printf("Nothing happened \n");
+            break;
+
+        //handle connect
         case ENET_EVENT_TYPE_CONNECT:
             printf ("A new client connected from %x:%u.\n", 
                     event.peer -> address.host,
                     event.peer -> address.port);
-
             /* Store any relevant client information here. */
             event.peer -> data = "Client information";
+            break;
 
+        //handle disconnect
+        case ENET_EVENT_TYPE_DISCONNECT:
+            printf ("%s disconected.\n", event.peer -> data);
+            /* Reset the peer's client information. */
+            event.peer -> data = NULL;
             break;
 
         case ENET_EVENT_TYPE_RECEIVE:
@@ -177,18 +150,10 @@ void dispatch_network_events()
                     event.packet -> data,
                     event.peer -> data,
                     event.channelID);
-
             /* Clean up the packet now that we're done using it. */
             enet_packet_destroy (event.packet);
-            
             break;
            
-        case ENET_EVENT_TYPE_DISCONNECT:
-            printf ("%s disconected.\n", event.peer -> data);
-
-            /* Reset the peer's client information. */
-
-            event.peer -> data = NULL;
         }
     }
 }
--- netclient/c_lib/net_lib/net_lib.h
+++ netclient/c_lib/net_lib/net_lib.h
@@ -1,27 +1,23 @@
 #pragma once
 
-//put stuff that gets exposed to cython at top here
-//extern "C" {
-    #include "./time/physics_timer.c" //physics timer
-//
+#include <c_lib/time/physics_timer.c>
 
-
-//#include <net_lib/client/pviz.c>
 
 #include <net_lib/export.cpp>
 
-#include <net_lib/common/net_peer.cpp>
-#include <net_lib/common/sequencer.c>
+#include <net_lib/net_peer.cpp>
+//#include <net_lib/common/sequencer.c>
 #include <net_lib/message_handler.c>
 #include <net_lib/packet_buffer.cpp>
-#include <net_lib/common/python_channel.cpp>
 
-#ifdef DC_CLIENT
-#include <net_lib/client/client.cpp>
-#include <net_lib/client.c>
-#endif
+//#include <net_lib/common/python_channel.cpp>
 
-#ifdef DC_SERVER
-#include <net_lib/server/server.c>
-#include <net_lib/server.c>
-#endif
+//#ifdef DC_CLIENT
+//#include <net_lib/client/client.cpp>
+//#include <net_lib/client.c>
+//#endif
+
+//#ifdef DC_SERVER
+//#include <net_lib/server/server.c>
+//#include <net_lib/server.c>
+//#endif
