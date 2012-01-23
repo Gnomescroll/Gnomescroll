c1e3b9e88106a4163ba30672eda7cdbd7baa8263
HaltingState <haltingstate@gmail.com>
2012-01-23 16:55:25 -0500

    testing

--- netclient/c_lib/voxel/voxel_skeleton.hpp
+++ netclient/c_lib/voxel/voxel_skeleton.hpp
@@ -9,36 +9,77 @@
 
 static const char* test_string = "3\n4 5\n #comment \n 6 5 \x00 \n";
 
-class Voxel_skeleton
+
+#include <sys/stat.h>
+
+off_t fsize(const char *filename) {
+    struct stat st; 
+    if (stat(filename, &st) == 0)
+        return st.st_size;
+    return -1; 
+}
+
+class Voxel_loader
 {
     private:
+
+        //this function will always return on a new line or null
         void check_for_comments(char* s, int* index)
         {   
+            jmp:
             while(s[*index] == ' ' || s[*index] == '\t' || s[*index] == '\n') (*index)++;
             if(s[*index] != '#' || s[*index] == 0) return;
-            printf("Comment at: %i \n", *index);
-
             while(s[*index] != '\n' && s[*index] != 0) (*index)++;
-            //if(s[*index] != 0) (*index)++; //scanf skips newlines anyways
-            //this function will always return on a new line or null
-            printf("skipped to: %i \n", *index);
+            goto jmp;
         }
 
     public:
+    //Voxel_volume** voxel_volume_list;
+    //int voxel_volume_num;
+    //int* voxel_volume;
 
-    Voxel_volume** voxel_volume_list;
-    int voxel_volume_num;
-    int* voxel_volume;
+    Voxel_loader() {}
 
-
-    Voxel_skeleton(char* string)
+    void load_skeleton_from_file(char* file_name)
     {
-        init_from_string(string);
+
+        int size = fsize(file_name);
+
+        char* buffer = new char[size+1];
+
+        FILE *fp = fopen(file_name, "r"); //open file for reading
+
+        int nbytes = fread(buffer, sizeof(char), size, fp);
+        if ( nbytes != size )
+        {
+            printf("load_skeleton_from_file: failed to write %zu bytes\n", nbytes);
+            return;       
+        }
+        buffer[size] = NULL;
+
+        printf("skeleton file size= %i \n", size);
+
+        char* str_tmp = new char[512];
+
+
+        int num_volumes;
+
+        int index = 0;
+        int read;
+
+        //read in number of voxel volumes
+        check_for_comments(buffer, &index);
+        sscanf (buffer+index, "%d %n", &num_volumes, &read);
+        index += read;
+
+        printf("num_volumes= %i \n", num_volumes);
+
+
     }
 
     void init_from_string(char* s)
     {
-
+        /*
         int index = 0;
         int read;
      
@@ -62,11 +103,37 @@ class Voxel_skeleton
         sscanf (s+index, "%d %d %n", &n1, &n2, &read);
         printf("num1= %i num2= %i \n", n1, n2);
         printf("read %i bytes\n", read);
+        */
+    }
+
+};
+
+class Voxel_skeleton
+{
+    private:
+
+    public:
+
+    Voxel_volume** voxel_volume_list;
+    int voxel_volume_num;
+    int* voxel_volume;
+
+
+    Voxel_skeleton(char* string)
+    {
+    }
+
+    void init_from_string(char* s)
+    {
     }
 
 };
 
 void test_voxel_skeleton()
 {
-    class Voxel_skeleton a((char*) test_string);
+    return;
+    class Voxel_loader vl;
+    vl.load_skeleton_from_file("./media/voxel/test_skeleton");
+
+    //class Voxel_skeleton a((char*) test_string);
 }
