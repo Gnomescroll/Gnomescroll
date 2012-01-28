e7011865c34c1237105577e6b2618a5ee60c13e9
Steve Leonard <sleonard76@gmail.com>
2012-01-28 04:16:27 -0800

    restore wscript

--- netclient/c_lib/compat_gl.h
+++ netclient/c_lib/compat_gl.h
@@ -5,23 +5,23 @@
         #define NO_SDL_GLEXT
         #include <GL/glew.h>
         #include <SDL.h>
-        #include <SDL_keyboard.h>
-        #include <SDL_keysym.h>
+        //#include <SDL_keyboard.h>
+        //#include <SDL_keysym.h>
         #include <SDL_opengl.h>
     #elif _WIN32
         //#define NO_SDL_GLEXT //do not need on windows?
         #include <windows.h>
         #include <SDL.h>
-        #include <SDL_keyboard.h>
-        #include <SDL_keysym.h>
+        //#include <SDL_keyboard.h>
+        //#include <SDL_keysym.h>
         #include <GL/glew.h>
         //#include <GL/wglew.h> //just in case?
     #elif __APPLE__
         //#define NO_SDL_GLEXT
         #include <GL/glew.h>
         #include <SDL.h>
-        #include <SDL_keyboard.h>
-        #include <SDL_keysym.h>
+        //#include <SDL_keyboard.h>
+        //#include <SDL_keysym.h>
         #include <OpenGL/gl.h>
         #include <OpenGL/glu.h>
         //#include <SDL_opengl.h>
