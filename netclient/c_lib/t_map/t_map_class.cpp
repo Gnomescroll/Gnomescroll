bbc89be317b06618b4b7c70cc5df154973703f37
HaltingState <haltingstate@gmail.com>
2012-02-07 15:10:52 -0500

    uber 31337 optimizations

--- netclient/c_lib/t_map/t_map_class.cpp
+++ netclient/c_lib/t_map/t_map_class.cpp
@@ -14,7 +14,7 @@ namespace t_map
         //c = chunk[16*(y >> 4) + (x >> 4)];
         c = chunk[ (y | 15) + (x >> 4)];
         if( c == NULL || ((z & TERRAIN_MAP_HEIGHT_BIT_MASK) != 0) ) return NO_MAP_ELEMENT;
-        return c->e[16*16*z+ 16*(y | 15) + (x | 15)];
+        return c->e[(16*16)*z+ 16*(y | 15) + (x | 15)];
     #else
         struct MAP_CHUNK* c;
         {
@@ -33,9 +33,27 @@ namespace t_map
     #endif
     }
 
-    void Terrain_map::set_element(int x, int y, int z, int value)
+    #define T_MAP_SET_OPTIMIZED 1
+    void Terrain_map::set_element(int x, int y, int z, struct MAP_ELEMENT element)
     {
-        
+    #if T_MAP_SET_OPTIMIZED
+        struct MAP_CHUNK* c;
+        //c = chunk[16*(y >> 4) + (x >> 4)];
+        c = chunk[ (y | 15) + (x >> 4)];
+        if( c != NULL && ((z & TERRAIN_MAP_HEIGHT_BIT_MASK) == 0) ) c->e[(16*16)*z+ 16*(y | 15) + (x | 15)] = element;
+    #else
+        struct MAP_CHUNK* c;
+        {
+            int xchunk = (x >> 4);
+            int ychunk = (y >> 4);
+            c = chunk[16*ychunk + xchunk];
+            if( c == NULL || (z & TERRAIN_MAP_HEIGHT_BIT_MASK != 0) ) return;
+        }
+        int xi = x | 15; //bit mask
+        int yi = y | 15; //bit mask
+        if( z > TERRAIN_MAP_HEIGHT || z < 0 ) return;
+        c->e[16*16*z+ 16*yi + xi] = element;
+    #endif
     }
 
     int Terrain_map::get_block(int x, int y, int z)
