12fb056140e56b16c536e8d13e85bea8c691f421
HaltingState <haltingstate@gmail.com>
2012-02-13 13:08:16 -0500

    removed deprecated reference stuff

--- netclient/c_lib/t_map/deprecate/t_compress.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-int map_save_to_disk(char *fn);
--- netclient/c_lib/t_map/deprecate/t_extern.h
+++
@@ -1,9 +0,0 @@
-
-#include <compat_gl.h>
-
-extern int must_lock_block_surface;
-extern SDL_Surface* block_surface;
-extern SDL_PixelFormat *block_surface_pixel_format;
-extern int block_surface_width, block_surface_height;
-extern GLuint block_texture;
-extern GLuint block_texture_no_gamma_correction;
