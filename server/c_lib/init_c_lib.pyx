c78770ddd9b32ce6daa4cdab1e109dc5247fa8bf
Steve Leonard <sleonard76@gmail.com>
2012-02-13 03:28:24 -0800

    remove team crap from init_c_lib

--- server/c_lib/init_c_lib.pyx
+++ server/c_lib/init_c_lib.pyx
@@ -141,30 +141,9 @@ def teleport_Agent(int id, float x, float y, float z):
 
 """ Game Modes (CTF) """
 
-cdef extern from "./game/teams.hpp":
-    cdef cppclass Team:
-        int id
-        char* name
-        int n
-        bool viewers
-
-    cdef cppclass NoTeam:
-        int id
-        char* name
-        int n
-        bool viewers
-
-    cdef cppclass CTFTeam:  #inherits Team
-        pass
-
 cdef extern from "./game/ctf.hpp":
     cdef cppclass CTF:
-        NoTeam none
-        CTFTeam one
-        CTFTeam two
         void set_team_color(int team, unsigned char r, unsigned char g, unsigned char b)
-        void add_agent_to_team(int team, int agent)
-        void remove_agent_from_team(int agent)
         void start()
         void check_agent_proximities()
         void set_team_name(int team, char* name)
@@ -174,74 +153,6 @@ cdef extern from "./state/server_state.hpp" namespace "ServerState":
 
 def set_team_color(int team, unsigned char r, unsigned char g, unsigned char b):
     ctf.set_team_color(team, r,g,b)
-
-class DummyCTFTeam(object):
-
-    def __init__(self, id):
-        if id not in [1,2]:
-            print "DummyCTFTeam object -- id invalid %d" % (id,)
-            raise ValueError
-        self.id = id
-
-    def __getattribute__(self, k):
-
-        id = object.__getattribute__(self, 'id')
-        cdef Team t
-        if id == 1:
-            t = <Team>(ctf.one)
-        elif id == 2:
-            t = <Team>(ctf.two)
-
-        elif k == 'name':
-            return t.name
-        elif k == 'n':
-            return t.n
-        elif k == 'viewers':
-            return t.viewers
-        elif k == 'id':
-            return k.id
-        elif k.startswith('__'):
-            return object.__getattribute__(self, k)
-
-        raise AttributeError
-
-ctf_one = DummyCTFTeam(1)
-ctf_two = DummyCTFTeam(2)
-
-class DummyNoTeam(object):
-
-    def __getattribute__(self, k):
-
-        cdef NoTeam t
-        t = ctf.none
-
-        if k == 'name':
-            return t.name
-        elif k == 'n':
-            return t.n
-        elif k == 'viewers':
-            return t.viewers
-        elif k == 'id':
-            return k.id
-        elif k.startswith('__'):
-            return object.__getattribute__(self, k)
-        raise AttributeError
-
-ctf_none = DummyNoTeam()
-
-def get_team(int id):
-    if id == 0:
-        return ctf_none
-    elif id == 1:
-        return ctf_one
-    elif id == 2:
-        return ctf_two
-    else:
-        print "c_lib_game_modes.get_team :: invalid team id %d" % (id,)
-
-
-def join_team(int agent_id, int team_id):
-    ctf.add_agent_to_team(team_id, agent_id)
 
 def ctf_start():
     ctf.start()
