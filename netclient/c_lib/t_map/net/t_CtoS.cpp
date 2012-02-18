b9c4b68d92d9674f3158c0e5f99d802224e5d51e
HaltingState <haltingstate@gmail.com>
2012-02-18 02:24:22 -0500

    Stuff

--- netclient/c_lib/t_map/net/t_CtoS.cpp
+++ netclient/c_lib/t_map/net/t_CtoS.cpp
@@ -8,65 +8,6 @@ using namespace t_map;
 
 #ifdef DC_SERVER
 
-// unsigned short x,y;
-// unsigned short chunk_alias;
-// unsigned short chunk_version;
-
-void map_chunk_subscription_request_CtoS::handle() 
-{
-    
-
-}
-
-void map_chunk_list_request_CtoS::handle() 
-{
-    x = x*16 + 8;
-    y = y*16 + 8;
-            
-    printf("map_chunk_list_request_CtoS: client= %i, x= %i y= %i r= %i \n", client_id, x,y,r);
-
-    const int r2 = r*r;
-
-    for(int i = 0; i < MAP_WIDTH/16; i++ )
-    for(int j = 0; j < MAP_HEIGHT/16; j++)
-    {
-        class MAP_CHUNK* c = main_map->chunk[j*MAP_CHUNK_WIDTH + i];
-        if( c == NULL ) continue;
-
-        int p1 = (c->xpos+8) - x;
-        int p2 = (c->ypos+8) - x;
-
-        if( p1*p1 + p2*p2 > r2)
-        {
-            printf("sending chunk meta to client %i \n", client_id);
-            class chunk_meta_data_StoC m;
-            m.chunk_x = i;
-            m.chunk_y = j;
-            m.version = 1; //c->version;
-            m.sendToClient(client_id);
-        }
-    }
-}
-
-/*
-    Old Messages
-*/
-
-
-void map_chunk_request_CtoS::handle() 
-{
-    //x, y, version
-    class MAP_CHUNK* c = main_map->chunk[y*MAP_CHUNK_WIDTH + x];
-    if( c == NULL )
-    {
-        printf("map_chunk_request_CtoS::handle: Warning: client requested null chunk! \n");
-        return;
-    }
-
-}
-
-
-
 void block_CtoS::handle() 
 {
     _set_broadcast(x,y,z, val);
@@ -76,14 +17,6 @@ void block_CtoS::handle()
 
 #ifdef DC_CLIENT
 
-void map_chunk_subscription_request_CtoS::handle() {}
-void map_chunk_list_request_CtoS::handle() {}
-
-/*
-    Old Messages
-*/
-
-void map_chunk_request_CtoS::handle() {}
 void block_CtoS::handle() {}
 
 #endif
--- netclient/c_lib/t_map/net/t_CtoS.hpp
+++ netclient/c_lib/t_map/net/t_CtoS.hpp
@@ -2,66 +2,6 @@
 
 #include "t_net.hpp"
 
-/*
-    New Messages
-*/
-
-class map_chunk_subscription_request_CtoS: public MapMessagePacketToServer<map_chunk_subscription_request_CtoS>
-{
-    public:
-
-        unsigned short x,y;
-        unsigned short chunk_alias;
-        unsigned short chunk_version;
-        
-        inline void packet(char* buff, int* buff_n, bool pack) 
-        {
-            pack_u16(&x, buff, buff_n, pack);
-            pack_u16(&y, buff, buff_n, pack);
-            pack_u16(&chunk_alias, buff, buff_n, pack);
-            pack_u16(&chunk_version, buff, buff_n, pack);
-        }
-
-        inline void handle();
-};
-
-class map_chunk_list_request_CtoS: public MapMessagePacketToServer<map_chunk_list_request_CtoS>
-{
-    public:
-
-        int x,y;    //x,y chunk cordinates
-        int r;      //r - radius
-        
-        inline void packet(char* buff, int* buff_n, bool pack) 
-        {
-            pack_u16(&x, buff, buff_n, pack);
-            pack_u16(&y, buff, buff_n, pack);
-            pack_u8(&r, buff, buff_n, pack);
-        }
-
-        inline void handle();
-};
-
-/*
-    Old Messages
-*/
-
-
-class map_chunk_request_CtoS: public MapMessagePacketToServer<map_chunk_request_CtoS>
-{
-    public:
-
-        int x,y;
-        int version;    //clients current version of chunk
-        
-        inline void packet(char* buff, int* buff_n, bool pack) 
-        {
-            pack_u16(&x, buff, buff_n, pack);
-            pack_u16(&y, buff, buff_n, pack);
-            pack_u16(&version, buff, buff_n, pack);
-        }
-        inline void handle();
-};
 
 class block_CtoS: public MapMessagePacketToServer<block_CtoS>
 {
