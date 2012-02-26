fd318b18be8cd489b60b2711cd75db40e447a593
HaltingState <haltingstate@gmail.com>
2012-02-26 00:31:46 -0800

    changes to windows build script

--- py2exe_package.py
+++
@@ -1,8 +0,0 @@
-#!/c/Python27/python.exe
-
-# setup.py
-from distutils.core import setup
-import py2exe
-import glob
-
-setup(console=["run.py"],)
--- windows_build.sh
+++ windows_build.sh
@@ -3,20 +3,20 @@
 rm -rf ./dist
 
 mkdir ./dist
-mkdir ./dist/netclient
-mkdir ./dist/server
+#mkdir ./dist/netclient
+#mkdir ./dist/server
 
-cp -f ./server/dats/cube_dat.py ./netclient/cube_dat.py
+cp -f ./server/cube.py ./netclient/cube_dat.py
 
 cd netclient
 ./win_package.sh
 cd ..
-cp -rf ./netclient/dist/. ./dist/netclient/
+cp -rf ./netclient/dist/. ./dist/
 
-cd server
-./win_package.sh
-cd ..
-cp -rf ./server/dist/. ./dist/server/
+#cd server
+#./win_package.sh
+#cd ..
+#cp -rf ./server/dist/. ./dist/server/
 
 rm -rf /c/Documents\ and\ Settings/Administrator/Desktop/Dropbox/gnomescroll_
 cp -rf ./dist/ /c/Documents\ and\ Settings/Administrator/Desktop/Dropbox/gnomescroll_
