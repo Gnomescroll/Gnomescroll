458ba20f94c9d6650978b1cf83d0c638a5ff9acf
HaltingState <haltingstate@gmail.com>
2012-02-26 01:03:49 -0800

    default windows settings

---
+++ netclient/default_win_settings.py
@@ -0,0 +1,45 @@
+''' User '''
+name = 'steve'
+alt_name = '[%s]' % (name,)
+
+''' Network '''
+#server = '127.0.0.1'
+server = '174.37.26.119'  # use internal default
+port = 4096
+
+''' Game Preferences '''
+auto_assign_team = True
+
+''' Window / Camera '''
+width = 1024
+height = 800
+fullscreen = False
+fov = 85.
+
+''' Controls '''
+sensitivity = 1000
+camera_speed = 0.6
+invert_mouse = False
+agent_motion = 'normal'
+
+''' HUD/Information '''
+hud = True  # show hud
+diagnostic_hud = True # show diagnostic HUD elements (graphs, ping, fps)
+
+inventory_hud_x_offset = 250
+inventory_hud_y_offset = 500
+
+cube_selector_x_offset = 150
+cube_selector_y_offset = 150
+
+fps = True  # display FPS
+ping = True # calculate and display ping
+ping_update_interval = 500  # ms delay between ping updates
+
+font = "inconsolata_18.fnt"
+font_size = 18
+
+''' Sound '''
+sound = True
+sfx = 100
+music = 100
