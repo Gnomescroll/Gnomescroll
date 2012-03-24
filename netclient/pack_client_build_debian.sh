4bad121b3cb56a7276e4363ef1a94cdcc1ca40bd
Steve Leonard <sleonard76@gmail.com>
2012-03-24 15:26:08 -0500

    scripts

---
+++ netclient/pack_client_build_debian.sh
@@ -0,0 +1,23 @@
+./waf clean
+./waf linux
+./waf
+version=`../get_current_head.sh`
+f="gnomescroll_linux32_"$version
+rm -rf $f
+mkdir $f
+cp -R ext $f/
+cp libc_lib.so $f/
+cp run $f/
+cp *.pyc $f/
+rm $f/settings.pyc
+#cp default_settings.py $f/settings.py
+cp -R media $f/
+mkdir $f/lib
+cp /usr/lib/i386-linux-gnu/libpng12.so.0 $f/lib/
+cp README_linux $f/
+rm $f.tar.gz
+tar cvzf $f.tar.gz $f
+#mv $f.tar.gz ~/Dropbox/Public
+#rm -rf ~/gs_build/$f
+#mv $f ~/gs_build/
+
