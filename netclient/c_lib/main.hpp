2baa70c13d628d689d98570ad6c8ae99dcb84faa
Steve Leonard <sleonard76@gmail.com>
2012-04-01 14:54:27 -0700

    todo

--- netclient/c_lib/main.hpp
+++ netclient/c_lib/main.hpp
@@ -10,10 +10,10 @@ int run() GNOMESCROLL_API;
 
 }
 
-//extern "C"
-//{
-    //void main_init() GNOMESCROLL_API;
-    //void main_init() { Main::init(); }
-    //int main_run() GNOMESCROLL_API;
-    //int main_run() { return Main::run(); }
-//}
+extern "C"
+{
+    void main_init() GNOMESCROLL_API;
+    void main_init() { Main::init(); }
+    int main_run() GNOMESCROLL_API;
+    int main_run() { return Main::run(); }
+}
--- netclient/gnomescroll_client.cpp
+++ netclient/gnomescroll_client.cpp
@@ -1,14 +1,14 @@
 #include <c_lib/main.hpp>
+#include <SDL.h>
 
 //uncomment when not compiling as shared lib
-//#include <SDL.h>
 //#include <c_lib/c_lib.cpp>
 
 int main(int argc, char** argv)
 {
-    Main::init();
-    int ret = Main::run();
-    //main_init();
-    //int ret = main_run();
+    //Main::init();
+    //int ret = Main::run();
+    main_init();
+    int ret = main_run();
     return ret;
 }
