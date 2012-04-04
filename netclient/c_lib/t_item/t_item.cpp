5ab4ff1edd59ea76dba5a82d079b27213ae9de6e
Steve Leonard <sleonard76@gmail.com>
2012-04-04 03:03:42 -0700

    link  to server/c_lib/

--- netclient/c_lib/t_item/t_item.cpp
+++ netclient/c_lib/t_item/t_item.cpp
@@ -5,5 +5,5 @@
 #include <c_lib/t_item/sprite.cpp>
 #include <c_lib/t_item/free_item.cpp>
 #include <c_lib/t_item/interface.cpp>
-#include <c_lib/t_item/list.cpp>
+//#include <c_lib/t_item/list.cpp>
 #include <c_lib/t_item/net.cpp>
