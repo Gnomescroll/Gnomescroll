774dac7a7b16c124c8c20eabaa5c07ea0cdc17d3
Steve Leonard <sleonard76@gmail.com>
2012-04-07 20:08:44 -0700

    .

--- netclient/c_lib/objects/common/api/draw.hpp
+++ netclient/c_lib/objects/common/api/draw.hpp
@@ -8,14 +8,14 @@
 #include <c_lib/common/common.hpp>
 #include <c_lib/voxel/common_data.h>
 
-void drawBillboardSprite(Vec3 position, int texture_index, float texture_scale)
+void drawAxialBillboardSprite(Vec3 position, int texture_index, float texture_scale)
 {
     #if DC_CLIENT
     position.z += texture_scale;
     
     if (current_camera == NULL
     || !current_camera->in_view(position.x, position.y, position.z))
-        return;
+        return; // TODO: necessary?
 
     if (point_fulstrum_test(position.x, position.y, position.z) == false)
         return; // fulstrum check
@@ -99,3 +99,44 @@ void drawMinivox(Vec3 position, Vec3 forward, Vec3 right, Vec3 normal, float xy_
     }
     #endif
 }
+
+void drawBillboardSprite(Vec3 position, int texture_index, float texture_scale)
+{
+    #if DC_CLIENT
+    position.z += texture_scale;
+
+    Vec3 up = vec3_init(
+        model_view_matrix[0]*texture_scale,
+        model_view_matrix[4]*texture_scale,
+        model_view_matrix[8]*texture_scale
+    );
+    Vec3 right = vec3_init(
+        model_view_matrix[1]*texture_scale,
+        model_view_matrix[5]*texture_scale,
+        model_view_matrix[9]*texture_scale
+    );
+
+    float tx_min, tx_max, ty_min, ty_max;
+    tx_min = (float)(texture_index%16)* (1.0/16.0);   // TODO -- get dimensions from spritesheet lookup
+    tx_max = tx_min + (1.0/16.0);
+    ty_min = (float)(texture_index/16)* (1.0/16.0);
+    ty_max = ty_min + (1.0/16.0);
+
+    Vec3 p = vec3_sub(position, vec3_add(right, up));
+    glTexCoord2f(tx_min,ty_max);
+    glVertex3f(p.x, p.y, p.z);
+
+    p = vec3_add(position, vec3_sub(up, right));
+    glTexCoord2f(tx_max,ty_max);
+    glVertex3f(p.x, p.y, p.z);
+
+    p = vec3_add(position, vec3_add(up, right));
+    glTexCoord2f(tx_max,ty_min);
+    glVertex3f(p.x, p.y, p.z);
+
+    p = vec3_add(position, vec3_sub(right, up));
+    glTexCoord2f(tx_min,ty_min);
+    glVertex3f(p.x, p.y, p.z);
+    #endif
+}
+
