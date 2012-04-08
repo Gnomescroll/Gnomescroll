641d9eb4925bdae08a07ef799bac99587b170719
Steve Leonard <sleonard76@gmail.com>
2012-04-07 17:20:53 -0700

    remove render

--- netclient/c_lib/items/items.hpp
+++ netclient/c_lib/items/items.hpp
@@ -13,6 +13,10 @@
 #include <c_lib/items/turret.hpp>
 #include <c_lib/items/pickup.hpp>
 
-#include <c_lib/items/inventory/inventory.hpp>
-#include <c_lib/items/inventory/render.hpp>
+
+#include <c_lib/items/inventory/contents.hpp>
+#include <c_lib/items/inventory/server.hpp>
+#include <c_lib/items/inventory/client.hpp>
+#include <c_lib/items/inventory/base.hpp>
 #include <c_lib/items/inventory/packets.hpp>
+#include <c_lib/items/inventory/inventory.hpp>
