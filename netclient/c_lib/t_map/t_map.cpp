fe1c48e4096b8695f334fc60b7ccd9ff5344e85e
Steve Leonard <sleonard76@gmail.com>
2012-04-08 12:12:30 -0700

    lower drop probability

--- netclient/c_lib/t_map/t_map.cpp
+++ netclient/c_lib/t_map/t_map.cpp
@@ -109,7 +109,7 @@ int apply_damage(int x, int y, int z, int dmg)
 // TODO: MOVE
 void block_spawn_items(int block_value, int x, int y, int z)
 {
-    const float drop_probability = 0.3f;
+    const float drop_probability = 0.1f;
     float p = randf();
     if (p > drop_probability) return;
 
