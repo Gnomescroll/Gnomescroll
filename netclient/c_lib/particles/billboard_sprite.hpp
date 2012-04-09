566a6e83840810202675427c38255462f8dca8c3
HaltingState <haltingstate@gmail.com>
2012-04-08 20:12:39 -0700

    server compiles and segfaults

--- netclient/c_lib/particles/billboard_sprite.hpp
+++ netclient/c_lib/particles/billboard_sprite.hpp
@@ -3,6 +3,7 @@
 namespace Particles
 {
 
+#ifdef DC_CLIENT
 class BillboardSprite
 {
     public:
@@ -14,5 +15,17 @@ class BillboardSprite
 
         BillboardSprite();
 };
+#else
+class BillboardSprite
+{
+    public:
+        float scale;
+        int texture_index;
 
+        void draw(Vec3 v) {};
+        void draw_from_bottom(Vec3 v) {};
+
+        BillboardSprite() {};
+};
+#endif
 }
