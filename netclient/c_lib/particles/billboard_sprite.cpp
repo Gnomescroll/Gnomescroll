07d4dab22022bdf1484b4a882f60d5bafa064bf3
HaltingState <haltingstate@gmail.com>
2012-04-09 16:14:53 -0700

    uber bugs

--- netclient/c_lib/particles/billboard_sprite.cpp
+++ netclient/c_lib/particles/billboard_sprite.cpp
@@ -7,11 +7,9 @@ namespace Particles
 void BillboardSprite::draw(Vec3 v)
 {
     #if DC_CLIENT
-/*
-    if (current_camera == NULL
-    || !current_camera->in_view(v.x, v.y, v.z))
+    if (point_fulstrum_test(v.x, v.y, v.z) == false)
         return;
-*/
+
     Vec3 up = vec3_init(
         model_view_matrix[0]*this->scale,
         model_view_matrix[4]*this->scale,
@@ -53,12 +51,10 @@ void BillboardSprite::draw_from_bottom(Vec3 v)
 
     //v.z += this->scale / 2;
     v.z += this->scale;
-
-/*
-    if (current_camera == NULL
-    || !current_camera->in_view(v.x, v.y, v.z))
+    
+    if (point_fulstrum_test(v.x, v.y, v.z) == false)
         return;
-*/
+
     Vec3 up = vec3_init(
         model_view_matrix[0]*this->scale,
         model_view_matrix[4]*this->scale,
