f9d04beb0874d16e08371cd870079aec6f918903
Steve Leonard <sleonard76@gmail.com>
2012-04-10 23:38:30 -0700

    all api method templates gone

--- netclient/c_lib/objects/common/api/tick.hpp
+++ netclient/c_lib/objects/common/api/tick.hpp
@@ -1,17 +1,17 @@
 #pragma once
 
+#include <c_lib/physics/vec3.hpp>
 #include <c_lib/t_map/t_map.hpp>
 
 void tickTTL(ObjectState* state)
-{
+{   // increment ttl
     if (state->ttl >= 0)
         state->ttl++;
 }
 
 int tickStayOnGround(ObjectState* state, Vec3 position)
-{
+{   // fall/climb with terrain
     #if DC_SERVER
-    // fall/climb with terrain
     int x = (int)position.x;
     int y = (int)position.y;
     int z = (int)position.z;
@@ -42,9 +42,8 @@ int tickStayOnGround(ObjectState* state, Vec3 position)
     #endif
 }
 
-// moves vector from origin to dest by momentum amount
 Vec3 tickMoveToPoint(Vec3 dest, Vec3 origin, Vec3 momentum)
-{
+{   // moves vector from origin to dest by momentum amount
     Vec3 direction = vec3_sub(dest, origin);
     normalize_vector(&direction);
     direction = vec3_mult(direction, momentum);             //  apply magnitude to velocity
@@ -52,17 +51,14 @@ Vec3 tickMoveToPoint(Vec3 dest, Vec3 origin, Vec3 momentum)
     return origin;
 }
 
-// return x-angle (theta) rotation between two Vec3s
 float tickOrientToPointTheta(Vec3 dest, Vec3 origin)
-{
+{   // return x-angle (theta) rotation between two Vec3s
     Vec3 direction = vec3_sub(dest, origin);
     return vec3_to_theta(direction);
 }
 
-//  x- and y-angle (theta,phi) rotation between two Vec3s
 void tickOrientToPointThetaPhi(Vec3 dest, Vec3 origin, float* theta, float* phi)
-{
+{   //  x- and y-angle (theta,phi) rotation between two Vec3s
     Vec3 direction = vec3_sub(dest, origin);
     vec3_to_angles(direction, theta, phi);
-
 }
