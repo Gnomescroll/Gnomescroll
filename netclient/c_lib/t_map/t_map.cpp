41413d3ff863a34a9d44227fcc6e16a63423e7b6
Steve Leonard <sleonard76@gmail.com>
2012-04-10 23:21:30 -0700

    gemstone spawns

--- netclient/c_lib/t_map/t_map.cpp
+++ netclient/c_lib/t_map/t_map.cpp
@@ -148,7 +148,7 @@ void apply_damage_broadcast(int x, int y, int z, int dmg, TerrainModificationAct
     msg.action = action;
     msg.broadcast();
 
-#if 0
+#if 1
     int block_value = get(x,y,z);
     block_spawn_items(block_value, x,y,z);
 #else
