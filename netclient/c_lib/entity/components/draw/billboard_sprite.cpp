3170d0bcac4438726d57c54dcd316b6c6818eb56
Steve Leonard <sleonard76@gmail.com>
2012-04-16 22:57:55 -0700

    disable sprites on server

--- netclient/c_lib/entity/components/draw/billboard_sprite.cpp
+++ netclient/c_lib/entity/components/draw/billboard_sprite.cpp
@@ -1,6 +1,11 @@
 #include "billboard_sprite.hpp"
 
+// TODO -- dont include this stuff on server at all
+
+#if DC_CLIENT
 #include <c_lib/draw/draw.hpp>
+#endif
+
 #include <c_lib/entity/components/physics.hpp>
 
 namespace Components
@@ -8,9 +13,11 @@ namespace Components
 
 void BillboardSpriteComponent::draw()
 {
+    #if DC_CLIENT
     PhysicsComponent* state = (PhysicsComponent*)this->object->get_component_interface(COMPONENT_INTERFACE_PHYSICS);
     if (state == NULL) return;
     drawBillboardSprite(state->get_position(), this->sprite_index, this->scale);
+    #endif
 }
 
 
