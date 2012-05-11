b14e63c78db25efc23660ee0bfdc722f0ceebced
HaltingState <haltingstate@gmail.com>
2012-05-11 07:16:59 -0700

    new grid dimensions

--- netclient/c_lib/t_hud/texture.cpp
+++ netclient/c_lib/t_hud/texture.cpp
@@ -22,35 +22,6 @@ void init_texture()
     init_item_sheet();
     init_nanite_texture();
 
-    SDL_Surface* s;
-
-
-    // Texture 2
-    s = create_surface_from_file((char*) "media/sprites/inventory_slots.png");
-    
-    if (s == NULL)
-    {
-        printf("t_hud: ItemGridTileTexture spritesheet load error\n");
-        assert(s != NULL);
-        return;
-    }
- 
-    glEnable(GL_TEXTURE_2D);
-    glGenTextures( 1, &ItemGridSlotTexture );
-    glBindTexture( GL_TEXTURE_2D, ItemGridSlotTexture  );
-
-    //glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
- 
-    GLuint internalFormat = GL_RGBA;
-    GLuint format = GL_RGBA;
-
-    glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, s->w, s->h, 0, format, GL_UNSIGNED_BYTE, s->pixels );
-    glDisable(GL_TEXTURE_2D);
-
-    ItemGridSlotSurface = s;
-
 }
 
 void init_item_sheet()
