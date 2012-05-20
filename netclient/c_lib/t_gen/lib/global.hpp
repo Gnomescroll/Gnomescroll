bbef96b009b96bccd094d153fbec13cecfbc2ecb
HaltingState <haltingstate@gmail.com>
2012-05-20 07:13:18 -0700

    stuff

--- netclient/c_lib/t_gen/lib/global.hpp
+++ netclient/c_lib/t_gen/lib/global.hpp
@@ -18,58 +18,29 @@ template<typename T> T Interpolate(const double& t, T a, T b) {return T(a+(b-a)*
 
 //#define _USE_MATH_DEFINES // Exposes  M_PI and M_SQRT2
 //#include <math.h> // For sqrt, hypot, M_PI and M_SQRT2
-#define M_SQRT3     1.7320508075688772935274463415059 // Add a few more defines suitable for long double:
-#define M_GOLD      1.6180339887498948482045868343656 // The Golden Ratio suitable for long double
+
+//#define M_SQRT3     1.7320508075688772935274463415059 // Add a few more defines suitable for long double:
+//#define M_GOLD      1.6180339887498948482045868343656 // The Golden Ratio suitable for long double
+
 //_____________________________________________________________________________
 
 
 template<typename T> T    Clamped(T lo, T it, T hi) {return Min(Max(lo,it),hi);} // useful for acos, asin etc. clamp(-1,1); // (me<lo) ? lo : ((me>hi) ? hi : me);}
 template<typename T> T        Min(T  a, T  b)       {return a<b ? a : b;}
 template<typename T> T        Max(T  a, T  b)       {return a>b ? a : b;}
-//template<typename T> T        Min(T  a, T  b, T  c) {return Min(Min(a,b),c);}
-//template<typename T> T        Max(T  a, T  b, T  c) {return Max(Max(a,b),c);}
-#if 0
-template<typename T> T   Saturate(      T it      ) {return Min(Max(0.0,it),1.0);} // Convention from GPU shaders
-template<typename T> T    Clamped(T lo, T it, T hi) {return Min(Max(lo,it),hi);} // useful for acos, asin etc. clamp(-1,1); // (me<lo) ? lo : ((me>hi) ? hi : me);}
-template<typename T> bool Between(T lo, T it, T hi) {return (lo<=it)&&(it<=hi);} // Inclusive
-template<typename T> bool  Within(T lo, T it, T hi) {return (lo< it)&&(it< hi);} // Exclusive
-#endif 
 
-/*
-template<typename T> T  Floor(T a) This is defined at the bottom of this file.
-template<typename T> T  Round(T a) This is defined at the bottom of this file.
-
-_________________________ Assembly Language Speed-ups _________________________
-
-Standard Floor method flushes the processor pipeline when rounding mode is set!
-If exactly in the middle (like 0.5) the FPU rounds to the nearest even number.
-Shift and scale this to the answer required. */
-static int Floor(double x) { return x>0 ? int(x) : int(x)-1;}
-
-/*
-  //ASSERT(a>(INT_MIN/2.0-1));
-  //ASSERT(a<(INT_MAX/2.0+1));
-  const double Half=-0.5;
-  int i;
-  __asm {
-    fld   a        //         a
-    fadd  st,st(0) //       2*a
-    fadd  Half     //       2*a-0.5   use fsubr instead to implement Ceil
-    fistp i        // Round(2*a-0.5)
-    sar   i,1      // Round(2*a-0.5)/2
-  }
-  return i;
+static int Floor(double x) 
+{ 
+  return x>0 ? int(x) : int(x)-1;
 }
-*/
 
-// Use the same trick for casting to integer types:
-static int Truncate(double a) { // c++ equivalent is return int(x);
+static int Truncate(double a) 
+{
     return int(a);
 }
-// Use the same trick for rounding:
-static int Round(double a) {
-  //ASSERT(a>(INT_MIN/2.0-1));
-  //ASSERT(a<(INT_MAX/2.0+1));
+
+static int Round(double a) 
+{
   const double Half=0.5;
     return int(a+Half);
 }
