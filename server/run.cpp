9928e381155d30535d6fef1fb069480e70dd29fe
Steve Leonard <sleonard76@gmail.com>
2012-05-22 07:34:31 -0700

    unbuffered stdout in server

--- server/run.cpp
+++ server/run.cpp
@@ -2,8 +2,13 @@
 #include <c_lib/main.cpp>
 #include <c_lib/common/lua/lua.hpp>
 
+#include <stdio.h>
+
 int main(int argc, char** argv)
 {
+    // set output unbuffered
+    setvbuf(stdout, NULL, _IONBF, 0);   // NULL,0 are ignored when _IONBF enabled
+    
     // program accepts one option: path to lua settings file
     // will use a default value if not provided
     if (argc > 1) LUA::set_options_file(argv[1]);
