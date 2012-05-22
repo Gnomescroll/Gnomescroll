167a9c917da88fda84c6c15d8e0c53e80445dda4
Steve Leonard <sleonard76@gmail.com>
2012-05-22 05:09:03 -0700

    fix store

--- netclient/c_lib/item/config/nanite_store_dat.hpp
+++ netclient/c_lib/item/config/nanite_store_dat.hpp
@@ -10,16 +10,16 @@ namespace Item
 
 void load_nanite_store()
 {
-    nanite_item_def("mining_laser", 1);
+    nanite_item_def("mining_laser", 3);
     nanite_item_set(0, 0,0);
 
-    nanite_item_def("laser_rifle", 5);
+    nanite_item_def("laser_rifle", 8);
     nanite_item_set(0, 1,0);
 
-    nanite_item_def("crate_3", 10);
+    nanite_item_def("crate_3", 20);
     nanite_item_set(0, 0,1);
 
-    nanite_item_def("block_placer", 20);
+    nanite_item_def("block_placer", 25);
     nanite_item_set(0, 1,1);
 }
 
