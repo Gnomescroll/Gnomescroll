274a9ac00bc58e064e24856142abe29fdae42eed
Steve Leonard <sleonard76@gmail.com>
2012-05-23 09:26:13 -0700

    run server forever log name improved

--- server/run_server_forever.py
+++ server/run_server_forever.py
@@ -11,13 +11,17 @@ if platform.system() != "Linux":
     sys.exit()
 
 subprocess.Popen('ulimit -c unlimited', shell=True)
-    
+
+with open('./VERSIONX.old') as f:
+    version = f.read().strip()
+
+print "Server version is ", version
 print "Starting eternal server"
 MAX_DUMPS = 100
 ct = 0
 while True:
     print '%d-th server run' % ct
-    logname = 'log%d' % (time.time(),)
+    logname = 'log-%s-%d' % (version, time.time(),)
     subprocess.call(['./m643_run_log.sh %s %d' % (logname, int(time.time()),)], shell=True)
     print "Server died"
     ct += 1
