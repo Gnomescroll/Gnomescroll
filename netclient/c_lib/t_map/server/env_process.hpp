6b0346d9d90456ba18fd3dd6f5a2d54c01d0ba19
Steve Leonard <sleonard76@gmail.com>
2012-05-27 04:30:02 -0700

    disable dead code

--- netclient/c_lib/t_map/server/env_process.hpp
+++ netclient/c_lib/t_map/server/env_process.hpp
@@ -5,18 +5,18 @@ namespace t_map
 void regolith_post_processing()
 {
 
-	int cube_id = dat_get_cube_id("regolith");
+    //int cube_id = dat_get_cube_id("regolith");
 
-	class MAP_CHUNK* c;
-	for(int i=0; i< )
+    //class MAP_CHUNK* c;
+    //for(int i=0; i< )
 
-	main_map->chunk[]
+    //main_map->chunk[]
 
 }
 
 void map_post_processing()
 {
-	regolith_post_processing();
+    regolith_post_processing();
 
 }
 
