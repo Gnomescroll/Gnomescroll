5c74bcb78d796210eaa24eafdcab0da5baaa763a
Steve Leonard <sleonard76@gmail.com>
2012-05-27 04:32:40 -0700

    script

--- netclient/shell/clean_osx_app.sh
+++ netclient/shell/clean_osx_app.sh
@@ -1,8 +1,3 @@
-cd ~/Desktop/Gnomescroll.app/Contents/Frameworks
-rm libGLEW.1.7.dylib
-cp /usr/local/lib/libGLEW.1.7.0.dylib .
-chown maslow:staff libGLEW.1.7.0.dylib
-chmod 755 libGLEW.1.7.0.dylib
-cd ../MacOS
+cd ~/Desktop/Gnomescroll.app/Contents/MacOS
 install_name_tool -change "/usr/local/lib/libGLEW.1.7.0.dylib" "@loader_path/../Frameworks/libGLEW.1.7.0.dylib" Gnomescroll
 install_name_tool -change "/usr/local/lib/libluajit-51.2.dylib" "@loader_path/../Frameworks/libluajit-51.2.dylib" Gnomescroll
---
+++ netclient/shell/pack_osx_app.sh
@@ -0,0 +1,3 @@
+cd ~/Desktop
+zip -r Gnomescroll.app.zip Gnomescroll.app/
+cp Gnomescroll.app.zip ~/Dropbox/Public/
