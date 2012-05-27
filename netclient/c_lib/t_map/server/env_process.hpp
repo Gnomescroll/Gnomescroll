9054239729e7923b449aff604445d18277ac461a
HaltingState <haltingstate@gmail.com>
2012-05-27 04:28:49 -0700

    ok

---
+++ netclient/c_lib/t_map/server/env_process.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace t_map
+{
+void regolith_post_processing()
+{
+
+	int cube_id = dat_get_cube_id("regolith");
+
+	class MAP_CHUNK* c;
+	for(int i=0; i< )
+
+	main_map->chunk[]
+
+}
+
+void map_post_processing()
+{
+	regolith_post_processing();
+
+}
+
+
+}
