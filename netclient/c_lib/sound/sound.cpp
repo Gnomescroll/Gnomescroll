d2eae3bb2a5309027f5821324d3645dc3bf95b45
Steve Leonard <sleonard76@gmail.com>
2012-05-30 11:35:51 -0700

    sound wrapped with quadrant functions

--- netclient/c_lib/sound/sound.cpp
+++ netclient/c_lib/sound/sound.cpp
@@ -1,13 +1,13 @@
 #include "sound.hpp"
 
 #include <physics/vec3.hpp>
+#include <physics/quadrant.hpp>
 #include <t_map/t_map.hpp>
 
 namespace Sound
 {
 
-struct Vec3 origin;
-Vec3 listener_position;
+struct Vec3 listener_position;
 
 void init()
 {
@@ -31,10 +31,8 @@ void set_volume(float vol)
 void update_listener(float x, float y, float z, float vx, float vy, float vz, float fx, float fy, float fz, float ux, float uy, float uz)
 {
     if (!Options::sound) return;
-    origin = vec3_init(map_dim.x/2, map_dim.y/2, 0);
-    //origin = vec3_init(0,0, 0);
     listener_position = vec3_init(x,y,z);
-    return OpenALSound::update_listener(origin.x, origin.y, z, vx,vy,vz, fx,fy,fz, ux,uy,uz);
+    return OpenALSound::update_listener(x,y,z, vx,vy,vz, fx,fy,fz, ux,uy,uz);
 }
 
 // Public
@@ -56,14 +54,10 @@ int play_3d_sound(char* fn, float x, float y, float z, float vx, float vy, float
 {
     if (!Options::sound) return -1;
 
-    // translate point to our origin
-    // handles map wrapping
-    Vec3 p = vec3_init(x,y,z);
-    p = vec3_sub(p, listener_position);
-    p = vec3_add(p, origin);
-    p = translate_position(p);
+    x = quadrant_translate_f(listener_position.x, x);
+    y = quadrant_translate_f(listener_position.y, y);
 
-    return OpenALSound::play_3d_sound(fn, p.x,p.y,p.z, vx,vy,vz);
+    return OpenALSound::play_3d_sound(fn, x,y,z, vx,vy,vz);
 }
 
 void close()
