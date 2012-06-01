c88400dded60f873641f8de72a65f264d1ecd395
Steve Leonard <sleonard76@gmail.com>
2012-06-01 16:55:44 -0700

    fix uninitialised voronoi

--- netclient/c_lib/t_gen/lib/voronoi2.cpp
+++ netclient/c_lib/t_gen/lib/voronoi2.cpp
@@ -3,8 +3,8 @@
 
 #include <t_gen/lib/voronoi.hpp>
 
-double Vo(double x, double y, double z, int a, int b) {
-
+double Vo(double x, double y, double z, int a, int b)
+{
     //BYTE B=BYTE(Round(255*Clamped(0.0, Voronoi::Get(x/10.0,y/10.0, 0.5, Voronoi::Difference21, Voronoi::Quadratic), 1.0))); // Turn the parameter into a Greyscale Level.
     //SetPixel(x,y, RGB(B,B,B));
 
@@ -14,54 +14,60 @@ double Vo(double x, double y, double z, int a, int b) {
     //enum DistanceMethod {Length, Length2, Manhattan, Chebychev, Quadratic, Minkowski4, Minkowski5};
 
 
-    enum Voronoi::VoronoiType A;
-    switch(a) {
+    enum Voronoi::VoronoiType A = Voronoi::First;
+    switch(a)
+    {
         case 0:
-        A = Voronoi::First;
-        break;
+            A = Voronoi::First;
+            break;
         case 1:
-        A = Voronoi::Second;
-        break;
+            A = Voronoi::Second;
+            break;
         case 2:
-        A = Voronoi::Third;
-        break;
+            A = Voronoi::Third;
+            break;
         case 3:
-        A = Voronoi::Fourth;
-        break;
+            A = Voronoi::Fourth;
+            break;
         case 4:
-        A = Voronoi::Difference21;
-        break;
+            A = Voronoi::Difference21;
+            break;
         case 5:
-        A = Voronoi::Difference32;
-        break;
+            A = Voronoi::Difference32;
+            break;
         case 6:
-        A = Voronoi::Crackle;
-        break;
-        printf("option %i not valid for a\n", a);
-        A = Voronoi::First;
+            A = Voronoi::Crackle;
+            break;
+
+        default:
+            printf("option %i not valid for a\n", a);
+            A = Voronoi::First;
+            break;
     }
     //enum DistanceMethod {Length, Length2, Manhattan, Chebychev, Quadratic, Minkowski4, Minkowski5};
 
-    enum Voronoi::DistanceMethod B;
+    enum Voronoi::DistanceMethod B = Voronoi::Length;
 
-    switch(b) {
+    switch(b)
+    {
         case 0:
-        B = Voronoi::Length;break;
+            B = Voronoi::Length;break;
         case 1:
-        B = Voronoi::Length2;break;
+            B = Voronoi::Length2;break;
         case 2:
-        B = Voronoi::Manhattan;break;
+            B = Voronoi::Manhattan;break;
         case 3:
-        B = Voronoi::Chebychev;break;
+            B = Voronoi::Chebychev;break;
         case 4:
-        B = Voronoi::Quadratic;break;
+            B = Voronoi::Quadratic;break;
         case 5:
-        B = Voronoi::Minkowski4;break;
+            B = Voronoi::Minkowski4;break;
         case 6:
-        B = Voronoi::Minkowski5;break;
+            B = Voronoi::Minkowski5;break;
         default:
-        printf("option %i not valid for b\n", b);
-        B = Voronoi::Length;
+            printf("option %i not valid for b\n", b);
+            B = Voronoi::Length;
+            break;
     }
 
     return Voronoi::Get(x,y,z, A,B);
