78df1ec006f76a699de0c4071646ea741319337d
Steve Leonard <sleonard76@gmail.com>
2012-06-07 20:37:13 -0700

    improved heightmap gen

--- src/c_lib/t_gen/noise_map2.hpp
+++ src/c_lib/t_gen/noise_map2.hpp
@@ -11,7 +11,7 @@ __attribute((always_inline)) static float fade(float t);
 
 static float dot(float* g, float x, float y)
 {
-	return g[0]*x + g[1]*y;
+    return g[0]*x + g[1]*y;
 }
 
 static float mix(float a, float b, float t) 
@@ -21,7 +21,7 @@ static float mix(float a, float b, float t)
 
 static float fade(float t) 
 {
-	return t*t*t*(t*(t*6-15)+10);
+    return t*t*t*(t*(t*6-15)+10);
 }
 
 
@@ -34,7 +34,7 @@ class PerlinField2D
     //static const int ssize = 64*64*32;
     //static const int xsize = 64;
 
-    int ssize;	//number of gradients
+    int ssize;  //number of gradients
 
     int xsize;
 
@@ -47,12 +47,12 @@ class PerlinField2D
 
     void init(int _xsize, int _grad_max)
     {
-    	if(_xsize < 1) GS_ABORT();
+        if(_xsize < 1) GS_ABORT();
 
-    	xsize = _xsize;
+        xsize = _xsize;
         ssize = xsize*xsize;
 
-       	grad_max = _grad_max;
+        grad_max = _grad_max;
         //init_genrand(seed);
         init_genrand(rand());
 
@@ -76,7 +76,7 @@ class PerlinField2D
     #if 1
         for(int i=0; i<grad_max; i++)
         {
-        	float t = 6.28318531*i* (1.0 / ((float) grad_max));
+            float t = 6.28318531*i* (1.0 / ((float) grad_max));
             float x = sin(t);
             float y = cos(t);
 
@@ -158,180 +158,180 @@ float base(float x, float y)
 
 class PerlinOctave2D
 {
-	public:
-	int octaves;
-	class PerlinField2D* octave_array;
+    public:
+    int octaves;
+    class PerlinField2D* octave_array;
 
-	PerlinOctave2D(int _octaves)
-	{
-		octaves = _octaves;
-		octave_array = new PerlinField2D[octaves];
+    PerlinOctave2D(int _octaves)
+    {
+        octaves = _octaves;
+        octave_array = new PerlinField2D[octaves];
 
-		//for(int i=0; i<octaves; i++) octave_array[i].init(pow(2,i+2), 15);
-		for(int i=0; i<octaves; i++) octave_array[i].init(2*(i+1)+1, 4);
-		//for(int i=0; i<octaves; i++) octave_array[i].init((i*(i+1))+1, 4);
-	}
+        //for(int i=0; i<octaves; i++) octave_array[i].init(pow(2,i+2), 15);
+        for(int i=0; i<octaves; i++) octave_array[i].init(2*(i+1)+1, 4);
+        //for(int i=0; i<octaves; i++) octave_array[i].init((i*(i+1))+1, 4);
+    }
 
-	~PerlinOctave2D()
-	{
-		delete[] octave_array;
-	}
+    ~PerlinOctave2D()
+    {
+        delete[] octave_array;
+    }
 
-	void save_octaves()
-	{
+    void save_octaves()
+    {
 
-		const int xres = 512;
-		const int yres = 512;
+        const int xres = 512;
+        const int yres = 512;
 
-		const float xresf = 1.0 / ((float) xres);
-		const float yresf = 1.0 / ((float) yres);
+        const float xresf = 1.0 / ((float) xres);
+        const float yresf = 1.0 / ((float) yres);
 
-	    float* out = new float[xres*yres*octaves];
+        float* out = new float[xres*yres*octaves];
 
-	    class PerlinField2D* m;
-		for(int k=0; k<octaves; k++)
-		{
-			m = &octave_array[k];
-			int zoff = k*xres*yres;
-		    for(int i=0; i<xres; i++)
-		    for(int j=0; j<yres; j++)
-		    {
-		        float x = i*xresf;
-		        float y = j*yresf;
+        class PerlinField2D* m;
+        for(int k=0; k<octaves; k++)
+        {
+            m = &octave_array[k];
+            int zoff = k*xres*yres;
+            for(int i=0; i<xres; i++)
+            for(int j=0; j<yres; j++)
+            {
+                float x = i*xresf;
+                float y = j*yresf;
 
-		        out[i+j*yres+zoff] = m->base(x,y);
-		    }
-		}
+                out[i+j*yres+zoff] = m->base(x,y);
+            }
+        }
 
-		save_perlin("octave_map_01", out, xres, yres*octaves);
+        save_perlin("octave_map_01", out, xres, yres*octaves);
 
-		for(int k=0; k<octaves; k++)
-	    for(int i=0; i<xres; i++)
-	    for(int j=0; j<yres; j++)
-	    {
-	        out[i+j*yres+k*xres*yres] = 0.0;
-	    }
+        for(int k=0; k<octaves; k++)
+        for(int i=0; i<xres; i++)
+        for(int j=0; j<yres; j++)
+        {
+            out[i+j*yres+k*xres*yres] = 0.0;
+        }
 
-		for(int k=0; k<octaves; k++)
-		{
+        for(int k=0; k<octaves; k++)
+        {
 
-			for(int k2=0; k2< k; k2++)
-			{
-				m = &octave_array[k2];
-				float p = 1.0;
-				int zoff = k*xres*yres;
+            for(int k2=0; k2< k; k2++)
+            {
+                m = &octave_array[k2];
+                float p = 1.0;
+                int zoff = k*xres*yres;
 
-			    for(int i=0; i<xres; i++)
-			    for(int j=0; j<yres; j++)
-			    {
-			        float x = i*xresf;
-			        float y = j*yresf;
+                for(int i=0; i<xres; i++)
+                for(int j=0; j<yres; j++)
+                {
+                    float x = i*xresf;
+                    float y = j*yresf;
 
-			        out[i+j*yres+zoff] += p*m->base(x,y);
-			    }
+                    out[i+j*yres+zoff] += p*m->base(x,y);
+                }
 
-			    p *= 0.50;
-			}
-		}
-
-
-    	//save_png("octave_map_01", out, xres, yres*octaves);
-		save_perlin("octave_map_02", out, xres, yres*octaves);
-		//void save_png(const char* filename, float* in, int xres, int yres)
+                p *= 0.50;
+            }
+        }
 
 
-	}
-
-	void save_octaves2()
-	{
-
-		const int xres = 512;
-		const int yres = 512;
-
-		const float xresf = 1.0 / ((float) xres);
-		const float yresf = 1.0 / ((float) yres);
-
-		const int DEGREE = octaves; //8;
-
-		//(i + xres*DEGREE*n)+ (j*xres*yres*DEGREE)
-	    float* out = new float[xres*yres*octaves*DEGREE];
-
-	    int line_width = xres*DEGREE;
-
-	    for(int i=0; i<xres*yres*octaves*DEGREE; i++) out[i] = 0.0;
-
-	    class PerlinField2D* m;
+        //save_png("octave_map_01", out, xres, yres*octaves);
+        save_perlin("octave_map_02", out, xres, yres*octaves);
+        //void save_png(const char* filename, float* in, int xres, int yres)
 
 
-		for(int k=0; k<octaves; k++)
-		{
-			m = &octave_array[k];
-			int zoff = k*yres*line_width;
+    }
 
-		    for(int i=0; i<xres; i++)
-		    for(int j=0; j<yres; j++)
-		    {
-		        float x = i*xresf;
-		        float y = j*yresf;
+    void save_octaves2()
+    {
 
-		        out[i+j*line_width+zoff] += m->base(x,y);
-		    }
-		}
+        const int xres = 512;
+        const int yres = 512;
 
-	    for(int n=1; n<DEGREE; n++)
-	    {
-	    	float PERSISTANCE = (n+1)*(1.0/( (float) DEGREE ));
+        const float xresf = 1.0 / ((float) xres);
+        const float yresf = 1.0 / ((float) yres);
 
-	    	int xoff = n*xres;
+        const int DEGREE = octaves; //8;
 
-			for(int k=0; k<octaves; k++)
-			{
-				int yoff = k*yres*line_width;
-				float p = 1.0;
+        //(i + xres*DEGREE*n)+ (j*xres*yres*DEGREE)
+        float* out = new float[xres*yres*octaves*DEGREE];
 
-				for(int k2=0; k2 <= k; k2++)
-				{
-					m = &octave_array[k2];
+        int line_width = xres*DEGREE;
 
-				    for(int i=0; i<xres; i++)
-				    for(int j=0; j<yres; j++)
-				    {
-				        float x = i*xresf;
-				        float y = j*yresf;
+        for(int i=0; i<xres*yres*octaves*DEGREE; i++) out[i] = 0.0;
 
-				        out[ (i+xoff)+ (j*line_width+yoff) ] += p*m->base(x,y);
-				    }
-
-				    p *= PERSISTANCE;
-				}
-			}
-
-		}
-    	//save_png("octave_map_01", out, xres, yres*octaves);
-		save_perlin("octave_map_03", out, xres*DEGREE, yres*octaves);
-		//void save_png(const char* filename, float* in, int xres, int yres)
+        class PerlinField2D* m;
 
 
-	}
+        for(int k=0; k<octaves; k++)
+        {
+            m = &octave_array[k];
+            int zoff = k*yres*line_width;
+
+            for(int i=0; i<xres; i++)
+            for(int j=0; j<yres; j++)
+            {
+                float x = i*xresf;
+                float y = j*yresf;
+
+                out[i+j*line_width+zoff] += m->base(x,y);
+            }
+        }
+
+        for(int n=1; n<DEGREE; n++)
+        {
+            float PERSISTANCE = (n+1)*(1.0/( (float) DEGREE ));
+
+            int xoff = n*xres;
+
+            for(int k=0; k<octaves; k++)
+            {
+                int yoff = k*yres*line_width;
+                float p = 1.0;
+
+                for(int k2=0; k2 <= k; k2++)
+                {
+                    m = &octave_array[k2];
+
+                    for(int i=0; i<xres; i++)
+                    for(int j=0; j<yres; j++)
+                    {
+                        float x = i*xresf;
+                        float y = j*yresf;
+
+                        out[ (i+xoff)+ (j*line_width+yoff) ] += p*m->base(x,y);
+                    }
+
+                    p *= PERSISTANCE;
+                }
+            }
+
+        }
+        //save_png("octave_map_01", out, xres, yres*octaves);
+        save_perlin("octave_map_03", out, xres*DEGREE, yres*octaves);
+        //void save_png(const char* filename, float* in, int xres, int yres)
 
 
-	float sample(float x, float y, float persistance)
-	{	
-		float p = 1.0;
-		float tmp = 0.0;
-		for(int i=0; i<octaves; i++)
-		{
-			tmp += octave_array[i].base(x,y);
-			p *= persistance;
-		}
-		return tmp;
-	}
+    }
+
+
+    float sample(float x, float y, float persistance)
+    {   
+        float p = 1.0;
+        float tmp = 0.0;
+        for(int i=0; i<octaves; i++)
+        {
+            tmp += octave_array[i].base(x,y);
+            p *= persistance;
+        }
+        return tmp;
+    }
 };
 
 
 void test_octave_2d()
 {
-	return;
+    return;
     PerlinOctave2D m(8);
 
     m.save_octaves2();
@@ -343,12 +343,17 @@ void test_octave_2d_map_gen(int tile)
     PerlinOctave2D m1(8);
     PerlinOctave2D m2(8);
 
-	const int xres = 512;
-	const int yres = 512;
+    const int xres = 512;
+    const int yres = 512;
 
-	const float xresf = 1.0 / ((float) xres);
-	const float yresf = 1.0 / ((float) yres);
+    const float xresf = 1.0 / ((float) xres);
+    const float yresf = 1.0 / ((float) yres);
 
+    float* values = (float*)malloc(512*512*sizeof(float));
+
+    float min_value_generated = 100000;
+    float max_value_generated = -100000;
+    
     for(int i=0; i<512; i++)
     for(int j=0; j<512; j++)
     {
@@ -361,15 +366,56 @@ void test_octave_2d_map_gen(int tile)
         //if(roughness < 0) roughness = 0.0;
         if(roughness < 0) roughness *= -1.0/32.0;
 
-        float value = 32 + 32*roughness*m1.sample(x,y, 0.125);
-
+        float value = 32 + 32.0f*roughness*m1.sample(x,y, 0.125);
+        if (value < min_value_generated) min_value_generated = value;
+        if (value > max_value_generated) max_value_generated = value;
+        values[i+512*j] = value;
+        
         //value += 32*oct_0.sample(x, y, 32.5, PERSISTANCE);
 
-        for (int k=0; k<value; k++) t_map::set(i,j,k,tile);
+        //for (int k=0; k<value; k++) t_map::set(i,j,k,tile);
 
 
         //out[i+j*yres] = p3d.one_over_f(x,y,64.0);
     }
 
+    GS_ASSERT(min_value_generated < 0);
+    GS_ASSERT(max_value_generated > 0);
+
+    float min_value_found = 10000.0f;
+
+    const int max_depth = -24;
+    const int max_height = 64;
+
+    float min_scale = 1.0f;
+    if (min_value_generated < max_depth) min_scale = max_depth / min_value_generated;
+    GS_ASSERT(min_scale >= 0.0f);
+    float max_scale = 1.0f;
+    if (max_value_generated > max_height) max_scale = max_height / max_value_generated;
+    GS_ASSERT(max_scale >= 0.0f);
+    for(int i=0; i<512; i++)
+    for(int j=0; j<512; j++)
+    {
+        float v = values[i+512*j];
+        if (v > 0) v *= max_scale;
+        if (v < 0) v *= min_scale;
+        if (v < min_value_found) min_value_found = v;
+        values[i+512*j] = v;
+    }
+
+    GS_ASSERT(min_value_found >= max_depth);
+
+    const int floor_height = 32;
+    int baseline = floor_height - min_value_found;
+
+    for(int i=0; i<512; i++)
+    for(int j=0; j<512; j++)
+    {
+        for (int k=0; k<baseline+values[i+512*j]; k++)
+            t_map::set(i,j,k,tile);
+    }
+
+    free(values);
 }
+
 }
