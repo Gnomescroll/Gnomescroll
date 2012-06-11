4edbb572dbf8c03ad65069c202a2532833a16679
HaltingState <haltingstate@gmail.com>
2012-06-11 08:37:09 -0700

    stuff

--- src/c_lib/t_map/t_vbo_class.hpp
+++ src/c_lib/t_map/t_vbo_class.hpp
@@ -92,7 +92,7 @@ class Vbo_map
     }
 
     #define MAP_VBO_CULLING 1
-    #define MAP_VBO_CULLING_RECYCLE 0
+    #define MAP_VBO_CULLING_RECYCLE 0   //delete and free vbos out of range
     //update all VBOs that need updating
     void update_map()
     {
