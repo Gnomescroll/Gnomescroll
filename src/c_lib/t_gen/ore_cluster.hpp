0421e1aa92f4edd7befb7c944d5110ccd6257dbb
HaltingState <haltingstate@gmail.com>
2012-06-15 23:58:45 -0700

    Stuff

--- src/c_lib/t_gen/ore_cluster.hpp
+++ src/c_lib/t_gen/ore_cluster.hpp
@@ -31,12 +31,12 @@ void populate_ore()
 {
 	populate_ore_veins(4096, "methane_ice");
 	populate_ore_veins(4096, "iron_ore");
-	
+
 	populate_ore_veins(2048, "graphite");
 
 	populate_ore_veins(2048, "copper_ore");
 	populate_ore_veins(2048, "gallium_ore");
-	populate_ore_veins(2048, "iridium_ore");
+	populate_ore_veins(1024, "iridium_ore");
 }
 
 /*
