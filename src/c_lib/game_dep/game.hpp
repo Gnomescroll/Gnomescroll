cad683107cbe704d5faa40c58b55877a87f6b9a2
Steve Leonard <sleonard76@gmail.com>
2012-06-19 07:37:25 -0700

    mv

--- src/c_lib/game_dep/game.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <common/defines.h>
-
-const unsigned int GAME_MAX_PLAYERS = PLAYERS_MAX;
-const unsigned int N_TEAMS = 1;
-const int NO_TEAM = 255;
-const bool use_ctf_flags = false;
