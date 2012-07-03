a0bb90811edb6d347b74563e471e8dfe4de76619
HaltingState <haltingstate@gmail.com>
2012-07-03 14:42:47 -0700

    stuff

--- src/c_lib/common/mallox.hpp
+++ src/c_lib/common/mallox.hpp
@@ -16,3 +16,8 @@ return; \
 #else
 #define MALLOX(type, array_name, size) type* array_name = (type*) _alloca(size);
 #endif
+
+define MALLOX(type, array_name, size) \
+static type* array_name = NULL\
+static array_name##__max int = 0;\
+static array_name##__index int = 0; \
