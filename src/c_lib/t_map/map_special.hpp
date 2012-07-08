18a9b2fe45ffdf08d4b7ebd17da42d811a889e5d
HaltingState <haltingstate@gmail.com>
2012-07-08 12:21:59 -0700

    adding control points

--- src/c_lib/t_map/map_special.hpp
+++ src/c_lib/t_map/map_special.hpp
@@ -16,7 +16,7 @@ class CONTROL_POINT_LIST
 
 	int cpi; //control point index
 	int cpm; //control point max
-	class CONTROL_POINT cpa; //control point array;
+	class CONTROL_POINT* cpa; //control point array;
 
 	CONTROL_POINT_LIST()
 	{
@@ -24,6 +24,8 @@ class CONTROL_POINT_LIST
 		cpm = 8;
 		cpa = (CONTROL_POINT*) malloc(8*sizeof(class CONTROL_POINT));
 	}
+
+	~CONTROL_POINT_LIST() {}
 
 	void add_control_point(int x, int y, int z)
 	{
@@ -37,8 +39,18 @@ class CONTROL_POINT_LIST
 		if(cpi == cpm)
 		{
 			cpm *= 2;
-			cpa = realloc(cpa, cpm*sizeof(class CONTROL_POINT));
+			cpa = (CONTROL_POINT*) realloc(cpa, cpm*sizeof(class CONTROL_POINT));
 		}
 	}
 
-}
+	bool control_point_in_range_check(int x, int y, int z)
+	{
+		for(int i=0; i<cpi; i++)
+		{
+			if( abs(x-cpa[i].x) <= 6 && abs(y-cpa[i].y) <= 6 && abs(z-cpa[i].z) <= 6 )	
+				return true;
+		}
+
+		return false;
+	}
+};
