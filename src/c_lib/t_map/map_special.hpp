bb4b51f2e01305664f6e4bd27ce206ce3fc71329
HaltingState <haltingstate@gmail.com>
2012-07-08 12:36:03 -0700

    map control points

--- src/c_lib/t_map/map_special.hpp
+++ src/c_lib/t_map/map_special.hpp
@@ -43,6 +43,23 @@ class CONTROL_POINT_LIST
 		}
 	}
 
+	void remove_control_point(int x, int y, int z)
+	{
+		for(int i=0; i<cpi; i++)
+		{
+			if(x==cpa[i].x && y==cpa[i].y && z==cpa[i].z)
+			{
+				cpa[i] = cpa[cpi-1];
+				cpi--;
+				GS_ASSERT(cpi >= 0);
+			}
+		}
+
+		printf("Error: tried to remove control point that does not exist!\n");
+		GS_ABBORT(); //should never reach this point
+	}
+
+
 	bool control_point_in_range_check(int x, int y, int z)
 	{
 		for(int i=0; i<cpi; i++)
@@ -53,4 +70,43 @@ class CONTROL_POINT_LIST
 
 		return false;
 	}
+
+#if DC_SERVER
+
+	void send_control_points_to_client(int client_id)
+	{
+		for(int i=0; i<cpi; i++)
+		{
+			control_point_create_StoC p;
+			p.x = cpa[i].x;
+			p.y = cpa[i].y;
+			p.z = cpa[i].z;
+			p.sendToClient(client_id);
+		}
+	}
+
+	void server_add_control_point(int x, int y, int z)
+	{
+		self->add_control_point(x,y,z);
+
+		control_point_create_StoC p;
+		p.x = x;
+		p.y = y;
+		p.z = z;
+		p.sendToClient(client_id);
+	}
+
+	void server_remove_control_point(int x, int y, int z)
+	{
+		remove_control_point(int x, int y, int z)
+
+		control_point_delete_StoC p;
+		p.x = x;
+		p.y = y;
+		p.z = z;
+		p.broadcast();
+	}
+
+#endif
+
 };
