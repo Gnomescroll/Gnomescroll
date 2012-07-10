1fe2502557846094e12d5016e7cf486a53018c69
Steve Leonard <sleonard76@gmail.com>
2012-07-10 05:07:22 -0500

    .

--- netclient/settings/settings_server.lua
+++ netclient/settings/settings_server.lua
@@ -22,7 +22,7 @@ options = {}
 options.server_name = "server_one"
 
 -- server --
-options.ip_address = "0.0.0.0"
+options.ip_address = "174.37.26.119"
 options.port = 0
 
 -- map --
@@ -31,7 +31,7 @@ options.seed = 80075112
 
 -- logger --
 options.logger = true
---options.log_chat = false
+options.log_chat = true
 
 ffi = require("ffi")
 ffi.cdef[[
