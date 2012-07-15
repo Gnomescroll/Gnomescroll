b6c412b762b52fe30cd0cb7fd57fce63676a108f
HaltingState <haltingstate@gmail.com>
2012-07-15 05:03:04 -0700

    Cleanup

--- src/c_lib/t_mob/_interface.hpp
+++ src/c_lib/t_mob/_interface.hpp
@@ -25,7 +25,7 @@ class cBone
 	//mat4 Offset, LocalTransform, GlobalTransform, OriginalLocalTransform;
 	cBone* parent;
 
-	cBone** bca; //bone child array
+	cBone* bca; //bone child array
 	int bcan; //bone child array index
 	//std::vector<cBone*> Children;
 
@@ -33,35 +33,58 @@ class cBone
 	{
 		parent = NULL;
 		name = NULL;
+
+		bca = NULL;
+		bcan = 0;
 	}
 	
 
 	~cBone() {}
+
+	void set_children(int num)
+	{
+		GS_ASSERT(bca == NULL)
+		bcan = num;
+		bca = new cBone[num];
+	}
 };
 
 
-// there is some type of alignment issue with my mat4 and the aimatrix4x4 class, so the copy must be manually
-void ConvertMatrix(struct Mat4* out, const aiMatrix4x4& in)
+void Mat4_transpose(struct Mat4 m1)
 {
-	out._11 = in.a1;
-	out._12 = in.a2;
-	out._13 = in.a3;
-	out._14 = in.a4;
+	struct Mat4 m2;
 
-	out._21 = in.b1;
-	out._22 = in.b2;
-	out._23 = in.b3;
-	out._24 = in.b4;
+	for(int i=0; i<4; i++)
+	{
+		for(int j=0; j<4; j++)
+		{
+			m2[i][j] = m1[j][i];
+		}
+	}
+	return m2;
+}
+// there is some type of alignment issue with my mat4 and the aimatrix4x4 class, so the copy must be manually
+void ConvertMatrix(struct Mat4& out, const aiMatrix4x4& in)
+{
+	out[0][0] = in.a1;
+	out[0][1] = in.a2;
+	out[0][2] = in.a3;
+	out[0][3] = in.a4;
 
-	out._31 = in.c1;
-	out._32 = in.c2;
-	out._33 = in.c3;
-	out._34 = in.c4;
+	out[1][0] = in.b1;
+	out[1][1] = in.b2;
+	out[1][2] = in.b3;
+	out[1][3] = in.b4;
 
-	out._41 = in.d1;
-	out._42 = in.d2;
-	out._43 = in.d3;
-	out._44 = in.d4;
+	out[2][0] = in.c1;
+	out[2][1] = in.c2;
+	out[2][2] = in.c3;
+	out[2][3] = in.c4;
+
+	out[3][0] = in.d1;
+	out[3][1] = in.d2;
+	out[3][2] = in.d3;
+	out[3][3] = in.d4;
 }
 
 class MeshAnimation
@@ -69,9 +92,8 @@ class MeshAnimation
 	public:
 // ------------------------------------------------------------------------------------------------
 // Recursively creates an internal node structure matching the current scene and animation.
-	class cBone* CreateBoneTree( aiNode* pNode, cBone* pParent)
+	class cBone* CreateBoneTree(class cBone* bone, aiNode* pNode, cBone* pParent)
 	{
-		class cBone* bone = new cBone();// create a node
 		bone->name = pNode->mName.data;// get the name of the bone
 		bone->parent = pParent; //set the parent, in the case this is the root node, it will be null
 
@@ -80,18 +102,50 @@ class MeshAnimation
 		//set bone name lookup
 
 
-		TransformMatrix(bone->LocalTransform, pNode->mTransformation);
-		bone->LocalTransform.Transpose();
+		ConvertMatrix(bone->LocalTransform, pNode->mTransformation);
+		bone->LocalTransform = Mat4_transpose(bone->LocalTransform);	//combine conversion and transpose?
+
 		bone->OriginalLocalTransform = bone->LocalTransform;// a copy saved
+		
 		CalculateBoneToWorldTransform(bone);
 
 		// continue for all child nodes and assign the created internal nodes as our children
 		// recursivly call this function on all children
-		for( unsigned int a = 0; a < pNode->mNumChildren; a++)
+
+		bone->set_children(pNode->mNumChildren);
+
+		for(int i=0; ii < pNode->mNumChildren; i++)
 		{
-			bone->Children.push_back(CreateBoneTree( pNode->mChildren[a], bone));
+			CreateBoneTree( &bone->bca[i], pNode->mChildren[a], bone);
 		}
-		return bone;
 	}
 
+};
+
+/*
+	This is slow and does ~n^2 more matrix multiplications than it should in tree depth
+*/
+
+// Calculates the global transformation matrix for the given internal node
+void CalculateBoneToWorldTransform(class cBone* bone)
+{
+	bone->global_transform = bone->local_transform;
+	class cBone* parent = bone->parent;
+
+	// this will climb the nodes up along through the parents concentating all the matrices to get the Object to World transform, or in this case, the Bone To World transform
+	while( parent )
+	{
+		bone->GlobalTransform = mat4_mult(bone->GlobalTransform, parent->LocalTransform);
+		parent = parent->parent; // get the parent of the bone we are working on 
+	}
 }
+
+// Recursively updates the internal node transformations from the given matrix array
+void UpdateTransforms(cBone* pNode) 
+{
+	CalculateBoneToWorldTransform( pNode);// update global transform as well
+
+	for(int i=0; i<pNode->bcan; i++)
+		UpdateTransforms(pNode->bca[i]);
+}
+
