62f38d599412edd9e54d7778176f41b8cf1b22ee
HaltingState <haltingstate@gmail.com>
2012-08-03 17:13:17 -0700

    stuff

---
+++ src/c_lib/t_mech/deprecated/_include.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+/*
+	CLIENT
+*/
+#ifdef DC_CLIENT
+
+#include <t_mech/state.cpp>
+#include <t_mech/draw.cpp>
+
+#endif
+
+/*
+	SERVER
+*/
+
+#ifdef DC_SERVER
+
+#include <t_mech/state.cpp>
+	
+#endif
---
+++ src/c_lib/t_mech/net/t_CtoS.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "t_net.hpp"
+
+
+namespace t_map
+{
+    
+class request_block_damage_CtoS: public MapMessagePacketToServer<request_block_damage_CtoS>
+{
+    public:
+        uint16_t x,y;
+        uint8_t z;
+        uint8_t request_id;
+        
+    inline void packet(char* buff, unsigned int* buff_n, bool pack)
+    {
+        pack_u16(&x, buff, buff_n, pack);
+        pack_u16(&y, buff, buff_n, pack);
+        pack_u8(&z, buff, buff_n, pack);
+        pack_u8(&request_id, buff, buff_n, pack);
+    }
+    
+    inline void handle();
+};
+
+}   // t_map
