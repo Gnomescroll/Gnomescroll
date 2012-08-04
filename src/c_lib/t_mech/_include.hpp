afbe8ddb710db2bfebf71822605880e67f758c29
HaltingState <haltingstate@gmail.com>
2012-08-03 18:38:41 -0700

    compiling

--- src/c_lib/t_mech/_include.hpp
+++ src/c_lib/t_mech/_include.hpp
@@ -2,15 +2,13 @@
 
 #include "_interface.hpp"
 
+#include <t_mech/net/CtoS.hpp>
+#include <t_mech/net/StoC.hpp>
 
 #ifdef DC_SERVER
-
 #include <t_mech/mech_state.hpp>
-
 #endif
 
 #ifdef DC_CLIENT
-
 #include <t_mech/mech_draw.hpp>
-
 #endif
