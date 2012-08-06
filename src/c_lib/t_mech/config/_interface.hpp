713ad198004d9c8930ed46555ce7e817353d5555
HaltingState <haltingstate@gmail.com>
2012-08-06 09:50:21 -0700

    Stuff

--- src/c_lib/t_mech/config/_interface.hpp
+++ src/c_lib/t_mech/config/_interface.hpp
@@ -1,8 +1,76 @@
 #pragma once
 
+#include <t_mech/common/common.hpp>
+#include <t_mech/config/_interface.hpp>
+#include <t_mech/properties.hpp>
+
+/*
+struct MECH_ATTRIBUTE
+{
+    int mech_type;
+    int mech_type_class;
+    int render_type;
+    int sprite_index;
+};
+
+enum MECH_CLASS
+{
+	MECH_CRYSTAL,
+	MECH_CROP,
+	MECH_WIRE,
+	MECH_SWITCH
+};
+
+enum MECH_RENDER_TYPE
+{
+    MECH_RENDER_TYPE_0, //X shaped
+    MECH_RENDER_TYPE_1, //crop/wheat type
+    MECH_RENDER_TYPE_2, //mycelium/ladder type
+};
+*/
 
 namespace t_mech
 {
+	int _current_mech_index -1;
+	struct MECH_ATTRIBUTE s;
 
+	void start_mech_dat()
+	{
+		_current_mech_index = -1;
+		s.mech_type = -1;
+		s.mech_type_class = -1;
+		s.render_type != -1;
+		s.sprite_index = 255;
+	}
 
+	void _push_mech()
+	{
+		GS_ASSERT(s.sprite_index != 255);
+		GS_ASSERT(s.mech_type_class != -1);
+		GS_ASSERT(s.render_type != -1);
+		mech_attribute[_current_mech_index] = s;
+
+		//reset
+		s.mech_type = -1;
+		s.mech_type_class = -1;
+		s.render_type != -1;
+		s.sprite_index = 255;
+	}
+
+	void mech_def(int mech_type, int mech_type_class, const char* name)
+	{
+		if(_current_mech_index != -1) 
+			_push_mech();
+		GS_ASSERT(mech_attribute[mech_type].mech_type == -1);
+		s.mech_type = mech_type;
+		s.mech_type_class = mech_type_class;
+
+		_current_mech_index = mech_type;
+	}
+
+	void end_mech_dat()
+	{
+		_push_mech();
+
+	}
 }
--- src/c_lib/t_mech/config/mech_dat.hpp
+++ src/c_lib/t_mech/config/mech_dat.hpp
@@ -1,8 +1,59 @@
 #pragma once
 
+#include <t_mech/config/mech_dat.hpp>
+
 namespace t_mech
 {
 
+/*
+struct MECH_ATTRIBUTE
+{
+    int mech_type;
+    int mech_type_class;
+    int render_type;
+    int sprite_index;
+};
 
-	
+enum MECH_CLASS
+{
+	MECH_CRYSTAL,
+	MECH_CROP,
+    MECH_MYCELIUM,
+	MECH_WIRE,
+	MECH_SWITCH
+};
+
+enum MECH_RENDER_TYPE
+{
+    MECH_RENDER_TYPE_0, //X shaped
+    MECH_RENDER_TYPE_1, //crop/wheat type
+    MECH_RENDER_TYPE_2, //mycelium/ladder type
+};
+*/
+
+void load_mech_dat()
+{
+    start_mech_dat();
+
+    mech_def(0, MECH_CRYSTAL, "blue_crystal");
+    s.sprite_index = 16*0 + 0;
+    s.render_type = MECH_RENDER_TYPE_0;
+
+    mech_def(1, MECH_CRYSTAL, "red_crystal");
+    s.sprite_index = 16*0 + 0;
+    s.render_type = MECH_RENDER_TYPE_0;
+
+    mech_def(2, MECH_MYCELIUM, "mycelium");
+    s.sprite_index = 16*1 + 0;
+    s.render_type = MECH_RENDER_TYPE_2;
+
+    mech_def(2, MECH_CRYSTAL, "mycelium_flower");
+    s.sprite_index = 16*1 + 1;
+    s.render_type = MECH_RENDER_TYPE_0;
+
+    end_mech_dat(); // finalize
+
+}
+
+
 }
