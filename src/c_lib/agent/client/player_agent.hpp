b7420967f737827ae58dc5d5ae6fef4b3ff04675
Steve Leonard <sleonard76@gmail.com>
2012-08-06 03:58:38 -0700

    .

--- src/c_lib/agent/client/player_agent.hpp
+++ src/c_lib/agent/client/player_agent.hpp
@@ -106,7 +106,7 @@ class PlayerAgent_state
 
         int* nearest_open_block(const float max_dist, const int z_low, const int z_high);
         int get_facing_side(int solid_pos[3], int open_pos[3], int side[3], float* distance);
-        int get_facing_side(int solid_pos[3], int open_pos[3], float* distance);
+        int get_facing_side(int solid_pos[3], int open_pos[3], float* distance);    // returns side, as integer. side<0 if failure
         
         PlayerAgent_state();
         ~PlayerAgent_state();
