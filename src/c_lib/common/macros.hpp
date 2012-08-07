f061d5de5a0cf3ff15a8a29e03c139a6c56e9704
Steve Leonard <sleonard76@gmail.com>
2012-08-07 05:43:08 -0700

    gs assert limit only on production

--- src/c_lib/common/macros.hpp
+++ src/c_lib/common/macros.hpp
@@ -16,8 +16,11 @@ print_trace();\
 exit(1);\
 }while(0)
 
-#define GS_ASSERT(condition) GS_ASSERT_LIMIT(condition, 50)
-//if(! (condition) ) { print_trace(); printf("GS_ASSERT error: %s, line %d function: %s \n", __FILE__, __LINE__, __FUNCTION__); }
+#if PRODUCTION
+#define GS_ASSERT(condition) GS_ASSERT_LIMIT(condition, 15)
+#else
+#define GS_ASSERT(condition) if(! (condition) ) { print_trace(); printf("GS_ASSERT error: %s, line %d function: %s \n", __FILE__, __LINE__, __FUNCTION__); }
+#endif
 
 #define GS_ASSERT_LIMIT(COND, LIMIT) \
 if(!(COND)) { \
