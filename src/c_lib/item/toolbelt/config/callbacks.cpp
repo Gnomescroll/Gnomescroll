4aacf7aa01c8ef30a69963aae4ab42a9298aaecc
HaltingState <haltingstate@gmail.com>
2012-08-10 23:43:16 -0700

    crystals cannot be placed over each other

--- src/c_lib/item/toolbelt/config/callbacks.cpp
+++ src/c_lib/item/toolbelt/config/callbacks.cpp
@@ -290,12 +290,19 @@ void place_mech(int agent_id, ItemID item_id, int item_type)
         if (b[2] <= 0) return;  // can't place on nothing
         if (!isSolid(b[0], b[1], b[2]-1)) return;
 
-        printf("place crystal: at %d %d %d \n", b[0],b[1],b[2] );
         
         static int crystal_id = t_mech::get_mech_type("blue_crystal");
 
         if( t_mech::can_place_crystal(b[0],b[1],b[2], 0) == true)
+        {
+            printf("place crystal: at %d %d %d \n", b[0],b[1],b[2] );
             t_mech::create_crystal(b[0],b[1],b[2], crystal_id);
+        
+        }
+        else
+        {
+
+        }
         //decrement_stack(agent_id, item_id, item_type);
     }
 
