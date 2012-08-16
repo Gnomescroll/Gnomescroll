80ba6b1dfd0d0a9714ec0a04c33cf3ef9f7a6fe3
HaltingState <haltingstate@gmail.com>
2012-08-15 19:10:12 -0700

    Skybox

--- src/c_lib/t_map/t_vbo_update.cpp
+++ src/c_lib/t_map/t_vbo_update.cpp
@@ -616,6 +616,8 @@ void set_vertex_buffers(class MAP_CHUNK* chunk, class Map_vbo* vbo)
 void Vbo_map::update_vbo(int i, int j)
 {
 
+    update_skylight(i,j); //update skylights
+
     class MAP_CHUNK* chunk = map->chunk[j*MAP_CHUNK_XDIM + i];  //map chunk
     class Map_vbo* vbo = vbo_array[j*MAP_CHUNK_XDIM + i];       //vbo for storing resulting vertices
 
