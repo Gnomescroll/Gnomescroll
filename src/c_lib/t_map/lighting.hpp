0c021a1db229daa96cb2a17973b8e1c736d9de59
Steve Leonard <sleonard76@gmail.com>
2012-08-17 20:58:55 -0700

    fix lighting

--- src/c_lib/t_map/lighting.hpp
+++ src/c_lib/t_map/lighting.hpp
@@ -7,61 +7,51 @@ namespace t_map
 {
 
 /*
-	Update skylights seperately
+    Update skylights seperately
 */
 
 //e[ (z<<8)+((y&15)<<4)+(x&15) ] = e;
 void update_skylight(int chunk_i, int chunk_j)
 {
+    class MAP_CHUNK* mc = main_map->chunk[32*chunk_j + chunk_i];
+    struct MAP_ELEMENT e;
 
+    //struct MAP_e e;
 
-	class MAP_CHUNK* mc = main_map->chunk[32*chunk_j + chunk_i];
-	struct MAP_ELEMENT e;
+    for(int i=0; i<16; i++)
+    for(int j=0; j<16; j++)
+    {
+        int k=127;
 
-	//struct MAP_e e;
+        for (; k>=0; k--)
+        {
+            e = mc->get_element(i,j,k);
+            if(e.block != 0)    //iterate until we hit top block
+                break;
 
-	for(int i=0; i<16; i++)
-	for(int j=0; j<16; j++)
-	{
-		int k = 128;
+            e.light  |= 0xff; //upper bits for skylight
+            mc->set_element(i,j,k,e);
+        }
+        if (k < 0) return;
 
+        for (int _k=0; k>=0 && _k<16; k--, _k++)
+        {
+            e = mc->get_element(i,j,k);
+            if(e.block != 0)
+                continue;
+            e.light  |= 16*(15-_k)+ 0x0f; //clear upper bits
+            mc->set_element(i,j,k,e);
+        }
 
-		while(1)
-		{
-			k--;
-			e = mc->get_element(i,j,k);
-			if(e.block != 0)	//iterate until we hit top block
-				break;
-
-			e.light  |= 0xff; //upper bits for skylight
-			mc->set_element(i,j,k,e);
-			if(k == 0) break;
-		}
-
-		for(int _k=0; _k<16; _k++)
-		{
-			k--;
-
-			e = mc->get_element(i,j,k);
-			if(e.block != 0)
-				continue;
-			e.light  |= 16*(15-_k)+ 0x0f; //clear upper bits
-			mc->set_element(i,j,k,e);
-			if(k == 0) break;
-		}
-
-		while(1)
-		{
-			k--;
-
-			e = mc->get_element(i,j,k);
-			if(e.block != 0)
-				continue;
-			e.light  |= 0x0f; //clear upper bits
-			mc->set_element(i,j,k,e);
-			if(k == 0) break;
-		}
-	}
+        for (; k>=0; k--)
+        {
+            e = mc->get_element(i,j,k);
+            if(e.block != 0)
+                continue;
+            e.light  |= 0x0f; //clear upper bits
+            mc->set_element(i,j,k,e);
+        }
+    }
 
 }
 
