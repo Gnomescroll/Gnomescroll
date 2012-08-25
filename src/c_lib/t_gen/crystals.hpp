c5a9f8044275b7f46520e966f4500462735aea41
Steve Leonard <sleonard76@gmail.com>
2012-08-25 12:09:17 -0700

    better crystal distribution

--- src/c_lib/t_gen/crystals.hpp
+++ src/c_lib/t_gen/crystals.hpp
@@ -13,20 +13,24 @@ typedef enum
     CRYSTAL_CLUSTER_GAUSSIAN,
 } CrystalClusterMode;
 
-const float CRYSTAL_CLUSTER_PROBABILITY = 0.005f; // this might need to be dynamic to meet a target cluster count
+const float CRYSTAL_CLUSTER_PROBABILITY = 0.012f; // this might need to be dynamic to meet a target cluster count
 const int CRYSTAL_CLUSTER_RADIUS = 3;
-const float CRYSTAL_CLUSTER_FALLOFF = 0.75f;
+const float CRYSTAL_CLUSTER_FALLOFF = 0.6f;
 const CrystalClusterMode CRYSTAL_CLUSTER_MODE = CRYSTAL_CLUSTER_LINEAR;
 const int CRYSTAL_CLUSTER_Z_DIFF_MAX = 3;
 
 const int n_crystals = 3;
 int crystals[n_crystals] = {t_map::ERROR_CUBE };
-int crystal_strata[n_crystals*2] = {0};
+float crystal_strata[n_crystals*2] = {0};
 
 int rock = t_map::ERROR_CUBE;
 int bedrock = t_map::ERROR_CUBE;
 
 float falloffs[CRYSTAL_CLUSTER_RADIUS*2+1] = {1.0f};
+
+int* cluster_size = NULL;
+int cluster_id = 0;
+int crystal_type_count[n_crystals] = {0};
 
 // TODO: move
 int get_highest_block_of_type(int block_id)
@@ -57,23 +61,41 @@ void init_crystals()
     rock = t_map::get_cube_id("rock");
     GS_ASSERT(rock != t_map::ERROR_CUBE);
 
-    int top = get_highest_block_of_type(rock) + 1;
+    //int top = get_highest_block_of_type(rock) + 1;
 
-    int step = (int)ceil(((1.0f/3.0f) * ((float)top)));
+    //int step = (int)ceil(((1.0f/3.0f) * ((float)top)));
     
-    // calculate stratification for crystal distribution
-    for (int i=0; i<n_crystals; i++)
-    {
-        crystal_strata[2*i+0] = step * (3-i-0);
-        crystal_strata[2*i+1] = step * (3-i-1);
-    }
+    //// calculate stratification for crystal distribution
+    //for (int i=0; i<n_crystals; i++)
+    //{
+        //crystal_strata[2*i+0] = step * (3-i-0);
+        //crystal_strata[2*i+1] = step * (3-i-1);
+    //}
+
+    i = 0;
+    crystal_strata[i++] = 0.0f;
+    crystal_strata[i++] = 0.425f;
+    crystal_strata[i++] = 0.425f;
+    crystal_strata[i++] = 0.75f;
+    crystal_strata[i++] = 0.75f;
+    crystal_strata[i++] = 1.0f;
 
     for (int i=0; i<CRYSTAL_CLUSTER_RADIUS*2+1; i++)
         falloffs[i] = powf(CRYSTAL_CLUSTER_FALLOFF, i);
 }
 
+int get_crystal_index(int crystal_id)
+{
+    for (int i=0; i<n_crystals; i++)
+        if (crystals[i] == crystal_id)
+            return i;
+    GS_ASSERT(false);
+    return 0;
+}
+
 void place_crystal_cluster(int x, int y, int z, int crystal_id)
 {
+    
     for (int i=x-CRYSTAL_CLUSTER_RADIUS; i<x+CRYSTAL_CLUSTER_RADIUS; i++)
     for (int j=y-CRYSTAL_CLUSTER_RADIUS; j<y+CRYSTAL_CLUSTER_RADIUS; j++)
     {
@@ -86,20 +108,26 @@ void place_crystal_cluster(int x, int y, int z, int crystal_id)
         int k = get_nearest_open_block(ii,jj,z);
         if (abs(z-k) > CRYSTAL_CLUSTER_Z_DIFF_MAX) continue;
         int id = t_map::get(ii,jj,k-1);
-        if (id != rock && id != bedrock) continue;
+        if (id != rock) continue;
         t_mech::create_crystal(ii,jj,k,crystal_id);
+        cluster_size[cluster_id]++;
+        crystal_type_count[get_crystal_index(crystal_id)]++;
     }
+    cluster_id++;
 }
 
-int get_crystal_type(int z)
+int get_crystal_type(float percent_complete)
 {
     int i=0;
     for (; i<n_crystals; i++)
-        if (z < crystal_strata[2*i+0] && z >= crystal_strata[2*i+1])
+    //{
+        //printf("pct: %f; i %d; range %0.2f-%0.2f\n", percent_complete, i, crystal_strata[2*i+0], crystal_strata[2*i+1]);
+        if (percent_complete >= crystal_strata[2*i+0] && percent_complete < crystal_strata[2*i+1])
             return crystals[i];
+    //}
     GS_ASSERT(false);
-    printf("z error: %d\n", z);
-    return crystals[0];
+    printf("pct error: %f\n", percent_complete);
+    return crystals[n_crystals-1];
 }
 
 void populate_crystals()
@@ -116,23 +144,70 @@ void populate_crystals()
     GS_ASSERT(bedrock != t_map::ERROR_CUBE);
 
     int ct = 0;
+    int ct_max = 1000;
+    int* loc = (int*)malloc(3 * ct_max * sizeof(int));
 
+    for (int k=127; k>=0; k--)
     for (int i=0; i<512; i++)
     for (int j=0; j<512; j++)
-    for (int k=0; k<128; k++)
     {
         if (randf() > CRYSTAL_CLUSTER_PROBABILITY) continue;   // probability test
         int id = t_map::get(i,j,k); // get cube
-        if (id != rock && id != bedrock) continue;   // check rock
+        if (id != rock) continue;
         if (t_map::get(i,j,k+1) != 0) continue; // check block above is open
 
-        // choose crystal type based on z level
-        int crystal_id = get_crystal_type(k+1);
-        place_crystal_cluster(i,j,k+1, crystal_id);
+        if (ct >= ct_max)
+        {
+            ct_max *= 2;
+            int* tmp = (int*)realloc(loc, 3*ct_max*sizeof(int));
+            GS_ASSERT(tmp != NULL);
+            if (tmp == NULL)
+            {
+                ct_max /= 2;
+                break;
+            }
+            loc = tmp;
+        }
+
+        loc[3*ct+0] = i;
+        loc[3*ct+1] = j;
+        loc[3*ct+2] = k+1;
         ct++;
     }
 
+    GS_ASSERT(ct > 0);
+    if (ct <= 0) return;
+
+    cluster_size = (int*)calloc(ct, sizeof(int));
+
+    float pct = 0.0f;
+    float inc = 1.0f/(float)ct;
+    for (int i=0; i<ct; i++)
+    {
+        int crystal_id = get_crystal_type(pct);
+        int x = loc[3*i+0];
+        int y = loc[3*i+1];
+        int z = loc[3*i+2];
+        place_crystal_cluster(x,y,z, crystal_id);
+        pct += inc;
+    }
+
+    free(loc);
+
     printf("Placed %d crystal clusters\n", ct);
+
+    int sum = 0;
+    for (int i=0; i<ct; i++)
+        sum += cluster_size[i];
+
+    float avg_cluster_size = ((float)sum)/((float)ct);
+    printf("Average cluster size: %f\n", avg_cluster_size);
+
+    printf("Crystal counts:\n");
+    for (int i=0; i<n_crystals; i++)
+        printf("\t%d: %d\n", i, crystal_type_count[i]);
+
+    free(cluster_size);
 }
 
 }   // t_gen
