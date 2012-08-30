d4589e2b9c7c646f9f411e9fe19a0e0a1b63b891
HaltingState <haltingstate@gmail.com>
2012-08-30 23:40:32 +0100

    changes

--- src/server/stdafx.h
+++ src/server/stdafx.h
@@ -7,10 +7,12 @@
 
 #define DC_SERVER 1
 
+#define __attribute__(x)
+#define __attribute(x)
 #define __WIN32__
-//#define __MSVC__
+#define __MSVC__
 
-#define S_ITERATOR_DEBUGGING=0
+#define S_ITERATOR_DEBUGGING 0
 #define _SECURE_SCL 0	//secure iterators, slow
 
 #pragma warning(disable: 4244) // possible loss of data
