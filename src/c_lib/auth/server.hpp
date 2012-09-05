f840b98cdfc8d7911ed1f01b7176a380bd4f3564
Steve Leonard <sleonard76@gmail.com>
2012-09-05 03:17:08 -0700

    .

--- src/c_lib/auth/server.hpp
+++ src/c_lib/auth/server.hpp
@@ -9,6 +9,8 @@ namespace Auth
 const char SECRET_KEY_PATH[] = "./data/secret_key";
 const unsigned int SECRET_KEY_SIZE = 64;
 
+const time_t AUTH_TOKEN_LIFETIME = 60 * 60; // 1 hour, in seconds 
+
 void init();
 void teardown();
 
