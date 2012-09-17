9470aec2369ef240eff259601c3d41cb69d9e0e7
Steve Leonard <sleonard76@gmail.com>
2012-09-17 07:25:39 -0700

    .

--- src/c_lib/common/common.cpp
+++ src/c_lib/common/common.cpp
@@ -63,8 +63,8 @@ time_t atott(const char* str)
     if (s == sizeof(long long))
         return atoll(str);
     #else
-    if (s == sizeof(__int64))
-        return _strtoui64(str, NULL, 10);
+    //if (s == sizeof(__int64))
+        //return _strtoui64(str, NULL, 10);
     #endif
     GS_ASSERT_LIMIT(false, 1);
     return 0;
