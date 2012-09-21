01fa9171796319e6d694b1ae9362c00af5c367c7
Steve Leonard <sleonard76@gmail.com>
2012-09-21 13:08:09 -0700

    server run script completed

--- server/shell/run_production.sh
+++ server/shell/run_production.sh
@@ -7,11 +7,15 @@ chown -R gnomescroll:gnomescroll .
 # compile & copy
 sudo -u gnomescroll ./waf production; ./waf; cp run gnomescroll_server
 # stop the run script
-killall gnomescroll_server_py
+kill `pidof gnomescroll_server_py`
 # stuff should save to disk. hopefully is done quick enough (should be done in a few seconds...)
 sleep 15
 # destroy the old screen
-./shell/killscreens.sh gnomescroll_server
-# create a detached screen with the forever script running inside 
-sudo -u gnomescroll screen -d -m -S gnomescroll_server ./shell/run_server_forever.py
+sudo -u gnomescroll ./shell/killscreens.sh gnomescroll_server
+# create a detached screen with the forever script running inside
+sudo -u gnomescroll /usr/bin/screen -d -m -S gnomescroll_server ./shell/run_server_forever.py  
 
+# To reattach to the screen,
+#su - gnomescroll
+#script /dev/null
+#screen -r gnomescroll_server
