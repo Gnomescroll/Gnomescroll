4469cd0a17df32c7ec17bfb7e7d30fe157d7470f
Steve Leonard <sleonard76@gmail.com>
2012-09-21 21:49:52 -0700

    update new run scripts

--- server/shell/begin_server.sh
+++ server/shell/begin_server.sh
@@ -2,16 +2,24 @@
 
 # make sure gnomescroll user owns the folder
 chown -R gnomescroll:gnomescroll .
-# compile & copy
-sudo -u gnomescroll ./waf production; ./waf; cp run gnomescroll_server
+
 # stop the run script
 kill `pidof gnomescroll_server_py`
-# stuff should save to disk. hopefully is done quick enough (should be done in a few seconds...)
-sleep 15
+
+# wait until it all shuts down...
+while [ `pidof gnomescroll_server gnomescroll_server_py` ]
+do
+    echo "waiting for old server shutdown..."
+    sleep 1
+done
+
 # destroy the old screen
 sudo -u gnomescroll ./shell/killscreens.sh gnomescroll_server
+
 # create a detached screen with the forever script running inside
 sudo -u gnomescroll /usr/bin/screen -d -m -S gnomescroll_server ./shell/run_server_forever.py  
+
+echo "Started server in new screen for user: gnomescroll"
 
 # To reattach to the screen,
 #su - gnomescroll
