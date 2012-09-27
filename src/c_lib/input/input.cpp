aaa7f171ff36966e472255420119e9658b66c12f
Steve Leonard <sleonard76@gmail.com>
2012-09-26 17:36:58 -0700

    prints

--- src/c_lib/input/input.cpp
+++ src/c_lib/input/input.cpp
@@ -49,13 +49,15 @@ int get_key_state()
 
 void bind_mouse()
 {
-    SDL_ShowCursor(0);
+    printf("Bind mouse\n");
+    SDL_ShowCursor(SDL_DISABLE);
     SDL_WM_GrabInput(SDL_GRAB_ON);
 }
 
 void unbind_mouse()
 {
-    SDL_ShowCursor(1);
+    printf("Unbind mouse\n");
+    SDL_ShowCursor(SDL_ENABLE);
     SDL_WM_GrabInput(SDL_GRAB_OFF);
 }
 
