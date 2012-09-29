6828ac2cbf5a5ae18f089d63fd49a50d31da0e22
Steve Leonard <sleonard76@gmail.com>
2012-09-29 14:54:08 -0700

    .

--- src/c_lib/serializer/_include.hpp
+++ src/c_lib/serializer/_include.hpp
@@ -4,6 +4,6 @@
 dont_include_this_file_in_client
 #endif
 
-#include <serializer/constants.cpp>
+#include <serializer/constants.hpp>
 #include <serializer/map.cpp>
 #include <serializer/_interface.cpp>
