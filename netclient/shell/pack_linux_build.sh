c9c5e1d471f9c7c71c200f58566cab1cb2939dd3
Steve Leonard <sleonard76@gmail.com>
2012-11-01 21:40:33 -0700

    fix client build

--- netclient/shell/pack_linux_build.sh
+++ netclient/shell/pack_linux_build.sh
@@ -1,25 +1,22 @@
-./waf clean
 ./waf production
 ./waf
 version=`cat ../src/c_lib/common/version.h | grep GS_VERSION | cut -d " " -f 3`
 f="gnomescroll_linux32_"$version
 rm -rf $f
 mkdir $f
-cp run $f/gnomescroll
-cp *.pyc $f/
-cp -R media $f/
+cp -d run $f/gnomescroll
+cp -d *.pyc $f/
+cp -d -R media $f/
 mkdir $f/settings
-cp ./settings/production.lua $f/settings/settings.lua
-cp ./settings/lua_library.lua $f/settings/lua_library.lua
+cp -d ./settings/production.lua $f/settings/settings.lua
+cp -d ./settings/lua_library.lua $f/settings/lua_library.lua
 mkdir $f/screenshot
 
-mkdir $f/lib
-cp ../lib/lin32/*.so $f/lib
-cp ../lib/lin32/*.so.* $f/lib
-mkdir $f/lib/awesomium/lib/
-cp ../lib/lin32/awesomium/lib/* $f/lib/awesomium/lib/
-mkdir $f/lib/assimp/lib/
-cp ../lib/lin32/assimp/lib/* $f/lib/assimp/lib/
+mkdir -p $f/lib/lin32/
+cp -d ../lib/lin32/*.so $f/lib/lin32/
+cp -d ../lib/lin32/*.so.* $f/lib/lin32/
+cp -R -d ../lib/lin32/awesomium $f/lib/lin32/
+cp -R -d ../lib/lin32/assimp $f/lib/lin32/
 
 #cp ../notes/README_linux_client $f/README
 rm $f.tar.gz
