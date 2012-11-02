5b82fff0d658634fafe99073fb489d91061c3e64
Steve Leonard <sleonard76@gmail.com>
2012-11-02 02:31:51 -0700

    dirent

--- src/c_lib/common/dirent.h
+++ src/c_lib/common/dirent.h
@@ -8,7 +8,92 @@
 # include <dirent.h>
 #endif
 
+#ifdef __MINGW32__
+/****************************************************************************
+*
+*   Copyright (c) 2010 Dave Hylands     <dhylands@gmail.com>
+*
+*   This program is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License version 2 as
+*   published by the Free Software Foundation.
+*
+*   Alternatively, this software may be distributed under the terms of BSD
+*   license.
+*
+*   See README and COPYING for more details.
+*
+****************************************************************************/
+/**
+*
+*   @file   dirent.h
+*
+*   @brief  The mingw version of dirent and friends doesn't include the 
+*           the d_type field. So we make our own version.
+*
+****************************************************************************/
+
+#if !defined( MINGW_DIRENT_H )
+#define MINGW_DIRENT_H         /**< Include Guard                             */
+
+/* ---- Include Files ---------------------------------------------------- */
+
+#include <inttypes.h>
+#include <sys/types.h>
+#include <io.h>
+
+/* ---- Constants and Types ---------------------------------------------- */
+
+#define DT_DIR      004
+#define DT_REG      010
+
+struct dirent
+{
+    long            d_ino;      /* Always zero.     */
+    off_t           d_off;      /* Always zero.     */
+    unsigned short  d_reclen;   /* Always zero      */
+    unsigned char   d_type;     /* Type of file     */
+    char            d_name[FILENAME_MAX]; /* File name. */
+};
+
+typedef struct
+{
+    /* dd_findsata stores the information needed for the
+     * _findfirst/_findnext API
+     */
+    void               *dd_finddata;
+
+    /* dd_dirent stores the dirent returned from readdir, which makes
+     * this threadsafe as long as only one thread uses this particular
+     * DIR entry.
+     */
+    struct  dirent      dd_dirent;
+
+    /* dd_dirpattern contains the directory pattern that was passed to 
+     * FindFirstFile.
+     */
+    char               *dd_dirpattern;
+
+    /* dd_handle is the HANDLE returned by FindFirstFile
+     */
+    void               *dd_handle;
+
+} DIR;
+
+/* ---- Variable Externs ------------------------------------------------- */
+
+/* ---- Function Prototypes ---------------------------------------------- */
+
+DIR *opendir( const char *dirName );
+struct dirent *readdir( DIR *dir );
+int closedir( DIR *dir );
+
+/** @} */
+
+#endif /* MINGW_DIRENT_H */
+#endif
+
 #ifdef _WIN32
+#ifndef __MINGW32__
 
 /*
  * dirent.h - dirent API for Microsoft Visual Studio
@@ -705,4 +790,5 @@ rewinddir(
 #endif
 #endif /*DIRENT_H*/
 
+#endif
 #endif
