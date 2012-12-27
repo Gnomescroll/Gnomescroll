2f86d4e1f97012cf947f658aadbdd5e1d1e91141
HaltingState <haltingstate@gmail.com>
2012-12-27 04:30:58 -0800

    skybox

--- src/c_lib/camera/skybox/rayleigh_scattering.hpp
+++ src/c_lib/camera/skybox/rayleigh_scattering.hpp
@@ -16,21 +16,23 @@ class Skyplane
 	static const int dim = 16;
 	const static int samples = 5;
 
-
-	float sun_distance;  				//sun distance from surface
-	static const float size = 128.0;	//skybox plane size
-
 	int sun_i, sun_j, sun_side; //used to find pixel for tracking sun
 	float sun_cos_max;
 
 	struct Vec3 vert[4];
 
 	float brightness_log_factor;
-	float brightness_scale_factor;
-	float brightness_sum_factor;
+	float phase_factor;
+	float sun_distance;  				//sun distance from surface
 	float light_epsilon;
 
-	float phase_factor;
+	float brightness_scale_factor;
+	float brightness_sum_factor;
+
+	float H0;
+	float skybox_height;
+
+	float plane_size;	//size of skybox box
 
 	Skyplane()
 	{
@@ -41,6 +43,11 @@ class Skyplane
 
 		brightness_scale_factor = 1.0;
 		brightness_sum_factor = 0.0;
+
+		H0 = 0.25f;			 	//percent of skybox where atomosphere hits 25%
+		skybox_height = 128.0;	//skybox height
+
+		plane_size = 128.0;
 
 	/*
 		for(int i=0; i<dim*dim; i++)
@@ -65,7 +72,7 @@ class Skyplane
 	void draw_sun(float theta, float phi, float x,float y,float z)
 	{
 		const float radius = 64.0;
-		const float plane_depth = size/2.0;
+		const float plane_depth = plane_size/2.0;
 
 		float stheta = 2*3.14159*theta;
 		float sphi   = 2*3.14159*phi;
@@ -97,7 +104,7 @@ class Skyplane
 	}
 	void update(float theta, float phi)
 	{
-		const float plane_depth = size/2.0;
+		const float plane_depth = plane_size*0.5;
 
 		const float _df = 1.0/ ((float) dim);
 
@@ -161,23 +168,6 @@ class Skyplane
 			r[i] = vec3_init(_r[3*i+0],_r[3*i+1],_r[3*i+2] );
 			u[i] = vec3_init(_u[3*i+0],_u[3*i+1],_u[3*i+2] );
 		}
-	/*
-		for(int i=0; i<6 ;i++)
-		{
-			struct Vec3 f = vec3_init(_f[3*i+0],_f[3*i+1],_f[3*i+2] );
-			struct Vec3 r = vec3_init(_r[3*i+0],_r[3*i+1],_r[3*i+2] );
-			struct Vec3 u = vec3_init(_u[3*i+0],_u[3*i+1],_u[3*i+2] );
-
-			struct Vec3 cu = vec3_cross(f,r);
-
-			printf("%i: dot= %.2f fr, fu, ru= %.2f %.2f %.2f \n", i,
-				vec3_dot(cu, u),
-				vec3_dot(f, r),
-				vec3_dot(f, u),
-				vec3_dot(r, u)
-				);
-		}
-	*/
 
 		//center of plane
 		struct Vec3 center[6];
@@ -213,19 +203,32 @@ class Skyplane
 		const float camera_z = 64.0f;
 		struct Vec3 c = vec3_init(0.0, 0.0, camera_z);
 
+		const float _dfp = _df*plane_size; //for speed
+		const int dim_half = dim/2;
+
 		for(int side=0; side<6; side++)
 		{
+
+			float center_x = center[side].x;
+			float center_y = center[side].y;
+			float center_z = center[side].z;
+
+			struct Vec3 _r = r[side];
+			struct Vec3 _u = u[side];
+
 			for(int i=0; i<dim; i++)
 			{
+
+				int _i = i - dim_half;
+
 				for(int j=0; j<dim; j++)
 				{
-					int _i = i -dim/2;
-					int _j = j -dim/2;
+					int _j = j - dim_half;
 
 					struct Vec3 b;
-					b.x = center[side].x + _df*size*(_i*r[side].x + _j*u[side].x);
-					b.y = center[side].y + _df*size*(_i*r[side].y + _j*u[side].y);
-					b.z = center[side].z + _df*size*(_i*r[side].z + _j*u[side].z);
+					b.x = center_x + _dfp*(_i*_r.x + _j*_u.x);
+					b.y = center_y + _dfp*(_i*_r.y + _j*_u.y);
+					b.z = center_z + _dfp*(_i*_r.z + _j*_u.z);
 
 					//can inline this even
 					
@@ -280,58 +283,6 @@ class Skyplane
 
 			}
 		}	
-/*
-		int m_side;
-		int m_i;
-		int m_j;
-		float m_cos = -1000;
-
-		for(int side=0; side<6; side++)
-		{
-			for(int i=0; i<dim; i++)
-			{
-				for(int j=0; j<dim; j++)
-				{
-					int _i = i -dim/2;
-					int _j = j -dim/2;
-
-					struct Vec3 b;
-					b.x = center[side].x + _df*size*(_i*r[side].x + _j*u[side].x);
-					b.y = center[side].y + _df*size*(_i*r[side].y + _j*u[side].y);
-					b.z = center[side].z + _df*size*(_i*r[side].z + _j*u[side].z);
-
-
-					struct Vec3 tb = vec3_sub(b, c);
-					//struct Vec3 tb = vec3_sub(_b, c);
-					struct Vec3 ts = vec3_sub(s, c);
-
-					tb = vec3_normalize(tb);
-					ts = vec3_normalize(ts);
-
-					float v = vec3_dot(tb, ts);
-
-					if(v > m_cos)
-					{
-						m_cos = v;
-						m_side = side;
-						m_i = i;
-						m_j = j;
-					}
-
-
-					//can inline this even
-					
-					//float light = update_point(c, b, s);
-					//float light = 0;
-					//farray[side][j*dim+i] = light;
-
-					//printf("light: %i %i = %.2f \n", i,j,light);
-				}
-
-			}
-		}
-		farray[m_side][m_j*dim+m_i] = 1.0;
-*/
 
 /*
 		printf("!!! sx,sy,sz= %.2f %.2f %.2f \n" ,sx,sy,sz);
@@ -351,6 +302,182 @@ class Skyplane
 			printf("%i: center= %.2f %.2f %.2f \n", i,center[i].x,center[i].y,center[i].z );
 		}
 	*/
+	}
+
+	float phase(struct Vec3 v1, struct Vec3 v2)
+	{
+/*
+		float t0 = vec3_cos2(v1,v2);
+		float t1 = vec3_cos2( vec3_normalize(v1),vec3_normalize(v2));
+
+		if(abs(t0-t1) > 0.0001f)
+		{
+			printf("phase error: t0, t1= %.2f %.2f \n", t0,t1);
+			GS_ASSERT(false);
+		}
+*/
+		//GS_ASSERT(vec3_cos2(v1,v2) == vec3_cos2( vec3_normalize(v1),vec3_normalize(v2)) );
+		return phase_factor*(1+vec3_cos2(v1,v2)); //*.75
+	}
+
+	//H 0 is the scale height, which is the height at which the atmosphere's average density is found.
+	//My implementation uses 0.25, so the average density is found 25 percent of the way up from the ground to the sky dome.
+	//the height is scaled so that 0 represents sea level and 1 is at the top of the atmosphere.
+
+	//function of wavelength
+
+	float out_scatter(const struct Vec3 &v1, const struct Vec3 &v2)
+	{
+		const float _f = 1.0f / ((float) samples);
+		const float _d = _f * vec3_distance(v1, v2);
+		const float _d_half = 0.5f* _d;
+		const float sbh_norm = -1.0f / (H0*skybox_height); //skybox height normalization
+
+		struct Vec3 vi = vec3_scalar_mult(vec3_sub(v2, v1), _f);
+
+		float _r[samples+1];
+
+
+		for(int i=0; i<=samples; i++)
+		{
+			struct Vec3 tmp1 = vec3_add(v1, vec3_scalar_mult(vi, i));
+			//float _h = tmp1.z *sbh_norm; //divide by -H0*skybox_height
+			_r[i] = exp(tmp1.z *sbh_norm); //intergrate over exp(-height / H0) 
+		}
+
+
+		float tmp = 0.0f;
+
+		for(int i=0; i<samples; i++)
+			tmp += _d_half*(_r[i] + _r[i+1]);  // _d*(_r[i] + _r[i+1])*0.5
+
+		return tmp;
+		//return 4.0*3.14159*tmp;
+	}
+
+	//start position, end position, sun position
+	float in_scatter(const struct Vec3 &a, const struct Vec3 &b, const struct Vec3 &s)
+	{
+		struct Vec3 c = a;
+
+		struct Vec3 bc = vec3_sub(b,c);
+		struct Vec3 bs = vec3_sub(s,c);
+
+		//For each point P along the ray from Pa to Pb , 
+		//PPc is the ray from the point to the sun
+		//PPa is the ray from the sample point to the camera.
+
+		const float _f = 1.0f / ((float) samples);
+		const float _d = _f * vec3_distance(b,a);	//for intergral
+		const float _d_half = 0.5f* _d;
+		const float sbh_norm = -1.0f / (H0*skybox_height); //skybox height normalization
+
+		GS_ASSERT(abs( vec3_distance(a,b) - vec3_length(vec3_sub(b,a))) < 0.0001 );
+
+
+		struct Vec3 vi = vec3_scalar_mult(vec3_sub(b, a), _f);
+
+		//compute sample points
+		struct Vec3 tmp1[samples+1];	//should be constant for MSVC
+
+		for(int i=0; i<=samples; i++)
+			tmp1[i] = vec3_add(a, vec3_scalar_mult(vi, i));
+
+		//direction of sun from sample point
+		struct Vec3 _s[samples+1];
+		for(int i=0; i<=samples; i++)
+		{
+			//do line sphere intersection to get length
+			//vector which is 20 units in direction of sun from tmp1
+
+			//_s[i] = vec3_add(tmp1[i], vec3_scalar_mult(vec3_normalize(vec3_sub(tmp1[i], s)), 20.0));
+
+			_s[i] = s; //skip to sun without raise limiting
+
+		}
+
+		float _t0 [samples+1]; //exp(h/H0)
+		float _t1 [samples+1]; //out_scatter PPc
+		float _t2 [samples+1]; //out_scatter PPa
+
+		for(int i=0; i<=samples; i++)
+		{
+			//_t0[i] = expf( -tmp1[i].z / (H0*skybox_height));
+			_t0[i] = expf(sbh_norm*tmp1[i].z); // expf( -tmp1[i].z / (H0*skybox_height));
+
+			_t1[i] = out_scatter(tmp1[i], _s[i]); //out_scatter from current point to sun
+			_t2[i] = out_scatter(tmp1[i], c); //out_scatter from current point to camera
+		
+			//printf("%i: _t0,_t1,_t2= %.2f %.2f %.2f \n", i, _t0[i],_t1[i],_t2[i] );
+		}
+
+		float _r [samples+1];
+		
+		//debug
+		//for(int i=0; i<=samples; i++)
+		//	_r[i] = _t0[i]*expf( -_t1[i] -_t2[i] );
+		for(int i=0; i<=samples; i++)
+			_r[i] = _t0[i]*expf( -1.0*(_t1[i] + _t2[i]) );
+
+/*
+		for(int i=0; i<=samples; i++)
+		{
+			printf("%i: _r[i] = %.2f, _t0,_t1,_t2= %.2f %.2f %.2f, -_t1[i] -_t2[i]= %.2f, exp= %.2f \n", 
+				i, _r[i], _t0[i],_t1[i],_t2[i],
+				-_t1[i] -_t2[i], exp(-_t1[i] -_t2[i]));
+		}
+*/
+
+		float tmp = 0.0f;
+
+		for(int i=0; i<samples; i++)
+			tmp += _d_half*(_r[i] + _r[i+1]); //  0.5f*_d*(_r[i] + _r[i+1]);//
+
+		//struct Vec3 s2 = vec3_sub(v2, c);	
+
+		//printf("phase: %.2f\n", phase(v2, s));
+/*
+		printf("a= %.2f %.2f %.2f  b= %.2f %.2f %.2f s= %.2f %.2f %.2f  phase: %.2f \n", 
+			a.x,a.y,a.z, 
+			b.x,b.y,b.z,
+			s.x,s.y,s.z, 
+			phase(b, s) );
+*/
+		//debug
+		tmp *= phase(bc, bs);
+		
+		//return brightness_scale_factor*(brightness_log_factor*log(tmp) + brightness_sum_factor);
+		return tmp;
+	}
+
+	static float sun_position(const struct Vec3 &a, const struct Vec3 &b, const struct Vec3 &s)
+	{
+		struct Vec3 c = a;
+		struct Vec3 tb = vec3_normalize(vec3_sub(b, c));
+		struct Vec3 ts = vec3_normalize(vec3_sub(s, c));
+
+		float v = vec3_dot(tb, ts);
+
+		if(v <= 0.0) v = 0.0;
+		v *= v;
+		return v;
+	}
+
+	//sun distaWnce
+	//atmosphere height
+	//planet radius
+
+	float update_point(const struct Vec3 &c, const struct Vec3 &b, const struct Vec3 &s)
+	{
+		const float ATMOSPHERE_DEPTH = 128.0;
+		//compute ray from camera to upper atmosphere (use sphere and radius)
+		//intersection with upper atomsphere
+		struct Vec3 _b;
+		_b = vec3_scalar_mult(vec3_normalize(b), ATMOSPHERE_DEPTH);
+		
+		//return sun_position(c,_b,s);
+		return in_scatter(c, _b, s);
+
 	}
 
 	void save(const char* filename, int side)
@@ -387,213 +514,6 @@ class Skyplane
 				}
 			}
 		}
-
-	}
-
-	float phase(struct Vec3 v1, struct Vec3 v2)
-	{
-/*
-		float t0 = vec3_cos2(v1,v2);
-		float t1 = vec3_cos2( vec3_normalize(v1),vec3_normalize(v2));
-
-		if(abs(t0-t1) > 0.0001f)
-		{
-			printf("phase error: t0, t1= %.2f %.2f \n", t0,t1);
-			GS_ASSERT(false);
-		}
-*/
-		//GS_ASSERT(vec3_cos2(v1,v2) == vec3_cos2( vec3_normalize(v1),vec3_normalize(v2)) );
-		return phase_factor*(1+vec3_cos2(v1,v2)); //*.75
-	}
-
-	//H 0 is the scale height, which is the height at which the atmosphere's average density is found.
-	//My implementation uses 0.25, so the average density is found 25 percent of the way up from the ground to the sky dome.
-
-	static const float H0 = 0.25f;
-	static const float SKYBOX_HEIGHT = 128.0; //skybox height
-
-
-	//function of wavelength
-
-	static const float damp = 0.50;
-	//static const float damp = 0.001;
-
-	float out_scatter(const struct Vec3 &v1, const struct Vec3 &v2)
-	{
-		float _f = 1.0f / ((float) samples);
-		float _d = _f * vec3_distance(v1, v2);
-		struct Vec3 vi = vec3_scalar_mult(vec3_sub(v2, v1), _f);
-
-		//H 0 is the scale height, which is the height at which the atmosphere's average density is found.
-		//My implementation uses 0.25, so the average density is found 25 percent of the way up from the ground to the sky dome.
-		//static const float H0 = 0.25f;
-		//static const float SKYBOX_HEIGHT = 128.0; //skybox height
-
-
-		//vi = vec3_scalar_mult(vi, _f);
-		//scale height to 0 to 1
-
-		//the height is scaled so that 0 represents sea level and 1 is at the top of the atmosphere.
-		//do over array of data?
-
-		float _r[samples+1];
-
-		for(int i=0; i<=samples; i++)
-		{
-			struct Vec3 tmp1 = vec3_add(v1, vec3_scalar_mult(vi, i));
-			float _h = tmp1.z /(H0*SKYBOX_HEIGHT);
-
-			_r[i] = exp(-_h); //intergrate over exp(-height / H0) 
-		}
-
-		float tmp = 0.0f;
-
-		for(int i=0; i<samples; i++)
-			tmp += _d*(_r[i] + _r[i+1]) / ( 2.0f);
-
-		return tmp;
-		//return 4*3.14159*tmp;
-
-
-	}
-
-	//start position, end position, sun position
-	float in_scatter(const struct Vec3 &a, const struct Vec3 &b, const struct Vec3 &s)
-	{
-		struct Vec3 c = a;
-
-		struct Vec3 bc = vec3_sub(b,c);
-		struct Vec3 bs = vec3_sub(s,c);
-
-		//For each point P along the ray from Pa to Pb , 
-		//PPc is the ray from the point to the sun
-		//PPa is the ray from the sample point to the camera.
-
-		float _f = 1.0f / ((float) samples);
-		float _d = _f * vec3_distance(b,a);	//for intergral
-
-		GS_ASSERT(abs( vec3_distance(a,b) - vec3_length(vec3_sub(b,a))) < 0.0001 );
-
-
-		struct Vec3 vi = vec3_scalar_mult(vec3_sub(b, a), _f);
-
-		//compute sample points
-		struct Vec3 tmp1[samples+1];	//should be constant for MSVC
-
-		for(int i=0; i<=samples; i++)
-			tmp1[i] = vec3_add(a, vec3_scalar_mult(vi, i));
-
-		//direction of sun from sample point
-		struct Vec3 _s[samples+1];
-		for(int i=0; i<=samples; i++)
-		{
-			//do line sphere intersection to get length
-			//vector which is 20 units in direction of sun from tmp1
-
-			//_s[i] = vec3_add(tmp1[i], vec3_scalar_mult(vec3_normalize(vec3_sub(tmp1[i], s)), 20.0));
-
-			_s[i] = s; //skip to sun without raise limiting
-
-		}
-
-		float _t0 [samples+1]; //exp(h/H0)
-		float _t1 [samples+1]; //out_scatter PPc
-		float _t2 [samples+1]; //out_scatter PPa
-
-		for(int i=0; i<=samples; i++)
-		{
-			_t0[i] = expf( -tmp1[i].z / (H0*SKYBOX_HEIGHT));
-			_t1[i] = out_scatter(tmp1[i], _s[i]); //out_scatter from current point to sun
-			_t2[i] = out_scatter(tmp1[i], c); //out_scatter from current point to camera
-		
-			//printf("%i: _t0,_t1,_t2= %.2f %.2f %.2f \n", i, _t0[i],_t1[i],_t2[i] );
-		}
-
-		float _r [samples+1];
-		
-		//debug
-		//for(int i=0; i<=samples; i++)
-		//	_r[i] = _t0[i]*expf( -_t1[i] -_t2[i] );
-		for(int i=0; i<=samples; i++)
-			_r[i] = _t0[i]*expf( -1.0*(_t1[i] + _t2[i]) );
-
-/*
-		for(int i=0; i<=samples; i++)
-		{
-			printf("%i: _r[i] = %.2f, _t0,_t1,_t2= %.2f %.2f %.2f, -_t1[i] -_t2[i]= %.2f, exp= %.2f \n", 
-				i, _r[i], _t0[i],_t1[i],_t2[i],
-				-_t1[i] -_t2[i], exp(-_t1[i] -_t2[i]));
-		}
-*/
-
-		float tmp = 0.0f;
-
-		for(int i=0; i<samples; i++)
-			tmp += _d*(_r[i] + _r[i+1]) / ( 2.0f);
-
-		//struct Vec3 s2 = vec3_sub(v2, c);	
-
-		//printf("phase: %.2f\n", phase(v2, s));
-/*
-		printf("a= %.2f %.2f %.2f  b= %.2f %.2f %.2f s= %.2f %.2f %.2f  phase: %.2f \n", 
-			a.x,a.y,a.z, 
-			b.x,b.y,b.z,
-			s.x,s.y,s.z, 
-			phase(b, s) );
-*/
-		//debug
-		tmp *= phase(bc, bs);
-		
-		//return brightness_scale_factor*(brightness_log_factor*log(tmp) + brightness_sum_factor);
-		return tmp;
-	}
-
-	static float sun_position(const struct Vec3 &a, const struct Vec3 &b, const struct Vec3 &s)
-	{
-		//compute ray from camera to upper atmosphere (use sphere and radius)
-		//intersection with upper atomsphere
-		
-		//const float ATMOSPHERE_DEPTH = 128.0;
-		//struct Vec3 _b;
-		//_b = vec3_scalar_mult(vec3_normalize(b), ATMOSPHERE_DEPTH);
-
-		struct Vec3 c = a;
-
-		struct Vec3 tb = vec3_sub(b, c);
-
-		//struct Vec3 tb = vec3_sub(_b, c);
-		struct Vec3 ts = vec3_sub(s, c);
-
-		tb = vec3_normalize(tb);
-		ts = vec3_normalize(ts);
-
-		float v = vec3_dot(tb, ts);
-
-		if(v <= 0.0)
-			v = 0.0;
-		v *= v;
-
-		return v;
-		//return vec3_cos2(tb,ts);
-
-
-		//return vec3_cos2(b,s);	//cos2 between end ray and sun, from camera
-	}
-
-	//sun distance
-	//atmosphere height
-	//planet radius
-
-	float update_point(const struct Vec3 &c, const struct Vec3 &b, const struct Vec3 &s)
-	{
-		const float ATMOSPHERE_DEPTH = 128.0;
-		//compute ray from camera to upper atmosphere (use sphere and radius)
-		//intersection with upper atomsphere
-		struct Vec3 _b;
-		_b = vec3_scalar_mult(vec3_normalize(b), ATMOSPHERE_DEPTH);
-		
-		//return sun_position(c,_b,s);
-		return in_scatter(c, _b, s);
 
 	}
 
@@ -774,7 +694,7 @@ class SkyboxRender
 
 		for(int i=0;i<6;i++)
 		{
-			const float plane_depth = sun.size/2.0;
+			const float plane_depth = sun.plane_size*0.5;
 			center[i] = vec3_scalar_mult(f[i], plane_depth);
 			center[i].z += plane_depth;
 
@@ -789,13 +709,13 @@ class SkyboxRender
 		for(int i=0; i<6; i++)
 		{
 			struct Vec3 rv, uv, lv, dv;
-			//f = vec3_scalar_mult(_f[i], sun.size/2);
-			rv = vec3_scalar_mult(r[i], sun.size/2);	//right
-			uv = vec3_scalar_mult(u[i], sun.size/2);	//up
+			//f = vec3_scalar_mult(_f[i], sun.plane_size/2.0);
+			rv = vec3_scalar_mult(r[i], sun.plane_size*0.5); 	//right
+			uv = vec3_scalar_mult(u[i], sun.plane_size*0.5);	//up
 
-			//nf = vec3_scalar_mult(_f[i], -sun.size/2);
-			lv = vec3_scalar_mult(r[i], -1.0*sun.size/2);	//left
-			dv = vec3_scalar_mult(u[i], -1.0*sun.size/2);	//down
+			//nf = vec3_scalar_mult(_f[i], -sun.plane_size/2.0);
+			lv = vec3_scalar_mult(r[i], -1.0*sun.plane_size*0.5);	//left
+			dv = vec3_scalar_mult(u[i], -1.0*sun.plane_size*0.5);	//down
 
 			struct Vec3 ul,bl,br,ur; 
 			ul = vec3_add3(center[i], uv, lv);
@@ -891,6 +811,9 @@ void init_rayleigh_scattering()
 	CFL.set_float("brightness_sum_factor", &SR->sun.brightness_sum_factor);
 	CFL.set_float("light_epsilon", &SR->sun.light_epsilon);
 	CFL.set_float("phase_factor", &SR->sun.phase_factor);
+
+	CFL.set_float("H0", &SR->sun.H0);
+	CFL.set_float("skybox_height", &SR->sun.skybox_height);
 
 	CFL.set_float("sun_distance", &SR->sun.sun_distance);
 	CFL.set_int("time_speed", &SR->time_speed);
