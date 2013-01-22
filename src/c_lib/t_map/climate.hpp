77e31ab2ec3ae62765d0404a4f5827ef045bcace
IceFlameCode <jalokim1998@poczta.onet.pl>
2013-01-22 11:07:59 +0100

    added basic climate stuff

---
+++ src/c_lib/t_map/climate.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#if DC_CLIENT
+dont_include_this_file_in_client
+#endif
+
+#include <t_map/t_map.hpp>
+#include <t_map/t_properties.hpp>
+
+namespace climate
+{
+    int get_temperature(int x, int y, int z)
+    {
+        GS_ASSERT(x < t_map::map_dim.x);
+        GS_ASSERT(x >= 0);
+        GS_ASSERT(y < t_map::map_dim.y);
+        GS_ASSERT(y >= 0);
+        GS_ASSERT(z < t_map::map_dim.z);
+        GS_ASSERT(z >= 0);
+        if(y>t_map::map_dim.y / 2) return (t_map::map_dim.y - y + x % 16 - x % 21) / 4 - z / 32;
+        else if(y<=t_map::map_dim.y / 2) return (y - x % 16 - x % 21) / 4 - z / 32;
+    }
+
+    int get_oxygen(int x, int y, int z)
+    {
+        GS_ASSERT(x < t_map::map_dim.x);
+        GS_ASSERT(x >= 0);
+        GS_ASSERT(y < t_map::map_dim.y);
+        GS_ASSERT(y >= 0);
+        GS_ASSERT(z < t_map::map_dim.z);
+        GS_ASSERT(z >= 0);
+        if(y>t_map::map_dim.y / 2) return t_map::map_dim.y - y + y % x + x % y - z / 8;
+        else if(y<=t_map::map_dim.y / 2) return y + y % x + x % y - z / 8;
+    }
+
+    int get_nitrogen(int x, int y, int z)
+    {
+        GS_ASSERT(x < t_map::map_dim.x);
+        GS_ASSERT(x >= 0);
+        GS_ASSERT(y < t_map::map_dim.y);
+        GS_ASSERT(y >= 0);
+        GS_ASSERT(z < t_map::map_dim.z);
+        GS_ASSERT(z >= 0);
+        if(y>t_map::map_dim.y / 2) return t_map::map_dim.y - y + 32 - x %  32 - z / 16;
+        else if(y<=t_map::map_dim.y / 2) return y + 32 - x % 32 - z / 16;
+    }
+
+    int get_methane(int x, int y, int z)
+    {
+        GS_ASSERT(x < t_map::map_dim.x);
+        GS_ASSERT(x >= 0);
+        GS_ASSERT(y < t_map::map_dim.y);
+        GS_ASSERT(y >= 0);
+        GS_ASSERT(z < t_map::map_dim.z);
+        GS_ASSERT(z >= 0);
+        if(x>t_map::map_dim.x / 2) return x - t_map::map_dim.x / 2 + z / 32 + y % 4;
+        else if(x<=t_map::map_dim.x / 2) return t_map::map_dim.x / 2 - x + z / 32 - y % 4;
+    }
+}
