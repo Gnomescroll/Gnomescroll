ef2ab6be54e2b19114d5fd8d6dd8dd0af4992cd0
Benjamin Rice <corpus_callosum@hotmail.com>
2013-01-23 04:45:52 -0500

    changed gen order FO REALZ

--- src/server/_main.cpp
+++ src/server/_main.cpp
@@ -15,13 +15,14 @@ void default_map_gen()
 {
     t_gen::noise_map_generate_map();
 
-    t_map::map_post_processing(); //regolith stuff
+    t_map::map_post_processing();  // regolith stuff
     t_gen::generate_rock_layer();
     t_gen::start_cave_generator();
     t_gen::populate_ore();
-    t_gen::place_native_plants();
+    t_gen::populate_crystals();
     t_gen::generate_ruins();
-    t_gen::add_terrain_features();
+    t_gen::add_terrain_features();  // gorges, trees, shrooms
+    t_gen::place_native_plants();
 
     map_gen::rough_floor(XMAX,YMAX,0,3, t_map::get_cube_type("bedrock"));
 }
@@ -82,7 +83,6 @@ void init_world()
     {
         serializer::begin_new_world_version();
         default_map_gen();
-        t_gen::populate_crystals();
         t_map::environment_process_startup();
         if (Options::serializer)
         {
