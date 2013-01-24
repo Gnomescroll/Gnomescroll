c30ddfe042f90191bcbdd4e49e20eb83a658d2f7
Steve Leonard <sleonard76@gmail.com>
2013-01-24 13:41:37 -0500

    clean up save_png

--- src/c_lib/SDL/png_save.hpp
+++ src/c_lib/SDL/png_save.hpp
@@ -1,7 +1,4 @@
 #pragma once
 
-//gamma corrected
-void save_png(const char* filename, float* in, int xres, int yres);
-
-//void save_png_RGB(const char* filename, float* in, int xres, int yres);
+void save_png(const char* filename, float* in, int xres, int yres, bool gamma_correction);
 void save_png_RGB(const char* filename, float* in, int xres, int yres, bool gamma_correction);
