8d85385339537626324d5a246751e52901675b27
HaltingState <haltingstate@gmail.com>
2013-01-25 03:39:14 -0800

    t_plant intergrated

--- src/c_lib/t_plant/plant_example.hpp
+++ src/c_lib/t_plant/plant_example.hpp
@@ -8,36 +8,43 @@ namespace t_plant
 
 	};
 */
-	class EXAMPLE_PLANT
+
+struct PlantExampleStruct
+{
+
+};
+
+void plant_example_init()
+{
+
+}
+
+void plant_example_teardown()
+{
+
+}
+
+void plant_example_tick()
+{
+
+}
+
+
+class EXAMPLE_PLANT
+{
+
+	struct BLOCK_LIST_ELEMENT
 	{
-
-		struct BLOCK_LIST_ELEMENT
-		{
-			int x,y,z;
-			int state;
-
-		};
-		static const int BLOCK_LIST_MAX = 1024;
-		struct BLOCK_LIST_ELEMENT block_list_array[BLOCK_LIST_MAX];
-		int block_list_n;
-
-		void init()
-		{
-			block_list_n = 0;
-
-		}
-
-		void teardown()
-		{
-
-		}
-
-		void tick()
-		{
-
-		}
+		int x,y,z;
+		int state;
 
 	};
+	static const int BLOCK_LIST_MAX = 1024;
+	struct BLOCK_LIST_ELEMENT block_list_array[BLOCK_LIST_MAX];
+	int block_list_n;
+
+
+};
 
 
 }
