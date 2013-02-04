960df9f3c922b73e1ce31903d2a68c40e84d78d1
HaltingState <haltingstate@gmail.com>
2013-02-03 16:23:18 -0800

    stuff

--- src/c_lib/t_map/lighting.hpp
+++ src/c_lib/t_map/lighting.hpp
@@ -497,7 +497,7 @@ void _push_envlight_update2(int x, int y, int z)
     }
 
     //cannot update light value of solid block!
-    struct MAP_ELEMENT e = mc->get_element(x & 15, y & 15 ,z);
+    struct MAP_ELEMENT e = mc->get_element(x & 15, y & 15, z);
     
     if(fast_cube_properties[e.block].solid == true)
     {
