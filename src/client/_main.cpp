a7ab6d6dfb4daf54a82be0b26898dd093849c463
HaltingState <haltingstate@gmail.com>
2013-02-04 01:24:43 -0800

    updating lighting

--- src/client/_main.cpp
+++ src/client/_main.cpp
@@ -566,7 +566,8 @@ int run()
 
         //int _now = _GET_MS_TIME();
 
-        t_map::_envlight_update_core(); //update lighting
+        t_map::_sunlight_update_core(); //update sun lighting
+        t_map::_envlight_update_core(); //update env lighting
 
         t_map::update_map();
         //int _then = _GET_MS_TIME();
