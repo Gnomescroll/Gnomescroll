7e54e364b59b7e463408d1effa6d4cac4beaf4fa
HaltingState <haltingstate@gmail.com>
2013-02-10 15:45:08 -0800

    stuff

--- src/server/_main.cpp
+++ src/server/_main.cpp
@@ -204,6 +204,21 @@ void tick()
         t_plant::tick();
     }
 
+    if (counter % (30*1800)  == 0)
+    {
+        //t_plant::tick();
+        int count = 0;
+        count += t_mech::count_mech( (int)t_mech::get_mech_type_dat("acadia_flower_stage_0") );
+        count += t_mech::count_mech( (int)t_mech::get_mech_type_dat("acadia_flower_stage_1") );
+        count += t_mech::count_mech( (int)t_mech::get_mech_type_dat("acadia_flower_stage_2") );
+
+        if(count < 256)
+            t_gen::place_native_plants();
+
+        //printf("count= %d \n", count);
+    }
+
+
     // Meteors
     const int meteor_fall_rate = ONE_HOUR * 16; // 16hrs
     const int meteor_shower_rate = ONE_HOUR * 4; // 4hrs
