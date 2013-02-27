510b3cc7e829eb5207fea30009646be80516e76e
HaltingState <haltingstate@gmail.com>
2013-02-27 02:43:32 -0800

    Stuf

--- src/c_lib/t_gen/terrain_features.hpp
+++ src/c_lib/t_gen/terrain_features.hpp
@@ -290,6 +290,7 @@ void carve_ray(float x_, float y_, float z_, int tiny_angle, int distance)
     //    printf("highest_z is now: %d  ", highest_z);
     //}
 
+    GS_ASSERT(z >= 0);
     GS_ASSERT(tiny_angle >= 0 && tiny_angle < NUM_LOOKUP_ANGLES);
     
     for (int i = 0; i < distance; i++)
@@ -297,6 +298,7 @@ void carve_ray(float x_, float y_, float z_, int tiny_angle, int distance)
         int x = x_ + (int)(i * sin_lookup_table[tiny_angle]);
         int y = y_ + (int)(i * cos_lookup_table[tiny_angle]);
         int z = z_;
+
 
         if(z < 1 || z > 127)
         {
