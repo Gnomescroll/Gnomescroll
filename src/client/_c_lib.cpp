4c04c9246a30af9a2746e9f329a1cd399f6614f6
HaltingState <haltingstate@gmail.com>
2013-05-13 17:26:18 -0700

    stuff

--- src/client/_c_lib.cpp
+++ src/client/_c_lib.cpp
@@ -119,7 +119,6 @@ bool c_lib_inited = false;
 #include <SDL/png_save.cpp>
 
 #include <SDL/haldCLUT/hald_clut.hpp> //should be .cpp
-#include <SDL/light/light_texture.hpp>
 
 //#include <SDL/mesh_loader/obj_loader.cpp>
 
