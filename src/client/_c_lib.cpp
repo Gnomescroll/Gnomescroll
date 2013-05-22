02e33e4bf9f9b61f8bb9f8022f46b4f531f581d5
Steve Leonard <sleonard76@gmail.com>
2013-05-22 14:27:15 -0400

    set minimum windows API versions

--- src/client/_c_lib.cpp
+++ src/client/_c_lib.cpp
@@ -57,7 +57,11 @@
 # include <malloc.h> //alloca function
 #endif
 
-#ifdef _WIN32
+#ifdef __WIN32__
+# define NTDDI_VERSION NTDDI_WINXP
+# define WINVER _WIN32_WINNT_WINXP
+# define _WIN32_WINNT _WIN32_WINNT_WINXP
+# define _WIN32_IE _WIN32_IE_IE50
 # include "windows.h"
 # undef interface
 # undef rad2
