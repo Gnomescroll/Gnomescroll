483351a0b2081c4530652215fb4852ef797ae9f2
Steve Leonard <sleonard76@gmail.com>
2013-10-04 01:22:54 -0400

    convert server wscript to use options

--- src/c_lib/common/common.hpp
+++ src/c_lib/common/common.hpp
@@ -82,7 +82,7 @@ unsigned char* hexstr_to_char(const char* hexstr)
     IF_ASSERT(len % 2 != 0)
         return NULL;
     size_t final_len = len / 2;
-    unsigned char* chrs = (unsigned char*)malloc((final_len+1) * sizeof(unsigned char));
+    unsigned char* chrs = (unsigned char*)malloc((final_len+1) * sizeof(*chrs));
     for (size_t i=0, j=0; j<final_len; i+=2, j++)
         chrs[j] = (hexstr[i] % 32 + 9) % 25 * 16 + (hexstr[i+1] % 32 + 9) % 25;
     chrs[final_len] = '\0';
