6f905cbb7f88130ae763fd2aa96146606d03da11
Kenje Hofileña <kenje4090@gmail.com>
2022-07-04 19:33:15 +0800

    updates

--- src/c_lib/common/compat_gl.h
+++ src/c_lib/common/compat_gl.h
@@ -5,22 +5,22 @@
 #ifdef linux
 # define NO_SDL_GLEXT
 # include <GL/glew.h>
-# include <SDL.h>
-# include <SDL_opengl.h>
+# include <SDL/SDL.h>
+# include <SDL/SDL_opengl.h>
 #elif _WIN32
 # ifdef __GNUC__
-#  include <SDL.h>
+#  include <SDL/SDL.h>
 #  include <GL/glew.h>
 # endif
 # ifdef __MSVC__
-#  include <SDL.h>
+#  include <SDL/SDL.h>
 #  include <GL/glew.h>
 # endif
 #elif __APPLE__
 # include <GL/glew.h>
-# include <SDL.h>
+# include <SDL/SDL.h>
 # include <OpenGL/gl.h>
 # include <OpenGL/glu.h>
 #endif
 
-#include <SDL_image.h>
+#include <SDL/SDL_image.h>
